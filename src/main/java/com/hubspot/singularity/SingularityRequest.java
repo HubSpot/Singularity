c95c2e2b038c90cb9ed92f824f81243e5384df35
Whitney Sorenson <wsorenson@hubspot.com>
2013-10-24 19:19:54 -0400

    more singularity request options

--- src/main/java/com/hubspot/singularity/SingularityRequest.java
+++ src/main/java/com/hubspot/singularity/SingularityRequest.java
@@ -1,6 +1,7 @@
 package com.hubspot.singularity;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -17,31 +18,37 @@ import com.google.common.collect.Lists;
 import com.hubspot.mesos.Resources;
 
 public class SingularityRequest {
-  
-  @NotNull
-  private final String command;
-  
+
   @NotNull
   private final String name;
-  
+
   private final String executor;
   private final Resources resources;
-  
+
   private final String schedule;
-  
+
   @Min(0)
   private final Integer instances;
+  private final Boolean rackSensitive;
   private final Boolean daemon;
-  
+
+  private final String command;
+  private final Map<String, String> args;
+  private final List<String> uris;
+  private final Object executorData;
+
   private static final Joiner JOINER = Joiner.on(" ");
-  
+
   @JsonCreator
-  public SingularityRequest(@JsonProperty("command") String command, @JsonProperty("name") String name, @JsonProperty("executor") String executor, @JsonProperty("resources") Resources resources, @JsonProperty("schedule") String schedule, @JsonProperty("instances") Integer instances, @JsonProperty("daemon") Boolean daemon) {
+  public SingularityRequest(@JsonProperty("command") String command, @JsonProperty("name") String name, @JsonProperty("executor") String executor, @JsonProperty("resources") Resources resources, @JsonProperty("schedule") String schedule,
+      @JsonProperty("instances") Integer instances, @JsonProperty("daemon") Boolean daemon, @JsonProperty("args") Map<String, String> args, @JsonProperty("uris") List<String> uris,
+      @JsonProperty("executorData") Object executorData, @JsonProperty("rackSensitive") Boolean rackSensitive) {
     schedule = adjustSchedule(schedule);
-    
+
     Preconditions.checkState(schedule == null || ((instances == null || instances == 0) && (daemon == null || !daemon)), "Scheduled requests can not be ran on more than one instance, and must not be daemons");
     Preconditions.checkState((daemon == null || daemon) || (instances == null || instances == 0), "Non-daemons can not be ran on more than one instance");
     Preconditions.checkState(schedule == null || CronExpression.isValidExpression(schedule), "Cron Schedule %s was not parseable", schedule);
+    Preconditions.checkState((command != null && executorData == null) || (executorData != null && executor != null && command == null), "If not using custom executor, specify a command. If using custom executor, specify executorData OR command.");
     
     this.command = command;
     this.name = name;
@@ -50,50 +57,68 @@ public class SingularityRequest {
     this.schedule = schedule;
     this.daemon = daemon;
     this.instances = instances;
+    this.rackSensitive = rackSensitive;
+    
+    this.args = args;
+    this.uris = uris;
+    this.executorData = executorData;
+  }
+
+  public Map<String, String> getArgs() {
+    return args;
+  }
+
+  public List<String> getUris() {
+    return uris;
+  }
+
+  public Object getExecutorData() {
+    return executorData;
   }
   
- /**
-  * 
-  *  Transforms unix cron into fucking quartz cron; adding seconds if not passed in and switching either day of month or day of week to ? 
-  *  
-  *   Field Name              Allowed Values          Allowed Special Characters
-  *   Seconds         0-59            , - * /
-  *   Minutes         0-59            , - * /
-  *   Hours           0-23            , - * /
-  *   Day-of-month            1-31            , - * ? / L W
-  *   Month           1-12 or JAN-DEC         , - * /
-  *   Day-of-Week             1-7 or SUN-SAT          , - * ? / L #
-  *   Year (Optional)         empty, 1970-2199                , - * /
-  */
+  public Boolean getRackSensitive() {
+    return rackSensitive;
+  }
+
+  /**
+   * 
+   * Transforms unix cron into fucking quartz cron; adding seconds if not passed
+   * in and switching either day of month or day of week to ?
+   * 
+   * Field Name Allowed Values Allowed Special Characters Seconds 0-59 , - * /
+   * Minutes 0-59 , - * / Hours 0-23 , - * / Day-of-month 1-31 , - * ? / L W
+   * Month 1-12 or JAN-DEC , - * / Day-of-Week 1-7 or SUN-SAT , - * ? / L # Year
+   * (Optional) empty, 1970-2199 , - * /
+   */
   private String adjustSchedule(String schedule) {
     if (schedule == null) {
       return null;
     }
-    
+
     String[] split = schedule.split(" ");
-    
-    if (split.length < 4) { 
+
+    if (split.length < 4) {
       throw new IllegalStateException(String.format("Schedule %s is invalid", schedule));
     }
-    
+
     List<String> newSchedule = Lists.newArrayListWithCapacity(6);
-    
+
     boolean hasSeconds = split.length > 5;
-    
+
     if (!hasSeconds) {
       newSchedule.add("*");
     } else {
       newSchedule.add(split[0]);
     }
-    
+
     int indexMod = hasSeconds ? 1 : 0;
-    
+
     newSchedule.add(split[indexMod + 0]);
     newSchedule.add(split[indexMod + 1]);
-    
+
     String dayOfMonth = split[indexMod + 2];
     String dayOfWeek = split[indexMod + 4];
-    
+
     if (dayOfWeek.equals("*")) {
       dayOfWeek = "?";
     } else if (!dayOfWeek.equals("?")) {
@@ -103,11 +128,10 @@ public class SingularityRequest {
     newSchedule.add(dayOfMonth);
     newSchedule.add(split[indexMod + 3]);
     newSchedule.add(dayOfWeek);
-    
+
     return JOINER.join(newSchedule);
   }
-  
-  
+
   public byte[] getRequestData(ObjectMapper objectMapper) throws Exception {
     return objectMapper.writeValueAsBytes(this);
   }
@@ -115,7 +139,7 @@ public class SingularityRequest {
   public static SingularityRequest getRequestFromData(byte[] request, ObjectMapper objectMapper) throws Exception {
     return objectMapper.readValue(request, SingularityRequest.class);
   }
-  
+
   public Integer getInstances() {
     return instances;
   }
@@ -123,12 +147,12 @@ public class SingularityRequest {
   public Boolean getDaemon() {
     return daemon;
   }
-  
+
   @JsonIgnore
   public boolean alwaysRunning() {
     return (daemon == null || daemon.booleanValue()) && !isScheduled();
   }
-  
+
   @JsonIgnore
   public boolean isScheduled() {
     return schedule != null;
