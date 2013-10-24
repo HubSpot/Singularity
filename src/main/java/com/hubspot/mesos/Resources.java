4b532aac0c994548b867e404921d97db02f6e267
Whitney Sorenson <wsorenson@hubspot.com>
2013-10-24 19:19:23 -0400

    add ports to resources

--- src/main/java/com/hubspot/mesos/Resources.java
+++ src/main/java/com/hubspot/mesos/Resources.java
@@ -7,13 +7,19 @@ public class Resources {
 
   private final int cpus;
   private final int memoryMb;
+  private final int numPorts;
 
   @JsonCreator
-  public Resources(@JsonProperty("cpus") int cpus, @JsonProperty("memoryMb") int memoryMb) {
+  public Resources(@JsonProperty("cpus") int cpus, @JsonProperty("memoryMb") int memoryMb, @JsonProperty("numPorts") int numPorts) {
     this.cpus = cpus;
     this.memoryMb = memoryMb;
+    this.numPorts = numPorts;
   }
 
+  public int getNumPorts() {
+    return numPorts;
+  }
+  
   public int getCpus() {
     return cpus;
   }
