ad32022f9adc26b47108a1d51e9b46a453d17cb7
Whitney Sorenson <wsorenson@hubspot.com>
2013-12-04 10:38:13 +0000

    email tweaks

--- SingularityService/src/main/java/com/hubspot/singularity/config/SMTPConfiguration.java
+++ SingularityService/src/main/java/com/hubspot/singularity/config/SMTPConfiguration.java
@@ -50,16 +50,16 @@ public class SMTPConfiguration {
     this.smtpLoggingConfiguration = smtpLoggingConfiguration;
   }
 
-  public String getUsername() {
-    return username;
+  public Optional<String> getUsername() {
+    return Optional.fromNullable(username);
   }
 
   public void setUsername(String username) {
     this.username = username;
   }
 
-  public String getPassword() {
-    return password;
+  public Optional<String> getPassword() {
+    return Optional.fromNullable(password);
   }
 
   public void setPassword(String password) {
