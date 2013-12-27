HubSpot/Singularity

commit 9377ca1ca7db498e2b68bffbcd93abd155ba28a2

Inch closer to working pagination

SingularityUI/app/collections/RequestsSearch.coffee
@@ -2,7 +2,7 @@ Requests = require './Requests'
 
 class RequestsSearch extends Requests
 
-    url: => "#{ env.SINGULARITY_BASE }/#{ constants.apiBase }/history/requests/search?count=6&#{ utils.paramsString @params }requestIdLike=#{ @query }"
+    url: => "#{ env.SINGULARITY_BASE }/#{ constants.apiBase }/history/requests/search?count=6&#{ $.param @params }requestIdLike=#{ @query }"
 
     initialize: (models, { @query, @params }) =>
 

SingularityUI/app/collections/TasksSearch.coffee
@@ -2,7 +2,7 @@ Tasks = require './Tasks'
 
 class TasksSearch extends Tasks
 
-    url: => "#{ env.SINGULARITY_BASE }/#{ constants.apiBase }/history/tasks/search?count=6&#{ utils.paramsString @params }requestIdLike=#{ @query }"
+    url: => "#{ env.SINGULARITY_BASE }/#{ constants.apiBase }/history/tasks/search?count=6&#{ $.param @params }requestIdLike=#{ @query }"
 
     initialize: (models, { @query, @params }) =>
 

SingularityUI/app/collections/HistoricalTasks.coffee
@@ -1,12 +1,24 @@
-Collection = require './collection'
+RequestTasks = require './RequestTasks'
 
-class RequestTasks extends Collection
-    url: => "#{ env.SINGULARITY_BASE }/#{ constants.apiBase }/history/request/#{ @requestId }/tasks#{ if @active then '/active' else '' }"
+class HistoricalTasksCollection extends Mixen(RequestTasks, Teeble.ServerCollection)
+    model: Backbone.Model
 
-    comparator: 'createdAt'
+    paginator_core:
+        url: "#{ env.SINGULARITY_BASE }/#{ constants.apiBase }/history/request/#{ @requestId }/tasks"
+        type: "GET"
+        dataType: "json"
 
-    initialize: (models, { @requestId, @active }) =>
-        super
+    paginator_ui:
+        firstPage: 1
+        currentPage: 1
+        perPage: 5
+        totalPages: 50
+        pagesInRange: 10
+
+    server_api:
+        count: -> @perPage
+        page: -> @currentPage
+        orderBy: 'updatedAt'
 
     parse: (tasks) ->
         _.each tasks, (task) ->
@@ -21,4 +33,5 @@ class RequestTasks extends Collection
 
         tasks
 
-module.exports = RequestTasks
+
+module.exports = HistoricalTasksCollection

SingularityUI/app/lib/view_helper.coffee
@@ -5,7 +5,6 @@ Handlebars.registerHelper 'ifGT', (v1, v2, options) -> if v1 > v2 then options.f
 Handlebars.registerHelper 'pluralize', (number, single, plural) -> if number is 1 then single else plural
 
 Handlebars.registerHelper 'hardBreak', (string, options) -> string?.replace(/(:|-)/g, '$1<wbr/>')
-Shlandlebars.registerHelper 'hardBreak', (string, options) -> string?.replace(/(:|-)/g, '$1<wbr/>')
 
 Handlebars.registerHelper 'eachWithFn', (items, options) ->
     _(items).map((item, i, items) =>

SingularityUI/app/utils.coffee
@@ -1,12 +1,12 @@
 class Utils
 
-    getHTMLTitleFromHistoryFragment: (fragment) ->
+    @getHTMLTitleFromHistoryFragment: (fragment) ->
         _.capitalize(fragment.split('\/').join(' '))
 
-    stringJSON: (object) ->
+    @stringJSON: (object) ->
         JSON.stringify object, null, '    '
 
-    viewJSON: (type, objectId) ->
+    @viewJSON: (type, objectId) ->
         lookupObject = {}
 
         if type is 'task'
@@ -19,13 +19,7 @@ class Utils
                 width: 800
             message: "<pre>#{ lookupObject[objectId].JSONString }</pre>"
 
-    setupSortableTables: ->
+    @setupSortableTables: ->
         sortable.init()
 
-    paramsString: (params) ->
-        paramsString = ''
-        for key, value of params
-            paramsString += "#{ key }=#{ value }&"
-        paramsString
-
-module.exports = new Utils
+module.exports = Utils

SingularityUI/app/views/request.coffee
@@ -2,6 +2,8 @@ View = require './view'
 
 RequestTasks = require '../collections/RequestTasks'
 
+HistoricalTasksCollection = require '../collections/HistoricalTasks'
+
 class RequestView extends View
 
     template: require './templates/request'
@@ -9,18 +11,10 @@ class RequestView extends View
     initialize: =>
         @request = app.allRequests[@options.requestId]
 
-        count = 0
         @requestTasksActive = new RequestTasks [], { requestId: @options.requestId, active: true }
-        @requestTasksActive.fetch().done =>
-            @fetchDoneActive = true
-            if ++count is 2
-              @render()
-
         @requestTasksHistorical = new RequestTasks [], { requestId: @options.requestId, active: false }
-        @requestTasksHistorical.fetch().done =>
-            @fetchDoneHistorical = true
-            if ++count is 2
-              @render()
+
+        $.when(@requestTasksActive.fetch(), @requestTasksHistorical.fetch()).done _.bind(@render, @)
 
     render: =>
         if not @request
@@ -47,56 +41,14 @@ class RequestView extends View
         utils.setupSortableTables()
 
     renderHistoricalTasksPaginated: ->
-        if @historicalTasksPaginated
-            $('.historical-tasks-paginated').html @historicalTasksPaginated.render().el
-            return
-
-        @teebleView = Teeble.TableView.extend({})
-
-        @PaginatedCollection = Teeble.ClientCollection.extend(
-            model: Backbone.Model
-            paginator_core:
-                url: "#{ env.SINGULARITY_BASE }/#{ constants.apiBase }/history/request/#{ @options.requestId }/tasks"
-                type: "GET"
-                dataType: "json"
-
-            paginator_ui:
-                firstPage: 1
-                currentPage: 1
-                perPage: 5
-                totalPages: 50
-                pagesInRange: 10
-
-            server_api:
-                count: -> @perPage
-                page: -> @currentPage
-                orderBy: 'updatedAt'
-
-            parse: (tasks) ->
-                # @totalPages = 50#Math.ceil(tasks / this.perPage)
-                # @totalRecords = 50#parseInt(tasks, 10)
-
-                # TODO abstract
-                # since duplicated from RequestTasks.coffee
-                _.each tasks, (task) ->
-                    task.id = task.taskId.id
-                    task.name = task.id
-                    task.updatedAtHuman = if task.updatedAt? then moment(task.updatedAt).from() else ''
-                    task.createdAtHuman = if task.createdAt? then moment(task.createdAt).from() else ''
-                    task.lastStatusHuman = if constants.taskStates[task.lastStatus] then constants.taskStates[task.lastStatus].label else ''
-                    task.isActive = if constants.taskStates[task.lastStatus] then constants.taskStates[task.lastStatus].isActive else false
-
-                tasks
-        )
-
-        @paginatedItems = new @PaginatedCollection()
-        @paginatedItems.fetch
+        @historicalTasks = new HistoricalTasksCollection [], { requestId: @options.requestId, active: false }
+        @historicalTasks.fetch
             reset: true
             success: =>
-                @paginatedItems.pager()
-                @historicalTasksPaginated = new @teebleView(
-                    compile: Shlandlebars.compile # oh yeah...
-                    collection: @paginatedItems
+                @historicalTasks.pager()
+                @historicalTasksView = new Teeble.TableView
+                    compile: Handlebars.compile
+                    collection: @historicalTasks
                     pagination: true
                     table_class: 'table'
                     partials: [
@@ -121,10 +73,9 @@ class RequestView extends View
                             </td>
                         '''
                     ]
-                )
 
-                @historicalTasksPaginated.setElement $('.historical-tasks-paginated')[0]
-                @historicalTasksPaginated.render()
+                @historicalTasksView.setElement $('.historical-tasks-paginated')[0]
+                @historicalTasksView.render()
 
                 @setupEvents()
 

SingularityUI/config.coffee
@@ -15,7 +15,6 @@ exports.config =
 
             order:
                 before: [
-                    'vendor/scripts/shlandlebars-1.1.2.js'
                     'vendor/scripts/log.coffee'
                     'vendor/scripts/parseDec.coffee'
                     'vendor/scripts/modernizr.custom-2.6.2.js'

commit 306669d9cdb4023d7ef64b0c5e37606064d62682

Fix search (broken by 9377ca1c)

SingularityUI/app/collections/RequestsSearch.coffee
@@ -2,7 +2,7 @@ Requests = require './Requests'
 
 class RequestsSearch extends Requests
 
-    url: => "#{ env.SINGULARITY_BASE }/#{ constants.apiBase }/history/requests/search?count=6&#{ $.param @params }requestIdLike=#{ @query }"
+    url: => "#{ env.SINGULARITY_BASE }/#{ constants.apiBase }/history/requests/search?count=6&#{ $.param @params }&requestIdLike=#{ @query }"
 
     initialize: (models, { @query, @params }) =>
 

SingularityUI/app/collections/TasksSearch.coffee
@@ -2,7 +2,7 @@ Tasks = require './Tasks'
 
 class TasksSearch extends Tasks
 
-    url: => "#{ env.SINGULARITY_BASE }/#{ constants.apiBase }/history/tasks/search?count=6&#{ $.param @params }requestIdLike=#{ @query }"
+    url: => "#{ env.SINGULARITY_BASE }/#{ constants.apiBase }/history/tasks/search?count=6&#{ $.param @params }&requestIdLike=#{ @query }"
 
     initialize: (models, { @query, @params }) =>
 