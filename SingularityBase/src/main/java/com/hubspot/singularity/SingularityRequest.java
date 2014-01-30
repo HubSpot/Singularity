fd9cf6cfdc11b61c041ff60431c4da484a0f68b9
tpetr <tpetr@hubspot.com>
2014-01-30 16:51:35 -0500

    need to ignore unknown

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityRequest.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityRequest.java
@@ -2,6 +2,7 @@ package com.hubspot.singularity;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
@@ -12,6 +13,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SingularityRequest extends SingularityJsonObject {
 
   private final String id;
