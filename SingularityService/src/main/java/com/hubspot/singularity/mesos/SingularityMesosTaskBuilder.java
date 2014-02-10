67e76c2d031a47590729aa126b0b210ed2131a1a
tpetr <tpetr@hubspot.com>
2014-02-10 14:35:14 -0500

    Revert "set resources on ExecutorInfo too -- fixes issue where hard memory limits arent set"
    Didn't seem to work.

--- SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityMesosTaskBuilder.java
+++ SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityMesosTaskBuilder.java
@@ -1,9 +1,25 @@
 package com.hubspot.singularity.mesos;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.mesos.Protos;
+import org.apache.mesos.Protos.CommandInfo;
+import org.apache.mesos.Protos.CommandInfo.URI;
+import org.apache.mesos.Protos.Environment;
+import org.apache.mesos.Protos.Environment.Variable;
+import org.apache.mesos.Protos.ExecutorID;
+import org.apache.mesos.Protos.ExecutorInfo;
+import org.apache.mesos.Protos.Resource;
+import org.apache.mesos.Protos.TaskID;
+import org.apache.mesos.Protos.TaskInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Objects;
-import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.protobuf.ByteString;
 import com.hubspot.mesos.MesosUtils;
@@ -11,17 +27,6 @@ import com.hubspot.mesos.Resources;
 import com.hubspot.singularity.SingularityTask;
 import com.hubspot.singularity.SingularityTaskId;
 import com.hubspot.singularity.SingularityTaskRequest;
-import org.apache.mesos.Protos;
-import org.apache.mesos.Protos.*;
-import org.apache.mesos.Protos.CommandInfo.URI;
-import org.apache.mesos.Protos.Environment.Variable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 public class SingularityMesosTaskBuilder {
 
@@ -35,23 +40,6 @@ public class SingularityMesosTaskBuilder {
     this.objectMapper = objectMapper;
     this.rackManager = rackManager;
   }
-
-  public List<Resource> buildMesosResources(Protos.Offer offer, Resources resources) {
-    final List<Resource> mesosResources = Lists.newArrayList();
-
-    if (resources.getNumPorts() > 0) {
-      final Resource portsResource = MesosUtils.getPortsResource(resources.getNumPorts(), offer);
-
-      if (portsResource != null) {
-        mesosResources.add(portsResource);
-      }
-    }
-
-    mesosResources.add(MesosUtils.getCpuResource(resources.getCpus()));
-    mesosResources.add(MesosUtils.getMemoryResource(resources.getMemoryMb()));
-
-    return mesosResources;
-  }
   
   public SingularityTask buildTask(Protos.Offer offer, SingularityTaskRequest taskRequest, Resources resources) {
     final String rackId = rackManager.getRackId(offer);
@@ -62,16 +50,26 @@ public class SingularityMesosTaskBuilder {
     final TaskInfo.Builder bldr = TaskInfo.newBuilder()
         .setTaskId(TaskID.newBuilder().setValue(taskId.toString()));
     
-    final long[] ports = MesosUtils.getPorts(MesosUtils.getPortsResource(resources.getNumPorts(), offer), resources.getNumPorts());
-    final List<Resource> mesosResources = buildMesosResources(offer, resources);
+    long[] ports = null;
+    Resource portsResource = null;
+    
+    if (resources.getNumPorts() > 0) {
+      portsResource = MesosUtils.getPortsResource(resources.getNumPorts(), offer);
+      ports = MesosUtils.getPorts(portsResource, resources.getNumPorts());
+    }
     
     if (taskRequest.getRequest().getExecutor() != null) {
-      prepareCustomExecutor(bldr, taskId, taskRequest, mesosResources, ports);
+      prepareCustomExecutor(bldr, taskId, taskRequest, ports);
     } else {
       prepareCommand(bldr, taskRequest, ports);
     }
-
-    bldr.addAllResources(mesosResources);
+    
+    if (portsResource != null) {
+      bldr.addResources(portsResource);
+    }
+    
+    bldr.addResources(MesosUtils.getCpuResource(resources.getCpus()));
+    bldr.addResources(MesosUtils.getMemoryResource(resources.getMemoryMb()));
     
     bldr.setSlaveId(offer.getSlaveId());
     
@@ -83,12 +81,11 @@ public class SingularityMesosTaskBuilder {
   }
   
   @SuppressWarnings("unchecked")
-  private void prepareCustomExecutor(final TaskInfo.Builder bldr, final SingularityTaskId taskId, final SingularityTaskRequest task, final List<Resource> taskResources, final long[] ports) {
+  private void prepareCustomExecutor(final TaskInfo.Builder bldr, final SingularityTaskId taskId, final SingularityTaskRequest task, final long[] ports) {
     bldr.setExecutor(
         ExecutorInfo.newBuilder()
           .setCommand(CommandInfo.newBuilder().setValue(task.getRequest().getExecutor()))
           .setExecutorId(ExecutorID.newBuilder().setValue(String.format("singularity-%s", taskId.toString().replace(':', '_'))))
-          .addAllResources(taskResources)
     );
     
     Object executorData = task.getRequest().getExecutorData();
