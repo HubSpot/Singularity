HubSpot/Singularity

commit 22be126738c657d65d29670f5a3e9472a1dbe476

auto-tail (fixes #47)

SingularityUI/app/lib/router.coffee
@@ -61,12 +61,14 @@ class Router extends Backbone.Router
         '*anything': 'templateFromURLFragment'
 
     dashboard: ->
+        app.views.current?.remove()
         if not app.views.dashboard?
             app.views.dashboard = new DashboardView
         app.views.current = app.views.dashboard
         app.show app.views.dashboard.render()
 
     search: ->
+        app.views.current?.remove()
         if not app.views.search?
             app.views.search = new SearchView
             app.views.current = app.views.search
@@ -76,6 +78,7 @@ class Router extends Backbone.Router
             app.show app.views.search
 
     status: ->
+        app.views.current?.remove()
         if not app.views.status?
             app.views.status = new StatusView
             app.views.current = app.views.status
@@ -88,12 +91,14 @@ class Router extends Backbone.Router
         @requestsFiltered 'active'
 
     requestsFiltered: (requestsFilter) ->
+        app.views.current?.remove()
         if not app.views.requests?
             app.views.requests = new RequestsView requestsFilter: requestsFilter
         app.views.current = app.views.requests
         app.show app.views.requests.render(requestsFilter).refresh()
 
     request: (requestId) ->
+        app.views.current?.remove()
         app.views.requestViews = {} if not app.views.requestViews
         if not app.views.requestViews[requestId]
             app.views.requestViews[requestId] = new RequestView requestId: requestId
@@ -107,12 +112,14 @@ class Router extends Backbone.Router
         @tasksFiltered 'active'
 
     tasksFiltered: (tasksFilter) ->
+        app.views.current?.remove()
         if not app.views.tasks?
             app.views.tasks = new TasksView tasksFilter: tasksFilter
         app.views.current = app.views.tasks
         app.show app.views.tasks.render(tasksFilter).refresh()
 
     task: (taskId) ->
+        app.views.current?.remove()
         app.views.taskViews = {} if not app.views.taskViews
         if not app.views.taskViews[taskId]
             app.views.taskViews[taskId] = new TaskView taskId: taskId
@@ -123,6 +130,7 @@ class Router extends Backbone.Router
             app.show app.views.taskViews[taskId]
 
     files: (taskId, path='') ->
+        app.views.current?.remove()
         app.views.filesViews = {} if not app.views.filesViews
         if not app.views.filesViews[taskId]
             app.views.filesViews[taskId] = new FilesView taskId: taskId, path: path
@@ -132,6 +140,7 @@ class Router extends Backbone.Router
         app.show app.views.filesViews[taskId].render()
 
     tail: (taskId, path='') ->
+        app.views.current?.remove()
         app.views.tailViews = {} if not app.views.tailViews
         if not app.views.tailViews[taskId] or app.views.tailViews[taskId].path isnt path
             app.views.tailViews[taskId] = new TailView taskId: taskId, path: path
@@ -139,6 +148,7 @@ class Router extends Backbone.Router
         app.show app.views.tailViews[taskId].render()
 
     racks: ->
+        app.views.current?.remove()
         if not app.views.racks?
             app.views.racks = new RacksView
             app.views.current = app.views.racks
@@ -148,6 +158,7 @@ class Router extends Backbone.Router
             app.show app.views.racks.refresh()
 
     slaves: ->
+        app.views.current?.remove()
         if not app.views.slaves?
             app.views.slaves = new SlavesView
             app.views.current = app.views.slaves
@@ -157,6 +168,7 @@ class Router extends Backbone.Router
             app.show app.views.slaves.refresh()
 
     webhooks: ->
+        app.views.current?.remove()
         if not app.views.webhooks?
             app.views.webhooks = new WebhooksView
             app.views.current = app.views.webhooks

SingularityUI/app/views/tailer.coffee
@@ -6,17 +6,28 @@ class TailerView extends Backbone.View
     lineTemplate: require './templates/logline'
 
     readLength: 30000
+    tailInterval: 3000
+
+    events:
+        'scroll': 'handleScroll'
 
     initialize: ->
+        @tailing = null
+
+        @nextPromise = Q {}
+        @prevPromise = Q {}
+
         @lines = LogLines.getInstance @options
 
+        @$container = @$('.tail-container')
+
         @lines.on 'sort', =>
-            children = @$el.children()
+            children = @$container.children()
 
             # append all if tailer element is empty
             if children.length == 0
                 @lines.each (model) =>
-                    @$el.append @renderLine model
+                    @$container.append @renderLine model
                 return
 
             head = children.first()
@@ -36,10 +47,11 @@ class TailerView extends Backbone.View
 
             # add lines at bottom
             _.each @lines.last(@lines.length - tailIndex - 1), (model) =>
-                @$el.append @renderLine model
+                @$container.append @renderLine model
 
-        # page / tail based on scroll events
-        @$el.scroll @handleScroll
+    remove: =>
+        @stopTailing()
+        super
 
     scrollToBottom: =>
         @$el.scrollTop @el.scrollHeight
@@ -49,22 +61,51 @@ class TailerView extends Backbone.View
         data.data = $.trim data.data
         @lineTemplate data
 
-    page: =>
+    fetchPrev: =>
+        # short circuit when we're at the top
         if @lines.getMinOffset() is 0
-            return
+            return Q
+                data: ''
+                offset: 0
 
-        @lines.fetch
-            data: $.param
-                offset: Math.max(@lines.getMinOffset() - @readLength, 0)
-                length: Math.min(@lines.getMinOffset(), @readLength)
-            remove: false
+        if @prevPromise.isFulfilled()
+            @prevPromise = Q @lines.fetch
+                data: $.param
+                    offset: Math.max(@lines.getMinOffset() - @readLength, 0)
+                    length: Math.min(@lines.getMinOffset(), @readLength)
+                remove: false
 
-    tail: =>
-        @lines.fetch
-            data: $.param
-                offset: @lines.getMaxOffset()
-                length: @readLength
-            remove: false
+        @prevPromise
+
+    fetchNext: (offset=null) =>
+        if @nextPromise.isFulfilled()
+            @nextPromise = Q @lines.fetch
+                data: $.param
+                    offset: offset or @lines.getMaxOffset()
+                    length: @readLength
+                remove: false
+
+        @nextPromise
+
+    # if not already tailing, fetch more
+    # if we're at the end, start tailing
+    maybeStartTailing: =>
+        if @tailing is null
+            @fetchNext().then (data) =>
+                if data.data is '\n'
+                    @startTailing()
+
+    startTailing: =>
+        if @tailing is null
+            @tailing = setInterval @tail, @tailInterval
+
+    tail: (offset=null) =>
+        @fetchNext(offset).then @scrollToBottom
+
+    stopTailing: =>
+        if @tailing isnt null
+            clearInterval @tailing
+            @tailing = null
 
     handleScroll: =>
         scrollTop = @$el.scrollTop()
@@ -72,27 +113,21 @@ class TailerView extends Backbone.View
         scrollMax = @el.scrollHeight
 
         if scrollTop is 0 and @lines.getMinOffset() > 0
-            @page()
+            # if at top, fetch previous lines
+            @fetchPrev()
         else if scrollBottom is scrollMax
-            @tail()  # TODO: keep attempting tail if we're at the bottom and theres no more data yet
+            # if at bottom, start tailing if appropriate
+            @maybeStartTailing()
+        else
+            # if somewhere in the middle, stop tailing
+            @stopTailing()
 
     render: =>
         @$el.addClass 'loading'
 
         # seek to the end of the file
         @lines.fetchEndOffset().then (offset) =>
-            ajaxPromise = @lines.fetch
-                data: $.param
-                    offset: Math.max(offset - @readLength, 0)
-                    length: Math.min(offset, @readLength)
-
-            ajaxPromise.done =>
-                @$el.removeClass 'loading'
-                @scrollToBottom()
-                setTimeout @handleScroll
-
-            ajaxPromise.fail (jqXHR, status, error) =>
-                @$el.removeClass 'loading'
+            @tail Math.max(0, offset - @readLength)
 
         @
 

SingularityUI/app/views/tail.coffee
@@ -20,11 +20,15 @@ class TailView extends View
 
         @filename = _.last(pieces)
 
+    remove: =>
+        @tailer?.remove()
+        super
+
     render: =>
         @$el.html @template {@taskId, @path, @subfolders, @filename}
 
         @tailer = new TailerView
-            el: @$('.tail-container')
+            el: @$('.tail-outer')
             taskId: @taskId
             path: @path
 

commit 51fbac7ac77f5c70df49a1d9abf31bf7460f9ff5

Fix bug introduced by 22be126738c657d65d29670f5a3e9472a1dbe476 while keeping auto-tail fix

SingularityUI/app/lib/router.coffee
@@ -61,14 +61,12 @@ class Router extends Backbone.Router
         '*anything': 'templateFromURLFragment'
 
     dashboard: ->
-        app.views.current?.remove()
         if not app.views.dashboard?
             app.views.dashboard = new DashboardView
         app.views.current = app.views.dashboard
         app.show app.views.dashboard.render()
 
     search: ->
-        app.views.current?.remove()
         if not app.views.search?
             app.views.search = new SearchView
             app.views.current = app.views.search
@@ -78,7 +76,6 @@ class Router extends Backbone.Router
             app.show app.views.search
 
     status: ->
-        app.views.current?.remove()
         if not app.views.status?
             app.views.status = new StatusView
         app.views.current = app.views.status
@@ -88,14 +85,12 @@ class Router extends Backbone.Router
         @requestsFiltered 'active'
 
     requestsFiltered: (requestsFilter) ->
-        app.views.current?.remove()
         if not app.views.requests?
             app.views.requests = new RequestsView requestsFilter: requestsFilter
         app.views.current = app.views.requests
         app.show app.views.requests.render(requestsFilter).refresh()
 
     request: (requestId) ->
-        app.views.current?.remove()
         app.views.requestViews = {} if not app.views.requestViews
         if not app.views.requestViews[requestId]
             app.views.requestViews[requestId] = new RequestView requestId: requestId
@@ -109,14 +104,12 @@ class Router extends Backbone.Router
         @tasksFiltered 'active'
 
     tasksFiltered: (tasksFilter) ->
-        app.views.current?.remove()
         if not app.views.tasks?
             app.views.tasks = new TasksView tasksFilter: tasksFilter
         app.views.current = app.views.tasks
         app.show app.views.tasks.render(tasksFilter).refresh()
 
     task: (taskId) ->
-        app.views.current?.remove()
         app.views.taskViews = {} if not app.views.taskViews
         if not app.views.taskViews[taskId]
             app.views.taskViews[taskId] = new TaskView taskId: taskId
@@ -127,7 +120,6 @@ class Router extends Backbone.Router
             app.show app.views.taskViews[taskId]
 
     files: (taskId, path='') ->
-        app.views.current?.remove()
         app.views.filesViews = {} if not app.views.filesViews
         if not app.views.filesViews[taskId]
             app.views.filesViews[taskId] = new FilesView taskId: taskId, path: path
@@ -137,7 +129,6 @@ class Router extends Backbone.Router
         app.show app.views.filesViews[taskId].render()
 
     tail: (taskId, path='') ->
-        app.views.current?.remove()
         app.views.tailViews = {} if not app.views.tailViews
         if not app.views.tailViews[taskId] or app.views.tailViews[taskId].path isnt path
             app.views.tailViews[taskId] = new TailView taskId: taskId, path: path
@@ -145,7 +136,6 @@ class Router extends Backbone.Router
         app.show app.views.tailViews[taskId].render()
 
     racks: ->
-        app.views.current?.remove()
         if not app.views.racks?
             app.views.racks = new RacksView
             app.views.current = app.views.racks
@@ -155,7 +145,6 @@ class Router extends Backbone.Router
             app.show app.views.racks.refresh()
 
     slaves: ->
-        app.views.current?.remove()
         if not app.views.slaves?
             app.views.slaves = new SlavesView
             app.views.current = app.views.slaves
@@ -165,7 +154,6 @@ class Router extends Backbone.Router
             app.show app.views.slaves.refresh()
 
     webhooks: ->
-        app.views.current?.remove()
         if not app.views.webhooks?
             app.views.webhooks = new WebhooksView
             app.views.current = app.views.webhooks

SingularityUI/app/views/tailer.coffee
@@ -108,6 +108,9 @@ class TailerView extends Backbone.View
             @tailing = null
 
     handleScroll: =>
+        if not @$el.parents('html').length # Our view ain't in the page no mo
+            return
+
         scrollTop = @$el.scrollTop()
         scrollBottom = scrollTop + @$el.height()
         scrollMax = @el.scrollHeight