1ca7255156934cd5709f640052b0f13ac36875e5
Whitney Sorenson <wsorenson@hubspot.com>
2014-04-01 17:28:20 +0100

    unused import

--- SingularityService/src/main/java/com/hubspot/singularity/data/transcoders/SingularityPendingDeployTranscoder.java
+++ SingularityService/src/main/java/com/hubspot/singularity/data/transcoders/SingularityPendingDeployTranscoder.java
@@ -2,7 +2,6 @@ package com.hubspot.singularity.data.transcoders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
-import com.hubspot.singularity.SingularityDeployMarker;
 import com.hubspot.singularity.SingularityPendingDeploy;
 
 public class SingularityPendingDeployTranscoder implements Transcoder<SingularityPendingDeploy> {
