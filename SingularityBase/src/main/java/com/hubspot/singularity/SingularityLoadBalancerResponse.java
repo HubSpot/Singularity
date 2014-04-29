585026625a971edc67c7ef48e2088d8965b49710
tpetr <tpetr@hubspot.com>
2014-04-29 15:39:01 -0400

    add optional message field to SingularityLoadBalancerResponse

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityLoadBalancerResponse.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityLoadBalancerResponse.java
@@ -1,15 +1,17 @@
 package com.hubspot.singularity;
 
-import java.io.IOException;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Optional;
+
+import java.io.IOException;
 
 public class SingularityLoadBalancerResponse extends SingularityJsonObject {
 
   private final String loadBalancerRequestId;
   private final LoadBalancerState loadBalancerState;
+  private final Optional<String> message;
   
   public static SingularityLoadBalancerResponse fromBytes(byte[] bytes, ObjectMapper objectMapper) {
     try {
@@ -20,9 +22,10 @@ public class SingularityLoadBalancerResponse extends SingularityJsonObject {
   }
   
   @JsonCreator
-  public SingularityLoadBalancerResponse(@JsonProperty("loadBalancerRequestId") String loadBalancerRequestId, @JsonProperty("loadBalancerState") LoadBalancerState loadBalancerState) {
+  public SingularityLoadBalancerResponse(@JsonProperty("loadBalancerRequestId") String loadBalancerRequestId, @JsonProperty("loadBalancerState") LoadBalancerState loadBalancerState, @JsonProperty("message") Optional<String> message) {
     this.loadBalancerRequestId = loadBalancerRequestId;
     this.loadBalancerState = loadBalancerState;
+    this.message = message;
   }
 
   public String getLoadBalancerRequestId() {
@@ -32,10 +35,17 @@ public class SingularityLoadBalancerResponse extends SingularityJsonObject {
   public LoadBalancerState getLoadBalancerState() {
     return loadBalancerState;
   }
-  
+
+  public Optional<String> getMessage() {
+    return message;
+  }
+
   @Override
   public String toString() {
-    return "SingularityLoadBalancerResponse [loadBalancerRequestId=" + loadBalancerRequestId + ", loadBalancerState=" + loadBalancerState + "]";
+    return "SingularityLoadBalancerResponse [" +
+        "loadBalancerRequestId='" + loadBalancerRequestId + '\'' +
+        ", loadBalancerState=" + loadBalancerState +
+        ", message=" + message +
+        ']';
   }
-    
 }
