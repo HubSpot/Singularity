0dbf84a40198506cf5fd2f6ed9a5ad9cfbf2f1c3
Whitney Sorenson <wsorenson@hubspot.com>
2014-05-21 11:20:21 -0400

    removing unused

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityRequestParent.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityRequestParent.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Optional;
 
-
 public class SingularityRequestParent extends SingularityJsonObject {
 
   private final SingularityRequest request;
