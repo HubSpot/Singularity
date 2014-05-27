dfcec8c880f18ec3c09cfe1625a80d91e17bf787
tpetr <tpetr@hubspot.com>
2014-05-27 17:16:48 -0400

    mustache --> handlebars, properly escape env vars using bashEscaped helper

---
+++ SingularityExecutor/src/main/java/com/hubspot/singularity/executor/handlebars/BashEscapedHelper.java
@@ -0,0 +1,35 @@
+package com.hubspot.singularity.executor.handlebars;
+
+import java.io.IOException;
+
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+
+public class BashEscapedHelper implements Helper<Object> {
+
+  @Override
+  public CharSequence apply(Object context, Options options) throws IOException {
+    if (context == null) {
+      return "\"\"";
+    }
+
+    final StringBuilder sb = new StringBuilder();
+
+    sb.append('"');
+
+    for (char c : context.toString().toCharArray()) {
+      if (c == '\\' || c == '\"' || c == '$' || c == '`') {
+        sb.append('\\');
+        sb.append(c);
+      } else if (c == '!') {
+        sb.append("\"'!'\"");
+      } else {
+        sb.append(c);
+      }
+    }
+
+    sb.append('"');
+
+    return sb.toString();
+  }
+}
