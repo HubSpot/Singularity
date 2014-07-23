fdfbdec5d7c1649e0d2428974a354daad924fbe0
Whitney Sorenson <wsorenson@hubspot.com>
2014-07-23 15:32:08 -0400

    hack around json changes in 0.19.1

--- SingularityBase/src/main/java/com/hubspot/mesos/json/MesosFrameworkObject.java
+++ SingularityBase/src/main/java/com/hubspot/mesos/json/MesosFrameworkObject.java
@@ -3,6 +3,7 @@ package com.hubspot.mesos.json;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class MesosFrameworkObject {
@@ -10,19 +11,19 @@ public class MesosFrameworkObject {
   private final String name;
   private final String id;
   private final long registeredTime;
-  private final int active;
+  private final Object active;
   private final MesosResourcesObject resources;
   private final List<MesosTaskObject> tasks;
   
   @JsonCreator
-  public MesosFrameworkObject(@JsonProperty("name") String name, @JsonProperty("id") String id, @JsonProperty("registered_time") long registeredTime, @JsonProperty("active") int active, 
+  public MesosFrameworkObject(@JsonProperty("name") String name, @JsonProperty("id") String id, @JsonProperty("registered_time") long registeredTime, @JsonProperty("active") Object active, 
       @JsonProperty("resources") MesosResourcesObject resources, @JsonProperty("tasks") List<MesosTaskObject> tasks) {
     this.name = name;
     this.id = id;
     this.registeredTime = registeredTime;
-    this.active = active;
     this.resources = resources;
     this.tasks = tasks;
+    this.active = active;
   }
   
   public String getId() {
@@ -45,8 +46,19 @@ public class MesosFrameworkObject {
     return registeredTime;
   }
   
-  public int getActive() {
+  public Object getActive() {
     return active;
+  }
+
+  @JsonIgnore
+  public boolean isActive() {
+    // hack to get around json changes in 0.19.1
+    if (active instanceof Integer) {
+      return (Integer) active > 0;
+    } else {
+      return Boolean.parseBoolean(active.toString());
+    }
+    
   }
   
 }
