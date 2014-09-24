f7ed5a6dee5a11aa58166e33d011578f29b00f4e
Whitney Sorenson <wsorenson@hubspot.com>
2014-09-24 11:03:50 -0400

    use HorizonNing

--- SingularityClient/src/main/java/com/hubspot/singularity/client/SingularityClientModule.java
+++ SingularityClient/src/main/java/com/hubspot/singularity/client/SingularityClientModule.java
@@ -1,6 +1,15 @@
 package com.hubspot.singularity.client;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
+import com.hubspot.horizon.HttpClient;
+import com.hubspot.horizon.HttpConfig;
+import com.hubspot.horizon.ning.NingHttpClient;
+import com.hubspot.jackson.datatype.protobuf.ProtobufModule;
 
 public class SingularityClientModule extends AbstractModule {
 
@@ -10,22 +19,18 @@ public class SingularityClientModule extends AbstractModule {
   public static final String CURATOR_NAME = "singularity.curator"; // bind this instead to provide a curator framework to discover singularity
 
   public static final String CONTEXT_PATH = "singularity.context.path";
-  //
-  //  @Override
+
   @Override
   protected void configure() {
-    //    bind(HttpClient.class).annotatedWith(Names.named(HTTP_CLIENT_NAME)).toInstance(new ApacheHttpClient());
+    ObjectMapper objectMapper = new ObjectMapper()
+    .setSerializationInclusion(Include.NON_NULL)
+    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    .registerModule(new GuavaModule())
+    .registerModule(new ProtobufModule());
+
+    HttpClient client = new NingHttpClient(HttpConfig.newBuilder().setObjectMapper(objectMapper).build());
+
+    bind(HttpClient.class).annotatedWith(Names.named(HTTP_CLIENT_NAME)).toInstance(client);
   }
-  //
-  //  @Provides
-  //  @Singleton
-  //  @Named(OBJECT_MAPPER_NAME)
-  //  public ObjectMapper getObjectMapper() {
-  //    return new ObjectMapper()
-  //    .setSerializationInclusion(Include.NON_NULL)
-  //    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-  //    .registerModule(new GuavaModule())
-  //    .registerModule(new ProtobufModule());
-  //  }
 
 }
