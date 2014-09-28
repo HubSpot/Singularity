9e3414955fd8a8cc47c138e50c96099cba0d5070
Whitney Sorenson <wsorenson@gmail.com>
2014-09-28 10:55:56 -0400

    add logging to reconcile

--- SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularityTaskReconciliation.java
+++ SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularityTaskReconciliation.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.mesos.Protos.Status;
 import org.apache.mesos.Protos.TaskStatus;
 import org.apache.mesos.SchedulerDriver;
 import org.slf4j.Logger;
@@ -11,6 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Sets;
 import com.google.inject.Inject;
+import com.hubspot.mesos.JavaUtils;
 import com.hubspot.singularity.SingularityTaskId;
 import com.hubspot.singularity.data.TaskManager;
 
@@ -26,6 +28,8 @@ public class SingularityTaskReconciliation {
   }
 
   public void reconcileTasks(SchedulerDriver driver) {
+    final long start = System.currentTimeMillis();
+
     Set<SingularityTaskId> taskIds = Sets.newHashSet(taskManager.getActiveTaskIds());
     List<TaskStatus> taskStatuses = taskManager.getLastActiveTaskStatuses();
 
@@ -40,7 +44,11 @@ public class SingularityTaskReconciliation {
       }
     }
 
-    driver.reconcileTasks(taskStatuses);
+    LOG.info("Requesting reconciliation for {} taskStatuses", taskStatuses.size());
+
+    Status status = driver.reconcileTasks(taskStatuses);
+
+    LOG.info("Requested reconciliation of {} taskStatuses with driver status {} in {}", taskStatuses.size(), status, JavaUtils.duration(start));
   }
 
 }
