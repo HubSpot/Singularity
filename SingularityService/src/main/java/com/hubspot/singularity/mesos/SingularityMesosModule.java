0e4300852b14b4c5a6b2d7401e4bb8d6bdb106aa
Henning Schmiedehausen <henning@groupon.com>
2014-10-14 16:57:10 -0700

    Split up guice modules
    Smaller, package specific modules for testing.
    
    Conflicts:
    	SingularityService/src/main/java/com/hubspot/singularity/SingularityServiceModule.java

---
+++ SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityMesosModule.java
@@ -0,0 +1,17 @@
+package com.hubspot.singularity.mesos;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+
+public class SingularityMesosModule extends AbstractModule {
+  @Override
+  public void configure() {
+    bind(SingularityDriver.class).in(Scopes.SINGLETON);
+    bind(SingularityLogSupport.class).in(Scopes.SINGLETON);
+    bind(SingularityMesosScheduler.class).in(Scopes.SINGLETON);
+    bind(SingularityMesosSchedulerDelegator.class).in(Scopes.SINGLETON);
+    bind(SingularityMesosTaskBuilder.class).in(Scopes.SINGLETON);
+    bind(SingularitySlaveAndRackManager.class).in(Scopes.SINGLETON);
+    bind(SingularityStartup.class).in(Scopes.SINGLETON);
+  }
+}
