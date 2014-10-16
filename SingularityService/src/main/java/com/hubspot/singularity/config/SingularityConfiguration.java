ceffdb24df43bc2080cec79b9f01a5d61bd37089
Whitney Sorenson <wsorenson@hubspot.com>
2014-10-16 13:45:17 -0400

    add TASK_FINISHED_NON_SCHEDULED_REQUEST
    new type of email configuration for a when a task finishes, but isn’t
    part of a scheduled request. Helps look for tasks which are exiting
    with the wrong status codes.

--- SingularityService/src/main/java/com/hubspot/singularity/config/SingularityConfiguration.java
+++ SingularityService/src/main/java/com/hubspot/singularity/config/SingularityConfiguration.java
@@ -536,7 +536,6 @@ public class SingularityConfiguration extends Configuration {
     this.zooKeeperConfiguration = zooKeeperConfiguration;
   }
 
-
   public SlavePlacement getDefaultSlavePlacement() {
     return defaultSlavePlacement;
   }
