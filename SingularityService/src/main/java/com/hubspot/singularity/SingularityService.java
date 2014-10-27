3f98cec73d0cfafb5c84da605208b701710f9c34
Henning Schmiedehausen <henning@groupon.com>
2014-10-27 13:21:40 -0700

    Make Singularity embeddable into a bigger service.
    Adds also an example on how to do it with a skeleton class.
    
    There is probably some more documentation required; I will write some once this has really gelled.

--- SingularityService/src/main/java/com/hubspot/singularity/SingularityService.java
+++ SingularityService/src/main/java/com/hubspot/singularity/SingularityService.java
@@ -1,9 +1,11 @@
 package com.hubspot.singularity;
 
 
-import com.hubspot.singularity.bundles.AcceptLanguageFilterBundle;
-import com.hubspot.singularity.bundles.CorsBundle;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import io.dropwizard.Application;
+import io.dropwizard.Bundle;
+import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.migrations.MigrationsBundle;
@@ -14,18 +16,28 @@ import io.dropwizard.views.ViewBundle;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Module;
 import com.hubspot.jackson.datatype.protobuf.ProtobufModule;
+import com.hubspot.singularity.bundles.AcceptLanguageFilterBundle;
+import com.hubspot.singularity.bundles.CorsBundle;
 import com.hubspot.singularity.config.SingularityConfiguration;
 import com.hubspot.singularity.guice.GuiceBundle;
 
-public class SingularityService extends Application<SingularityConfiguration> {
+public class SingularityService<T extends SingularityConfiguration> extends Application<T> {
 
   public static final String API_BASE_PATH = "/api";
 
   @Override
-  public void initialize(final Bootstrap<SingularityConfiguration> bootstrap) {
+  public void initialize(final Bootstrap<T> bootstrap) {
+
+    final Iterable<? extends Module> additionalModules = checkNotNull(getGuiceModules(bootstrap), "getGuiceModules() returned null");
+    final Iterable<? extends Bundle> additionalBundles = checkNotNull(getDropwizardBundles(bootstrap), "getDropwizardBundles() returned null");
+    final Iterable<? extends ConfiguredBundle<T>> additionalConfiguredBundles = checkNotNull(getDropwizardConfiguredBundles(bootstrap), "getDropwizardConfiguredBundles() returned null");
+
     final GuiceBundle<SingularityConfiguration> guiceBundle = GuiceBundle.defaultBuilder(SingularityConfiguration.class)
         .modules(new SingularityServiceModule())
+        .modules(additionalModules)
         .build();
     bootstrap.addBundle(guiceBundle);
 
@@ -41,6 +53,14 @@ public class SingularityService extends Application<SingularityConfiguration> {
       }
     });
 
+    for (Bundle bundle : additionalBundles) {
+      bootstrap.addBundle(bundle);
+    }
+
+    for (ConfiguredBundle<T> configuredBundle : additionalConfiguredBundles) {
+      bootstrap.addBundle(configuredBundle);
+    }
+
     bootstrap.getObjectMapper().registerModule(new ProtobufModule());
     bootstrap.getObjectMapper().registerModule(new GuavaModule());
     bootstrap.getObjectMapper().setSerializationInclusion(Include.NON_NULL);
@@ -50,13 +70,33 @@ public class SingularityService extends Application<SingularityConfiguration> {
   @Override
   public void run(final SingularityConfiguration configuration, final Environment environment) throws Exception {}
 
-  public static void main(final String[] args) {
+  /**
+   * Guice modules used in addition to the modules required by Singularity. This is an extension point when embedding
+   * Singularity into a custom service.
+   */
+  public Iterable<? extends Module> getGuiceModules(Bootstrap<T> bootstrap) {
+    return ImmutableList.of();
+  }
+
+  /**
+   * Dropwizard bundles used in addition to the bundles required by Singularity. This is an extension point when embedding
+   * Singularity into a custom service.
+   */
+  public Iterable<? extends Bundle> getDropwizardBundles(Bootstrap<T> bootstrap) {
+    return ImmutableList.of();
+  }
+
+  public Iterable<? extends ConfiguredBundle<T>> getDropwizardConfiguredBundles(Bootstrap<T> bootstrap) {
+    return ImmutableList.of();
+  }
+
+
+  public static void main(final String[] args) throws Exception {
     try {
-      new SingularityService().run(args);
+      new SingularityService<SingularityConfiguration>().run(args);
     } catch (final Throwable t) {
       t.printStackTrace();
       System.exit(1);
     }
   }
-
 }
