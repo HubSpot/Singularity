114e58bf57e4063e11b8223f901f6ee51a9dc342
Stephen Salinas <ssalinas@hubspot.com>
2014-12-11 09:48:56 -0500

    all to stderr except grep output

--- scripts/logfetch/grep.py
+++ scripts/logfetch/grep.py
@@ -2,17 +2,17 @@ import os
 import sys
 from termcolor import colored
 
-GREP_COMMAND_FORMAT = 'xargs -n {0} grep \'{1}\' < {2}'
+GREP_COMMAND_FORMAT = 'xargs -n {0} grep --color=always \'{1}\' < {2}'
 
 def grep_files(args, all_logs):
   if args.grep:
     greplist_filename = '{0}/.greplist'.format(args.dest)
     create_greplist(args, all_logs, greplist_filename)
     grep_command = GREP_COMMAND_FORMAT.format(len(all_logs), args.grep, greplist_filename)
-    sys.stdout.write(colored('Running "{0}" this might take a minute'.format(grep_command), 'blue') + '\n')
+    sys.stderr.write(colored('Running "{0}" this might take a minute'.format(grep_command), 'blue') + '\n')
     sys.stdout.write(os.popen(grep_command).read() + '\n')
     remove_greplist(greplist_filename)
-    sys.stdout.write(colored('Finished grep, exiting', 'green') + '\n')
+    sys.stderr.write(colored('Finished grep, exiting', 'green') + '\n')
 
 def create_greplist(args, all_logs, greplist_filename):
   greplist_file = open(greplist_filename, 'wb')
