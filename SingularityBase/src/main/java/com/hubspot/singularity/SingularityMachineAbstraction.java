4b9ffc42e9e06758413f1b237148de7f3f3ec961
Whitney Sorenson <wsorenson@hubspot.com>
2015-01-13 16:14:20 -0500

    fix warning

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityMachineAbstraction.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityMachineAbstraction.java
@@ -31,19 +31,24 @@ public abstract class SingularityMachineAbstraction<T extends SingularityMachine
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj)
+    if (this == obj) {
       return true;
-    if (obj == null)
+    }
+    if (obj == null) {
       return false;
-    if (getClass() != obj.getClass())
+    }
+    if (getClass() != obj.getClass()) {
       return false;
+    }
     @SuppressWarnings("unchecked")
     SingularityMachineAbstraction<T> other = (SingularityMachineAbstraction<T>) obj;
     if (id == null) {
-      if (other.id != null)
+      if (other.id != null) {
         return false;
-    } else if (!id.equals(other.id))
+      }
+    } else if (!id.equals(other.id)) {
       return false;
+    }
     return true;
   }
 
