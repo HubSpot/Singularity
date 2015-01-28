d6e8120ca5e310105faac8be619e1614c67d39fb
Danny Wolf <dwolf@hubspot.com>
2015-01-28 14:55:44 -0500

    Add docs for LogrotateTemplateContext.

--- SingularityExecutor/src/main/java/com/hubspot/singularity/executor/models/LogrotateTemplateContext.java
+++ SingularityExecutor/src/main/java/com/hubspot/singularity/executor/models/LogrotateTemplateContext.java
@@ -3,11 +3,19 @@ package com.hubspot.singularity.executor.models;
 import com.hubspot.singularity.executor.config.SingularityExecutorConfiguration;
 import com.hubspot.singularity.executor.task.SingularityExecutorTaskDefinition;
 
+/**
+ * Handlebars context for generating logrotate.conf files.
+ * Check `man logrotate` for more information.
+ */
 public class LogrotateTemplateContext {
 
   private final SingularityExecutorTaskDefinition taskDefinition;
   private final SingularityExecutorConfiguration configuration;
 
+  /**
+   * @param configuration configuration to pull from.
+   * @param taskDefinition information about the task we're writing the logrotate configs for.
+   */
   public LogrotateTemplateContext(SingularityExecutorConfiguration configuration, SingularityExecutorTaskDefinition taskDefinition) {
     this.configuration = configuration;
     this.taskDefinition = taskDefinition;
@@ -17,18 +25,35 @@ public class LogrotateTemplateContext {
     return configuration.getLogrotateDateformat();
   }
 
+  /**
+   * Log files are rotated count times before being removed.
+   * @return String count.
+   */
   public String getRotateCount() {
     return configuration.getLogrotateCount();
   }
 
+  /**
+   * Remove rotated logs older than $count days.
+   * The age is only checked if the logfile is to be rotated.
+   * @return String days.
+   */
   public String getMaxageDays() {
     return configuration.getLogrotateMaxageDays();
   }
 
+  /**
+   * Logs are moved into $directory for rotation.
+   * @return String directory.
+   */
   public String getRotateDirectory() {
     return configuration.getLogrotateToDirectory();
   }
 
+  /**
+   * Extra files for logrotate to rotate.
+   * @return String[] filenames to rotate.
+   */
   public String[] getExtrasFiles() {
     final String[] original = configuration.getLogrotateExtrasFiles();
     final String[] transformed = new String[original.length];
@@ -40,10 +65,20 @@ public class LogrotateTemplateContext {
     return transformed;
   }
 
+  /**
+   * dateformat for extra files.
+   * Only %Y %m %d and %s specifiers are allowed.
+   * @return String dateformat (e.g. "-%Y%m%d%s").
+   */
   public String getExtrasDateformat() {
     return configuration.getLogrotateExtrasDateformat();
   }
 
+  /**
+   * Default log to logrotate, defaults to service.log.
+   * This if this log doesn't exist, logrotate will return an error message.
+   * @return String filename to rotate.
+   */
   public String getLogfile() {
     return taskDefinition.getServiceLogOut();
   }
