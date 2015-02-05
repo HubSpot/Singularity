fb86e9a22e8bf356f539d359a080c295a4e6b798
Stephen Salinas <ssalinas@hubspot.com>
2015-02-05 17:06:27 -0500

    better finding of live logs in base directory

--- scripts/logfetch/live_logs.py
+++ scripts/logfetch/live_logs.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import fnmatch
 import grequests
 from glob import glob
 from termcolor import colored
@@ -22,24 +23,19 @@ def download_live_logs(args):
   for task in tasks:
     metadata = files_json(args, task)
     uri = DOWNLOAD_FILE_FORMAT.format(metadata['slaveHostname'])
-    service_log = '{0}-service.log'.format(task)
-    tail_log = '{0}-tail_of_finished_service.log'.format(task)
-    if (args.logtype and logfetch_base.log_matches(service_log, args.logtype)) or not args.logtype:
-      async_requests.append(
-        grequests.AsyncRequest('GET',uri ,
-          callback=generate_callback(uri, args.dest, service_log, args.chunk_size),
-          params={'path' : '{0}/{1}/service.log'.format(metadata['fullPathToRoot'], metadata['currentDirectory'])}
+    for log_file in base_directory_files(args, task):
+      logfile_name = '{0}-{1}'.format(task, log_file)
+      if (args.logtype and logfetch_base.log_matches(log_file, args.logtype)) or not args.logtype:
+        async_requests.append(
+          grequests.AsyncRequest('GET',uri ,
+            callback=generate_callback(uri, args.dest, logfile_name, args.chunk_size),
+            params={'path' : '{0}/{1}/logs/{1}'.format(metadata['fullPathToRoot'], metadata['currentDirectory'], log_file)}
+          )
         )
-      )
-      all_logs.append('{0}/{1}'.format(args.dest, service_log))
-    if (args.logtype and logfetch_base.log_matches(tail_log, args.logtype)) or not args.logtype:
-      async_requests.append(
-        grequests.AsyncRequest('GET',uri ,
-          callback=generate_callback(uri, args.dest, tail_log, args.chunk_size),
-          params={'path' : '{0}/{1}/tail_of_finished_service.log'.format(metadata['fullPathToRoot'], metadata['currentDirectory'])}
-        )
-      )
-      all_logs.append('{0}/{1}'.format(args.dest, tail_log))
+        if logfile_name.endswith('.gz'):
+          zipped_files.append('{0}/{1}'.format(args.dest, logfile_name))
+        all_logs.append('{0}/{1}'.format(args.dest, logfile_name.replace('.gz', '.log')))
+
     for log_file in logs_folder_files(args, task):
       logfile_name = '{0}-{1}'.format(task, log_file)
       if (args.logtype and logfetch_base.log_matches(log_file, args.logtype)) or not args.logtype:
@@ -75,3 +71,19 @@ def logs_folder_files(args, task):
     return [f['name'] for f in files if logfetch_base.is_in_date_range(args, f['mtime'])]
   else:
     return [f['path'].rsplit('/')[-1] for f in files_json if logfetch_base.is_in_date_range(args, f['mtime'])]
+
+def base_directory_files(args, task):
+  uri = BROWSE_FOLDER_FORMAT.format(logfetch_base.base_uri(args), task)
+  files_json = get_json_response(uri)
+  if 'files' in files_json:
+    files = files_json['files']
+    return [f['name'] for f in files if valid_logfile(args, f)]
+  else:
+    return [f['path'].rsplit('/')[-1] for f in files_json if valid_logfile(args, f)]
+
+def valid_logfile(args, fileData):
+    is_in_range = logfetch_base.is_in_date_range(args, fileData['mtime'])
+    not_a_directory = not fileData['mode'].startswith('d')
+    is_a_logfile = fnmatch.fnmatch(fileData['name'], '*.log') or fnmatch.fnmatch(fileData['name'], '*.out') or fnmatch.fnmatch(fileData['name'], '*.err')
+    return is_in_range and not_a_directory and is_a_logfile
+
