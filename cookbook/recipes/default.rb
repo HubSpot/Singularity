ea709265f048fc1770019eb2bc22f34ecebd8e68
Eric Herot <eric.github@herot.com>
2015-02-24 16:32:52 -0500

    Use the Mesos cookbook to install mesos instead of doing it ourselves

--- cookbook/recipes/default.rb
+++ cookbook/recipes/default.rb
@@ -1,6 +1,10 @@
-include_recipe "singularity::hosts"
-include_recipe "singularity::mesos"
-include_recipe "singularity::mesos_master"
-include_recipe "singularity::mesos_slave"
+user node[:singularity][:user] do
+  supports(manage_home: true)
+  home "/home/#{node[:singularity][:user]}"
+end
+
+include_recipe 'mesos'
+include_recipe 'mesos::master'
+include_recipe 'mesos::slave'
 include_recipe "singularity::mysql"
 include_recipe "singularity::java"
