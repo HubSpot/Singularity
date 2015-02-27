05cbdb1e3c9730a6cc7dff7c8c58361e6ea1056b
Eric Herot <eric.github@herot.com>
2015-02-27 11:48:01 -0500

    Do ServerSpec tests of full Singularity startup

--- cookbook/Berksfile
+++ cookbook/Berksfile
@@ -1,3 +1,5 @@
 source 'https://supermarket.getchef.com'
 
 metadata
+
+cookbook 'zookeeper'
--- cookbook/test/integration/default/serverspec/default_spec.rb
+++ cookbook/test/integration/default/serverspec/default_spec.rb
@@ -7,3 +7,23 @@ describe 'Filesystem' do
     it { should be_owned_by 'singularity' }
   end
 end
+
+describe 'Services' do
+  # 2181 - Zookeeper
+  # 3306 - MySQL
+  # 5050 - Mesos Master
+  # 5051 - Mesos Slave
+  # 7099 - Singularity
+  [3306, 5050, 7099].each do |p|
+    describe port(p) do
+      it { should be_listening.with('tcp') }
+    end
+  end
+  describe port(2181) do
+    it { should be_listening.with('tcp6') }
+  end
+  describe file('/etc/mesos-slave/attributes/rackid') do
+    it { is_expected.to be_file }
+    it { is_expected.to contain(/^us-east-1b$/) }
+  end
+end
