b93b1696f395af9f57dea453d11c02df92e18936
Eric Herot <eric.github@herot.com>
2015-03-12 11:26:52 -0400

    Include default recipe from docker cookbook instead of local docker recipe

--- cookbook/recipes/default.rb
+++ cookbook/recipes/default.rb
@@ -4,3 +4,4 @@ include_recipe "singularity::mesos_master"
 include_recipe "singularity::mesos_slave"
 include_recipe "singularity::mysql"
 include_recipe "singularity::java"
+include_recipe 'docker' if node[:docker][:enabled]
