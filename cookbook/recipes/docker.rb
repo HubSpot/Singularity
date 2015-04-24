da67024a1af38412908406191cde741a580dd4c0
Eric Herot <eric.github@herot.com>
2015-04-24 12:56:19 -0400

    Make sure apt-get update runs before attempting to install docker

--- cookbook/recipes/docker.rb
+++ cookbook/recipes/docker.rb
@@ -1,6 +1,9 @@
 docker_enabled = node[:docker][:enabled]
 docker_package_version = node[:docker][:package_version]
 
+node.set['apt']['compile_time_update'] = true
+include_recipe 'apt' if node.platform_family == 'debian'
+
 if docker_enabled
   apt_package "docker.io" do
     action :install
