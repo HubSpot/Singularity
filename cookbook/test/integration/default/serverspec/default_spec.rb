c4389fa1a543b47fa070e3f7950e1329facea60e
Eric Herot <eric.github@herot.com>
2015-04-27 13:52:06 -0400

    Test for correctly set baragon URL in singularity.yaml

--- cookbook/test/integration/default/serverspec/default_spec.rb
+++ cookbook/test/integration/default/serverspec/default_spec.rb
@@ -7,3 +7,19 @@ describe 'Filesystem' do
     it { should be_owned_by 'singularity' }
   end
 end
+
+context 'Configuration' do
+  describe file('/etc/singularity/singularity.yaml') do
+    it { is_expected.to be_file }
+    describe '#content' do
+      subject { super().content }
+      it do
+        is_expected.to(
+          match(
+            %r{^loadBalancerUri: http:\/\/localhost:8088/baragon/v2/request}
+          )
+        )
+      end
+    end
+  end
+end
