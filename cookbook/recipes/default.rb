9ecbfc999ae0a452a538b4c84641f73fa5fc84d4
Eric Herot <eric.github@herot.com>
2015-04-27 11:49:34 -0400

    Move some resources around to make package and source into discrete recipes

--- cookbook/recipes/default.rb
+++ cookbook/recipes/default.rb
@@ -2,6 +2,14 @@ include_recipe "singularity::hosts"
 include_recipe "singularity::mesos"
 include_recipe "singularity::mesos_master"
 include_recipe "singularity::mesos_slave"
+include_recipe "singularity::mysql"
+include_recipe "singularity::java"
+
+[node[:singularity][:conf_dir],
+ node[:singularity][:log_dir],
+ node[:singularity][:home],
+ "#{node[:singularity][:home]}/mysql",
+ "#{node[:singularity][:home]}/bin"].each { |cur_dir| directory cur_dir }
 
 case node['singularity']['install_type']
 when 'package'
@@ -12,6 +20,4 @@ else
   fail "Invalid install type: #{node['singularity']['install_type']}"
 end
 
-include_recipe "singularity::mysql"
-include_recipe "singularity::java"
 include_recipe 'singularity::logrotate'
