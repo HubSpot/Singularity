7153127d4f7d3add1ee9a630f333c30ec2f84506
Eric Herot <eric.github@herot.com>
2015-04-27 12:17:25 -0400

    Fix some naming and specifier details of the maven resource to make it place the correct jar file

--- cookbook/recipes/package.rb
+++ cookbook/recipes/package.rb
@@ -1,5 +1,8 @@
-maven 'singularity' do
+include_recipe 'maven'
+
+maven 'SingularityService' do
   group_id  'com.hubspot'
+  classifier 'shaded'
   version   node['singularity']['version']
-  dest      node['singularity']['home']
+  dest      "#{node[:singularity][:home]}/bin/"
 end
