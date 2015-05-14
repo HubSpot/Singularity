b49785f34317fb143696a6980e03fac121e71ab6
Jeff Byrnes <jeff@evertrue.com>
2015-05-14 14:27:48 -0400

    Remove cookbook, referring folks to new repository.

--- cookbook/recipes/package.rb
+++
@@ -1,8 +0,0 @@
-include_recipe 'maven'
-
-maven 'SingularityService' do
-  group_id  'com.hubspot'
-  classifier 'shaded'
-  version   node['singularity']['version']
-  dest      "#{node[:singularity][:home]}/bin/"
-end
