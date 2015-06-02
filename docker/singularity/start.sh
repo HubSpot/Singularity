d5299de15836a1f1296984bed8a63ba46ac5cede
Stephen Salinas <ssalinas@hubspot.com>
2015-06-02 17:07:09 -0400

    updated docker-compose

--- docker/singularity/start.sh
+++ docker/singularity/start.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-env PATH=/usr/local/singularity/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin
-env MESOS_HOME=/usr/local
-env MESOS_NATIVE_LIBRARY=/usr/local/lib/libmesos.so
-env PORT=7092
+PATH=/usr/local/singularity/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin
+MESOS_HOME=/usr/local
+MESOS_NATIVE_LIBRARY=/usr/local/lib/libmesos.so
+PORT=7092
+DEFAULT_URI_BASE="http://localhost:${SINGULARITY_PORT:=7099}/singularity"
 
-exec java \
-  -Xmx512m \
-  -Djava.net.preferIPv4Stack=true \
-  -Ddw.mesos.master=$SINGULARITY_MASTER \
-  -Ddw.zookeeper.quorum=$SINGULARITY_ZK \
-  -Ddw.ui.baseUrl="http://$SINGULARITY_HOSTNAME:7099/singularity" \
-  -jar /etc/singularity/singularity.jar \
-  server /etc/singularity/singularity.yaml
+
+[[ ! ${SINGULARITY_PORT:-} ]] || args+=( -Ddw.server.connector.port="$SINGULARITY_PORT" )
+[[ ! ${LOAD_BALANCER_URI:-} ]] || args+=( -Ddw.loadBalancerUri="$LOAD_BALANCER_URI")
+
+args+=( -Xmx512m )
+args+=( -Djava.net.preferIPv4Stack=true )
+args+=( -Ddw.mesos.master="${SINGULARITY_MASTER:=zk://localhost:2181/mesos}" )
+args+=( -Ddw.zookeeper.quorum="${SINGULARITY_ZK:=localhost:2181}" )
+args+=( -Ddw.ui.baseUrl="${SINGULARITY_URI_BASE:=$DEFAULT_URI_BASE}" )
+
+
+exec java "${args[@]}" -jar /etc/singularity/singularity.jar server /etc/singularity/singularity.yaml
