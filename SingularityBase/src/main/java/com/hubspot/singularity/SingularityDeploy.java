e4b0d61322a4e8c70e37bf696e9c5faa8aabe950
Stephen Salinas <ssalinas@hubspot.com>
2015-06-16 11:16:02 -0400

    updates to healthcheck protocol

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityDeploy.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityDeploy.java
@@ -85,7 +85,7 @@ public class SingularityDeploy {
       @JsonProperty("considerHealthyAfterRunningForSeconds") Optional<Long> considerHealthyAfterRunningForSeconds,
       @JsonProperty("loadBalancerOptions") Optional<Map<String, Object>> loadBalancerOptions,
       @JsonProperty("skipHealthchecksOnDeploy") Optional<Boolean> skipHealthchecksOnDeploy,
-      @JsonProperty("healthCheckScheme") Optional<HealthcheckProtocol> healthcheckProtocol) {
+      @JsonProperty("healthCheckProtocol") Optional<HealthcheckProtocol> healthcheckProtocol) {
     this.requestId = requestId;
 
     this.command = command;
@@ -246,7 +246,7 @@ public class SingularityDeploy {
     return healthcheckUri;
   }
 
-  @ApiModelProperty(required=false, value="Healthcheck scheme")
+  @ApiModelProperty(required=false, value="Healthcheck protocol")
   public Optional<HealthcheckProtocol> getHealthcheckProtocol() {
     return healthcheckProtocol;
   }
