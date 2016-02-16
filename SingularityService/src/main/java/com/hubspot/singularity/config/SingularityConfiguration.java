2e485f7114b2c4919c99229a4870b163ac8f0210
Stephen Salinas <ssalinas@hubspot.com>
2016-02-16 15:18:44 -0500

    fall back to configurable singularity-wide field for depoyMaxRetries

--- SingularityService/src/main/java/com/hubspot/singularity/config/SingularityConfiguration.java
+++ SingularityService/src/main/java/com/hubspot/singularity/config/SingularityConfiguration.java
@@ -83,6 +83,8 @@ public class SingularityConfiguration extends Configuration {
 
   private int defaultDeployStepWaitTimeMs = 0;
 
+  private int defaultDeployMaxTaskRetries = 0;
+
   private long deleteDeploysFromZkWhenNoDatabaseAfterHours = TimeUnit.DAYS.toHours(14);
 
   private long deleteDeadSlavesAfterHours = TimeUnit.DAYS.toHours(7);
@@ -374,6 +376,14 @@ public class SingularityConfiguration extends Configuration {
 
   public void setDefaultDeployStepWaitTimeMs(int defaultDeployStepWaitTimeMs) {
     this.defaultDeployStepWaitTimeMs = defaultDeployStepWaitTimeMs;
+  }
+
+  public int getDefaultDeployMaxTaskRetries() {
+    return defaultDeployMaxTaskRetries;
+  }
+
+  public void setDefaultDeployMaxTaskRetries(int defaultDeployMaxTaskRetries) {
+    this.defaultDeployMaxTaskRetries = defaultDeployMaxTaskRetries;
   }
 
   public long getDeleteDeploysFromZkWhenNoDatabaseAfterHours() {
