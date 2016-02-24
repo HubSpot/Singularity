a8b823e6569d3738423121e2ab3d02d2d16c5af4
tpetr <tpetr@hubspot.com>
2016-02-24 11:05:27 -0500

    better way to brunch

--- SingularityUI/gulpfile.js
+++ SingularityUI/gulpfile.js
@@ -1,18 +1,39 @@
 var gulp = require('gulp');
 var path = require('path');
-var brunch = require('brunch');
 var del = require('del');
+var child_process = require('child_process');
+
+var dest = path.resolve(__dirname, '../SingularityService/target/generated-resources/assets');
 
 gulp.task("clean", function() {
   return del([
-    path.resolve(__dirname, '../SingularityService/target/generated-resources/assets/static/**'),
-    path.resolve(__dirname, '../SingularityService/target/generated-resources/assets/index.html')], {force: true});
+    path.resolve(dest, 'static/**'),
+    path.resolve(dest, 'index.html')], {force: true});
 });
 
 gulp.task("build", function(cb) {
-  brunch.build({}, function () {
-    cb();
+  var brunch = child_process.execFile('node_modules/brunch/bin/brunch', ['build', '--production']);
+
+  var hasStderrOutput = false;
+
+  brunch.stdout.pipe(process.stdout);
+  brunch.stderr.pipe(process.stderr);
+
+  brunch.stderr.on('data', function () {
+    hasStderrOutput = true;
+  });
+
+  brunch.on('error', cb);
+
+  brunch.on('exit', function (code) {
+    if (hasStderrOutput) {
+      cb(new Error("Brunch build failed"));
+    } else if (code != 0) {
+      cb(new Error("Brunch exited with code " + code));
+    } else {
+      cb();
+    }
   });
 });
 
-gulp.task("default", ["clean", "build"])
+gulp.task("default", ["clean", "build"]);
