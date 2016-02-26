12c61acf36e014c10f2e89ccefeb96cab3b319bc
Stephen Salinas <ssalinas@hubspot.com>
2016-02-26 14:17:53 -0500

    fix all the indenting

--- scripts/logfetch/grep.py
+++ scripts/logfetch/grep.py
@@ -6,33 +6,33 @@ from termcolor import colored
 DEFAULT_GREP_COMMAND = 'grep --color=always \'{0}\''
 
 def grep_files(args, all_logs):
-  log('\n', args, False)
-  if args.grep:
-    if all_logs:
-      all_logs.sort()
-      grep_cmd = grep_command(args)
-      log(colored('\nRunning grep command ({0})\n'.format(grep_cmd), 'cyan'), args, False)
-      for filename in all_logs:
-        log('=> ' + colored(filename, 'cyan') + '\n', args, True)
-        content = subprocess.Popen(['cat', filename], stdout=subprocess.PIPE)
-        if filename.endswith('.gz'):
-          zcat = subprocess.Popen('zcat', stdin=content.stdout, stdout=subprocess.PIPE)
-          grep = subprocess.Popen(grep_cmd, stdin=zcat.stdout, shell=True)
+    log('\n', args, False)
+    if args.grep:
+        if all_logs:
+            all_logs.sort()
+            grep_cmd = grep_command(args)
+            log(colored('Running grep command ({0})\n'.format(grep_cmd), 'cyan'), args, False)
+            for filename in all_logs:
+                log('=> ' + colored(filename, 'cyan') + '\n', args, True)
+                content = subprocess.Popen(['cat', filename], stdout=subprocess.PIPE)
+                if filename.endswith('.gz'):
+                    zcat = subprocess.Popen('zcat', stdin=content.stdout, stdout=subprocess.PIPE)
+                    grep = subprocess.Popen(grep_cmd, stdin=zcat.stdout, shell=True)
+                else:
+                    grep = subprocess.Popen(grep_cmd, stdin=content.stdout, shell=True)
+                grep.communicate()
+            log(colored('Finished grep, exiting', 'green') + '\n', args, False)
         else:
-          grep = subprocess.Popen(grep_cmd, stdin=content.stdout, shell=True)
-        grep.communicate()
-      log(colored('Finished grep, exiting', 'green') + '\n', args, False)
-    else:
-      sys.stderr.write(colored('No logs found\n', 'magenta'))
+            sys.stderr.write(colored('No logs found\n', 'magenta'))
 
 def grep_command(args):
-  if 'grep' in args.grep:
-    return args.grep
-  else:
-    return DEFAULT_GREP_COMMAND.format(args.grep)
+    if 'grep' in args.grep:
+        return args.grep
+    else:
+        return DEFAULT_GREP_COMMAND.format(args.grep)
 
 def cat_command(filename):
-  if filename.endswith('.gz'):
-    return 'zcat {0}'.format(filename)
-  else:
-    return 'cat {0}'.format(filename)
+    if filename.endswith('.gz'):
+        return 'zcat {0}'.format(filename)
+    else:
+        return 'cat {0}'.format(filename)
--- scripts/logfetch/s3_logs.py
+++ scripts/logfetch/s3_logs.py
@@ -16,74 +16,74 @@ progress = 0
 goal = 0
 
 def download_s3_logs(args):
-  if not args.silent:
-    sys.stderr.write(colored('Checking for S3 log files', 'cyan') + '\n')
-  callbacks.progress = 0
-  logs = logs_for_all_requests(args)
-  async_requests = []
-  all_logs = []
-  for log_file in logs:
-    filename = log_file['key'].rsplit("/", 1)[1]
-    if logfetch_base.is_in_date_range(args, int(str(log_file['lastModified'])[0:-3])):
-      if not args.logtype or log_matches(args, filename):
-        logfetch_base.log(colored('Including log {0}'.format(filename), 'blue') + '\n', args, True)
-        if not already_downloaded(args.dest, filename):
-          async_requests.append(
-            grequests.AsyncRequest('GET', log_file['getUrl'], callback=callbacks.generate_callback(log_file['getUrl'], args.dest, filename, args.chunk_size, args.verbose, args.silent), headers=args.headers)
-          )
+    if not args.silent:
+        sys.stderr.write(colored('Checking for S3 log files', 'cyan') + '\n')
+    callbacks.progress = 0
+    logs = logs_for_all_requests(args)
+    async_requests = []
+    all_logs = []
+    for log_file in logs:
+        filename = log_file['key'].rsplit("/", 1)[1]
+        if logfetch_base.is_in_date_range(args, int(str(log_file['lastModified'])[0:-3])):
+            if not args.logtype or log_matches(args, filename):
+                logfetch_base.log(colored('Including log {0}'.format(filename), 'blue') + '\n', args, True)
+                if not already_downloaded(args.dest, filename):
+                    async_requests.append(
+                        grequests.AsyncRequest('GET', log_file['getUrl'], callback=callbacks.generate_callback(log_file['getUrl'], args.dest, filename, args.chunk_size, args.verbose, args.silent), headers=args.headers)
+                    )
+                else:
+                    logfetch_base.log(colored('Log already downloaded {0}'.format(filename), 'blue') + '\n', args, True)
+                all_logs.append('{0}/{1}'.format(args.dest, filename))
+            else:
+                logfetch_base.log(colored('Excluding {0} log does not match logtype argument {1}'.format(filename, args.logtype), 'magenta') + '\n', args, True)
         else:
-          logfetch_base.log(colored('Log already downloaded {0}'.format(filename), 'blue') + '\n', args, True)
-        all_logs.append('{0}/{1}'.format(args.dest, filename))
-      else:
-        logfetch_base.log(colored('Excluding {0} log does not match logtype argument {1}'.format(filename, args.logtype), 'magenta') + '\n', args, True)
+            logfetch_base.log(colored('Excluding {0}, not in date range'.format(filename), 'magenta') + '\n', args, True)
+    if async_requests:
+        logfetch_base.log(colored('Starting {0} S3 Downloads with {1} parallel fetches\n'.format(len(async_requests), args.num_parallel_fetches), 'cyan'), args, False)
+        callbacks.goal = len(async_requests)
+        grequests.map(async_requests, stream=True, size=args.num_parallel_fetches)
     else:
-      logfetch_base.log(colored('Excluding {0}, not in date range'.format(filename), 'magenta') + '\n', args, True)
-  if async_requests:
-    logfetch_base.log(colored('Starting {0} S3 Downloads with {1} parallel fetches\n'.format(len(async_requests), args.num_parallel_fetches), 'cyan'), args, False)
-    callbacks.goal = len(async_requests)
-    grequests.map(async_requests, stream=True, size=args.num_parallel_fetches)
-  else:
-    logfetch_base.log(colored('No S3 logs to download\n', 'cyan'), args, False)
-  logfetch_base.log(colored('All S3 logs up to date\n', 'cyan'), args, False)
-  return all_logs
+        logfetch_base.log(colored('No S3 logs to download\n', 'cyan'), args, False)
+    logfetch_base.log(colored('All S3 logs up to date\n', 'cyan'), args, False)
+    return all_logs
 
 def already_downloaded(dest, filename):
-  return (os.path.isfile('{0}/{1}'.format(dest, filename.replace('.gz', '.log'))) or os.path.isfile('{0}/{1}'.format(dest, filename)))
+    return (os.path.isfile('{0}/{1}'.format(dest, filename.replace('.gz', '.log'))) or os.path.isfile('{0}/{1}'.format(dest, filename)))
 
 def logs_for_all_requests(args):
-  s3_params = {'start': int(time.mktime(args.start.timetuple()) * 1000), 'end': int(time.mktime(args.end.timetuple()) * 1000)}
-  if args.taskId:
-    return logfetch_base.get_json_response(s3_task_logs_uri(args, args.taskId), args, s3_params)
-  else:
-    tasks = logfetch_base.tasks_for_requests(args)
-    logs = []
-    tasks_progress = 0
-    tasks_goal = len(tasks)
-    for task in tasks:
-      s3_logs = logfetch_base.get_json_response(s3_task_logs_uri(args, task), args, s3_params)
-      logs = logs + s3_logs if s3_logs else logs
-      tasks_progress += 1
-      logfetch_base.update_progress_bar(tasks_progress, tasks_goal, 'S3 Log Finder', args.silent)
-    logfetch_base.log(colored('\nAlso searching s3 history...\n', 'cyan'), args, False)
-    for request in logfetch_base.all_requests(args):
-      s3_logs = logfetch_base.get_json_response(s3_request_logs_uri(args, request), args, s3_params)
-      logs = logs + s3_logs if s3_logs else logs
-    return [dict(t) for t in set([tuple(l.items()) for l in logs])] # remove any duplicates
+    s3_params = {'start': int(time.mktime(args.start.timetuple()) * 1000), 'end': int(time.mktime(args.end.timetuple()) * 1000)}
+    if args.taskId:
+        return logfetch_base.get_json_response(s3_task_logs_uri(args, args.taskId), args, s3_params)
+    else:
+        tasks = logfetch_base.tasks_for_requests(args)
+        logs = []
+        tasks_progress = 0
+        tasks_goal = len(tasks)
+        for task in tasks:
+            s3_logs = logfetch_base.get_json_response(s3_task_logs_uri(args, task), args, s3_params)
+            logs = logs + s3_logs if s3_logs else logs
+            tasks_progress += 1
+            logfetch_base.update_progress_bar(tasks_progress, tasks_goal, 'S3 Log Finder', args.silent)
+        logfetch_base.log(colored('\nAlso searching s3 history...\n', 'cyan'), args, False)
+        for request in logfetch_base.all_requests(args):
+            s3_logs = logfetch_base.get_json_response(s3_request_logs_uri(args, request), args, s3_params)
+            logs = logs + s3_logs if s3_logs else logs
+        return [dict(t) for t in set([tuple(l.items()) for l in logs])] # remove any duplicates
 
 def s3_task_logs_uri(args, idString):
-  return S3LOGS_URI_FORMAT.format(logfetch_base.base_uri(args), TASK_FORMAT.format(idString))
+    return S3LOGS_URI_FORMAT.format(logfetch_base.base_uri(args), TASK_FORMAT.format(idString))
 
 def s3_request_logs_uri(args, idString):
-  return S3LOGS_URI_FORMAT.format(logfetch_base.base_uri(args), REQUEST_FORMAT.format(idString))
+    return S3LOGS_URI_FORMAT.format(logfetch_base.base_uri(args), REQUEST_FORMAT.format(idString))
 
 def log_matches(args, filename):
-  if 'filename' in args.file_pattern:
-    return logfetch_base.log_matches(filename, '*{0}*'.format(args.logtype.replace('logs/', '')))
-  else:
-    sys.stderr.write(colored('Cannot match on log file names for s3 logs when filename is not in s3 pattern', 'red'))
-    if args.no_name_fetch_off:
-      sys.stderr.write(colored('Will not fetch any s3 logs beacuse --no-name-fetch-off is set, remove this setting to fetch all for this case instead', 'red'))
-      return False
+    if 'filename' in args.file_pattern:
+        return logfetch_base.log_matches(filename, '*{0}*'.format(args.logtype.replace('logs/', '')))
     else:
-      sys.stderr.write(colored('Will fetch all s3 logs, set --no-name-fetch-off to skip s3 logs instead for this case', 'red'))
-      return True
+        sys.stderr.write(colored('Cannot match on log file names for s3 logs when filename is not in s3 pattern', 'red'))
+        if args.no_name_fetch_off:
+            sys.stderr.write(colored('Will not fetch any s3 logs beacuse --no-name-fetch-off is set, remove this setting to fetch all for this case instead', 'red'))
+            return False
+        else:
+            sys.stderr.write(colored('Will fetch all s3 logs, set --no-name-fetch-off to skip s3 logs instead for this case', 'red'))
+            return True
