d766d1c749e47a76c0027f8a7c77085e3951f251
Stephen Salinas <ssalinas@hubspot.com>
2016-03-04 15:04:39 -0500

    smarter ordering for queries that don't use zk first

--- SingularityService/src/main/java/com/hubspot/singularity/config/SingularityConfiguration.java
+++ SingularityService/src/main/java/com/hubspot/singularity/config/SingularityConfiguration.java
@@ -240,6 +240,8 @@ public class SingularityConfiguration extends Configuration {
   @Valid
   private GraphiteConfiguration graphiteConfiguration = new GraphiteConfiguration();
 
+  private boolean taskHistoryQueryUsesZkFirst = false;
+
   public long getAskDriverToKillTasksAgainAfterMillis() {
     return askDriverToKillTasksAgainAfterMillis;
   }
@@ -974,5 +976,13 @@ public class SingularityConfiguration extends Configuration {
 
   public void setMaxStaleTasksPerRequestInZkWhenNoDatabase(Optional<Integer> maxStaleTasksPerRequestInZkWhenNoDatabase) {
     this.maxStaleTasksPerRequestInZkWhenNoDatabase = maxStaleTasksPerRequestInZkWhenNoDatabase;
+  }
+
+  public boolean isTaskHistoryQueryUsesZkFirst() {
+    return taskHistoryQueryUsesZkFirst;
+  }
+
+  public void setTaskHistoryQueryUsesZkFirst(boolean taskHistoryQueryUsesZkFirst) {
+    this.taskHistoryQueryUsesZkFirst = taskHistoryQueryUsesZkFirst;
   }
 }
