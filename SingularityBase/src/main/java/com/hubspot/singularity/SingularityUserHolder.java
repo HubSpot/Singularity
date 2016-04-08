31ba9b33bd1411a65855ff00b066f9cbc36c3d6d
Calvin Pomerantz <cpomerantz@hubspot.com>
2016-04-08 11:13:21 -0400

    Expose admin authorization to UI and use that to restrict certain pages to be admin only

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityUserHolder.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityUserHolder.java
@@ -8,14 +8,17 @@ public class SingularityUserHolder {
   private final Optional<SingularityUser> user;
   private final boolean authenticated;
   private final boolean authEnabled;
+  private final boolean isAdmin;
 
   @JsonCreator
   public SingularityUserHolder(@JsonProperty("user") Optional<SingularityUser> user,
                                @JsonProperty("authenticated") boolean authenticated,
-                               @JsonProperty("authEnabled") boolean authEnabled) {
+                               @JsonProperty("authEnabled") boolean authEnabled,
+                               @JsonProperty("isAdmin") boolean isAdmin) {
     this.user = user;
     this.authenticated = authenticated;
     this.authEnabled = authEnabled;
+    this.isAdmin = isAdmin;
   }
 
   public Optional<SingularityUser> getUser() {
@@ -29,4 +32,6 @@ public class SingularityUserHolder {
   public boolean isAuthEnabled() {
     return authEnabled;
   }
+
+  public boolean isAdmin() { return isAdmin; }
 }
