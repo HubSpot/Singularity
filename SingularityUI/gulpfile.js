4b5b9b44176d1623e97fd7ac1edac797ad7b13ed
Calvin Pomerantz <cpomerantz@hubspot.com>
2016-05-03 15:47:41 -0400

    Add config default for showTaskDiskResource to gulpfile

--- SingularityUI/gulpfile.js
+++ SingularityUI/gulpfile.js
@@ -26,6 +26,7 @@ var templateData = {
   defaultHealthcheckTimeoutSeconds: process.env.SINGULARITY_HEALTHCHECK_TIMEOUT_SECONDS || 5,
   defaultDeployHealthTimeoutSeconds: process.env.SINGULARITY_DEPLOY_HEALTH_TIMEOUT_SECONDS || 120,
   defaultHealthcheckMaxRetries: process.env.SINGULARITY_HEALTHCHECK_MAX_RETRIES || 0,
+  showTaskDiskResource: process.env.SINGULARITY_SHOW_TASK_DISK_RESOURCE || "false",
   hideNewDeployButton: process.env.SINGULARITY_HIDE_NEW_DEPLOY_BUTTON || "false",
   hideNewRequestButton: process.env.SINGULARITY_HIDE_NEW_REQUEST_BUTTON || "false",
   loadBalancingEnabled: process.env.SINGULARITY_LOAD_BALANCING_ENABLED || "false",
