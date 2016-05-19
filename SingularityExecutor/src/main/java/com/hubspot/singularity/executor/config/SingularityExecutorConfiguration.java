9243079cbcd50adf23656b4c90edcb557a960954
Stephen Salinas <ssalinas@hubspot.com>
2016-05-19 12:12:47 -0400

    retry docker pulls configurably in custom executor

--- SingularityExecutor/src/main/java/com/hubspot/singularity/executor/config/SingularityExecutorConfiguration.java
+++ SingularityExecutor/src/main/java/com/hubspot/singularity/executor/config/SingularityExecutorConfiguration.java
@@ -218,6 +218,9 @@ public class SingularityExecutorConfiguration extends BaseRunnerConfiguration {
   private int dockerClientConnectionPoolSize = 5;
 
   @JsonProperty
+  private int dockerPullRetries = 1;
+
+  @JsonProperty
   private ThreadCheckerType threadCheckerType = ThreadCheckerType.PS;
 
   public SingularityExecutorConfiguration() {
@@ -618,6 +621,14 @@ public class SingularityExecutorConfiguration extends BaseRunnerConfiguration {
 
   public void setDockerClientConnectionPoolSize(int dockerClientConnectionPoolSize) {
     this.dockerClientConnectionPoolSize = dockerClientConnectionPoolSize;
+  }
+
+  public int getDockerPullRetries() {
+    return dockerPullRetries;
+  }
+
+  public void setDockerPullRetries(int dockerPullRetries) {
+    this.dockerPullRetries = dockerPullRetries;
   }
 
   public ThreadCheckerType getThreadCheckerType() {
