ae9bee6c19e84b96f76b9a90310f7cbf869130da
tpetr <tpetr@hubspot.com>
2016-05-20 10:43:20 -0400

    forgot to bind PriorityResource

--- SingularityService/src/main/java/com/hubspot/singularity/resources/SingularityResourceModule.java
+++ SingularityService/src/main/java/com/hubspot/singularity/resources/SingularityResourceModule.java
@@ -38,6 +38,7 @@ public class SingularityResourceModule extends AbstractModule {
     bind(WebhookResource.class);
     bind(AuthResource.class);
     bind(MetricsResource.class);
+    bind(PriorityResource.class);
 
     switch (uiConfiguration.getRootUrlMode()) {
     case UI_REDIRECT: {
