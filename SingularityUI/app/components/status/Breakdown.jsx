645ca11328f428fe4779234203857918562c8602
Kevin Moses <kmoses@hubspot.com>
2016-06-09 15:38:27 -0400

    Fill it up

--- SingularityUI/app/components/status/Breakdown.jsx
+++ SingularityUI/app/components/status/Breakdown.jsx
@@ -7,10 +7,28 @@ import Utils from '../../utils';
 
 export default class StatusList extends React.Component {
 
+  renderSections() {
+    return this.props.data.map((d, i) => {
+      return (
+        <a key={i} href={`${config.appRoot}${d.link}`}>
+          <span
+            data-type="column"
+            data-state-attribute={d.attribute}
+            style={{height: `${d.percent}%`}}
+            className={`chart__data-point chart-fill-${d.type}`}
+            data-original-title={`${d.count} ${d.label}`} />
+        </a>
+      );
+    });
+  }
+
   render() {
     return (
-      <div className="chart__column">
-
+      <div>
+        <div className="chart__column">
+          {this.renderSections()}
+        </div>
+        <h5 className="text-center">{this.props.total} Total</h5>
       </div>
     );
   }
@@ -22,6 +40,7 @@ StatusList.propTypes = {
     count: React.PropTypes.number.isRequired,
     type: React.PropTypes.string.isRequired,
     label: React.PropTypes.string.isRequired,
-    link: React.PropTypes.string.isRequired
+    link: React.PropTypes.string.isRequired,
+    percent: React.PropTypes.number.isRequired
   })).isRequired
 };
