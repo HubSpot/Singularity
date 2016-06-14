6c1ac2f54258d389466d36595a4a18c82da1c88c
Calvin Pomerantz <cpomerantz@hubspot.com>
2016-06-14 15:50:04 -0400

    Header added

--- SingularityUI/app/components/requestForm/RequestForm.jsx
+++ SingularityUI/app/components/requestForm/RequestForm.jsx
@@ -3,9 +3,21 @@ import { connect } from 'react-redux';
 
 class RequestForm extends React.Component {
 
+    header() {
+        if (this.props.edit) {
+            return <h3>Editing <a href={`${config.appRoot}/request/${this.props.request.request.id}`}>{this.props.request.request.id}</a></h3>
+        } else {
+            return <h3>New Request</h3>
+        }
+    }
+
     render() {
         return(
-            <h1> Hello World </h1>
+            <div className="row new-form">
+                <div className="col-md-5 col-md-offset-3">
+                    {this.header()}
+                </div>
+            </div>
         );
     }
 
