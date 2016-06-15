1d0730e11393aa5f77c9010639b2ebea85e341f1
Calvin Pomerantz <cpomerantz@hubspot.com>
2016-06-15 14:09:56 -0400

    Request type selectors and warning that editing a request options will only apply to new tasks

--- SingularityUI/app/components/requestForm/RequestForm.jsx
+++ SingularityUI/app/components/requestForm/RequestForm.jsx
@@ -1,18 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import FormField from '../common/formItems/FormFieldRedux';
+import { modifyField, clearForm } from '../../actions/form';
+import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
+import ToolTip from 'react-bootstrap/lib/Tooltip';
+import Utils from '../../utils';
 
-let FORM_NAME = 'requestForm';
+let FORM_ID = 'requestForm';
+
+let REQUEST_TYPES = ['SERVICE', 'WORKER', 'SCHEDULED', 'ON_DEMAND', 'RUN_ONCE'];
 
 class RequestForm extends React.Component {
+
+    componentDidMount() {
+        this.props.clearForm(FORM_ID);
+    }
 
     cantSubmit() {
         return false;
     }
 
-    submitForm() {
-        // TODO - Implement
+    submitForm(props, event) {
+        event.preventDefault()
         return null;
+    }
+
+    getButtonsDisabled(type) {
+        if (this.props.edit && this.getRequestType() !== type) {
+            return 'disabled';
+        } else {
+            return null;
+        }
+    }
+
+    updateTypeButtonClick(props, event) {
+        event.preventDefault();
+        props.update(FORM_ID, 'requestType', event.target.value);
+    }
+
+    getRequestType() {
+        if (this.props.edit) {
+            return this.props.request.request.requestType;
+        } else {
+            return this.props.requestType;
+        }
+    }
+
+    getActive(type) {
+        if (this.getRequestType() === type) {
+            return 'active';
+        }
     }
 
     header() {
@@ -23,17 +60,68 @@ class RequestForm extends React.Component {
         }
     }
 
+    renderNewTasksOnlyWarning() {
+        if (this.props.edit) {
+            return (
+                <div className="alert alert-info alert-slim" role="alert">
+                    <strong>Note:</strong> changes made below will only affect new tasks
+                </div>
+            );
+        } else {
+            return undefined
+        }
+    }
+
+    renderRequestTypeSelectors() {
+        let selectors = [];
+        let tooltip = (
+            <ToolTip id="cannotChangeAfterCreation">Option cannot be altered after creation</ToolTip>
+        );
+        let key = 0;
+        for (let requestType of REQUEST_TYPES) {
+            let selector = (
+                <button
+                    key={key}
+                    value={requestType}
+                    className={`btn btn-default ${this.getActive(requestType)}`}
+                    onClick={event => this.updateTypeButtonClick(this.props, event)}
+                    disabled={this.getButtonsDisabled(requestType)}
+                >
+                    {Utils.humanizeText(requestType)}
+                </button>
+            );
+            if (this.props.edit && requestType === this.getRequestType()) {
+                selectors.push (<OverlayTrigger placement="top" key={key} overlay={tooltip}>{selector}</OverlayTrigger>);
+            } else {
+                selectors.push (selector);
+            }
+            key ++;
+        }
+        return (
+            <div className="form-group">
+                <label>Type</label>
+                <div id="type" class="btn-group">
+                    {selectors}
+                </div>
+            </div>
+        );
+    }
+
+    renderRequestTypeSpecificFormFields() {
+        //
+    }
+
     renderForm() {
         let requestId = this.props.request.request ? this.props.request.request.id : undefined;
         return (
-            <form role='form'>
+            <form role='form' onSubmit={event => this.submitForm(this.props, event)}>
                 { this.props.edit ? undefined :
                     <div className="form-group required" onSubmit={this.submitForm}>
                         <label htmlFor="id">ID</label>
                         <FormField
                             id = "id"
                             className = "form-control"
-                            formId = {FORM_NAME}
+                            formId = {FORM_ID}
                             fieldId = 'requestId'
                             prop = {{
                                 placeholder: "eg: my-awesome-request",
@@ -42,6 +130,21 @@ class RequestForm extends React.Component {
                         />
                     </div>
                 }
+                <div class="form-group">
+                    <label htmlFor="owner">Owners <span className='form-label-tip'>separate multiple owners with commas</span></label>
+                    <FormField
+                            id = "owners"
+                            className = "tagging-input"
+                            formId = {FORM_ID}
+                            fieldId = 'owners'
+                            prop = {{
+                                inputType: 'text'
+                            }}
+                        />
+                </div>
+                {this.renderRequestTypeSelectors()}
+                {this.renderNewTasksOnlyWarning()}
+                {this.renderRequestTypeSpecificFormFields()}
                 <div id="button-row">
                     <span>
                         <button type="submit" className="btn btn-success btn-lg" disabled={this.cantSubmit() ? 'disabled' : undefined}>
@@ -69,13 +172,16 @@ class RequestForm extends React.Component {
 function mapStateToProps(state) {
     return {
         racks: state.api.racks.data,
-        request: state.api.request ? state.api.request.data : undefined
+        request: state.api.request ? state.api.request.data : undefined,
+        form: state.form[FORM_ID],
+        requestType: state.form && state.form[FORM_ID] ? state.form[FORM_ID].requestType : undefined
     }
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        //saveRequest: (slave) => { dispatch(FreezeAction.trigger(slave.id)); }
+        update: (formId, fieldId, newValue) => { dispatch(modifyField(formId, fieldId, newValue)); },
+        clearForm: (formId) => { dispatch(clearForm(formId)); }
     }
 }
 
