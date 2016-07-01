f45c75313fd9c08405d2dcf497909fcbf99b2e5c
Stephen Salinas <ssalinas@hubspot.com>
2016-07-01 13:04:02 -0400

    sort task requests first by priority, then by task start

--- SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularitySchedulerPriority.java
+++ SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularitySchedulerPriority.java
@@ -7,12 +7,14 @@ import java.util.Map;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.primitives.Doubles;
 import com.google.common.primitives.Longs;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.hubspot.mesos.JavaUtils;
 import com.hubspot.singularity.SingularityTaskId;
 import com.hubspot.singularity.SingularityTaskRequest;
+import com.hubspot.singularity.data.PriorityManager;
 import com.hubspot.singularity.data.RequestManager;
 import com.hubspot.singularity.data.TaskManager;
 
@@ -33,17 +35,24 @@ public class SingularitySchedulerPriority {
   };
 
   @Inject
-  public SingularitySchedulerPriority(TaskManager taskManager, RequestManager requestManager) {
+  public SingularitySchedulerPriority(TaskManager taskManager, final PriorityManager priorityManager) {
     this.taskManager = taskManager;
     this.mostRecentTaskStartPerRequest = Maps.newHashMap();
     this.comparator = new Comparator<SingularityTaskRequest>() {
 
       @Override
       public int compare(SingularityTaskRequest o1, SingularityTaskRequest o2) {
-        final Long request1LastStartedAt = mostRecentTaskStartPerRequest.get(o1.getRequest().getId());
-        final Long request2LastStartedAt = mostRecentTaskStartPerRequest.get(o2.getRequest().getId());
+        final Double request1Priority = priorityManager.getTaskPriorityLevelForRequest(o1.getRequest());
+        final Double request2Priority = priorityManager.getTaskPriorityLevelForRequest(o2.getRequest());
+        int priorityComp = Doubles.compare(request1Priority, request2Priority);
 
-        return Longs.compare(request1LastStartedAt, request2LastStartedAt);
+        if (priorityComp != 0) {
+          return priorityComp;
+        } else {
+          final Long request1LastStartedAt = mostRecentTaskStartPerRequest.get(o1.getRequest().getId());
+          final Long request2LastStartedAt = mostRecentTaskStartPerRequest.get(o2.getRequest().getId());
+          return Longs.compare(request1LastStartedAt, request2LastStartedAt);
+        }
       }
     };
   }
