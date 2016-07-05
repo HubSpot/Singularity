a831d73066ef083078058668a7769a86d1ceedf6
Calvin Pomerantz <cpomerantz@hubspot.com>
2016-07-05 11:01:02 -0400

    Fix linting errors machines page

--- SingularityUI/app/components/machines/MachinesPage.jsx
+++ SingularityUI/app/components/machines/MachinesPage.jsx
@@ -1,57 +1,57 @@
 import React from 'react';
 import OldTable from '../common/OldTable';
 
-let MachinesPage = React.createClass({
+const MachinesPage = React.createClass({
 
-    propTypes: {
-        header: React.PropTypes.string.isRequired, // Eg. 'Slaves', 'Racks'
-        states: React.PropTypes.arrayOf(React.PropTypes.shape({
-            stateName: React.PropTypes.string.isRequired, // Eg. 'Active', 'Frozen', etc
-            stateTableColumnMetadata: React.PropTypes.arrayOf(React.PropTypes.shape({
-                data: React.PropTypes.string,
-                className: React.PropTypes.string,
-                doSort: React.PropTypes.func,
-                sortable: React.PropTypes.boolean,
-                sortAttr: React.PropTypes.string
-            })).isRequired,
-            // Host info, in a format that can be passed into a table
-            hostsInState: React.PropTypes.arrayOf(React.PropTypes.shape({
-                dataId: React.PropTypes.string.isRequired,
-                className: React.PropTypes.string,
-                data: React.PropTypes.arrayOf(React.PropTypes.shape({
-                    component: React.PropTypes.func.isRequired,
-                    prop: React.PropTypes.object,
-                    id: React.PropTypes.string,
-                    className: React.PropTypes.string
-                })).isRequired
-            })).isRequired,
-            emptyTableMessage: React.PropTypes.string.isRequired
+  propTypes: {
+    header: React.PropTypes.string.isRequired, // Eg. 'Slaves', 'Racks'
+    states: React.PropTypes.arrayOf(React.PropTypes.shape({
+      stateName: React.PropTypes.string.isRequired, // Eg. 'Active', 'Frozen', etc
+      stateTableColumnMetadata: React.PropTypes.arrayOf(React.PropTypes.shape({
+        data: React.PropTypes.string,
+        className: React.PropTypes.string,
+        doSort: React.PropTypes.func,
+        sortable: React.PropTypes.boolean,
+        sortAttr: React.PropTypes.string
+      })).isRequired,
+      // Host info, in a format that can be passed into a table
+      hostsInState: React.PropTypes.arrayOf(React.PropTypes.shape({
+        dataId: React.PropTypes.string.isRequired,
+        className: React.PropTypes.string,
+        data: React.PropTypes.arrayOf(React.PropTypes.shape({
+          component: React.PropTypes.func.isRequired,
+          prop: React.PropTypes.object,
+          id: React.PropTypes.string,
+          className: React.PropTypes.string
         })).isRequired
-    },
+      })).isRequired,
+      emptyTableMessage: React.PropTypes.string.isRequired
+    })).isRequired
+  },
 
-    renderState(state, key) {
-      return (
-        <div key={key}>
-            <h2> {state.stateName} </h2>
-            <OldTable
-                noPages = {true}
-                tableClassOpts = "table-striped"
-                columnHeads = {state.stateTableColumnMetadata}
-                tableRows = {state.hostsInState}
-                emptyTableMessage = {state.emptyTableMessage}
-            />
-        </div>
-      );
-    },
+  renderState(state, key) {
+    return (
+    <div key={key}>
+      <h2> {state.stateName} </h2>
+      <OldTable
+        noPages = {true}
+        tableClassOpts = "table-striped"
+        columnHeads = {state.stateTableColumnMetadata}
+        tableRows = {state.hostsInState}
+        emptyTableMessage = {state.emptyTableMessage}
+      />
+    </div>
+    );
+  },
 
-    render() {
-      return (
-        <div>
-            <h1> {this.props.header} </h1>
-            {this.props.states.map(this.renderState)}
-        </div>
-      );
-    },
+  render() {
+    return (
+    <div>
+      <h1> {this.props.header} </h1>
+      {this.props.states.map(this.renderState)}
+    </div>
+    );
+  },
 });
 
 
