64cf34568b372d928775631372043bca12f62ea7
Calvin Pomerantz <cpomerantz@hubspot.com>
2016-07-07 13:38:24 -0400

    Make MachinesPage a functional component

--- SingularityUI/app/components/machines/MachinesPage.jsx
+++ SingularityUI/app/components/machines/MachinesPage.jsx
@@ -1,36 +1,8 @@
 import React from 'react';
 import OldTable from '../common/OldTable';
 
-const MachinesPage = React.createClass({
-
-  propTypes: {
-    error: React.PropTypes.string,
-    header: React.PropTypes.string.isRequired, // Eg. 'Slaves', 'Racks'
-    states: React.PropTypes.arrayOf(React.PropTypes.shape({
-      stateName: React.PropTypes.string.isRequired, // Eg. 'Active', 'Frozen', etc
-      stateTableColumnMetadata: React.PropTypes.arrayOf(React.PropTypes.shape({
-        data: React.PropTypes.string,
-        className: React.PropTypes.string,
-        doSort: React.PropTypes.func,
-        sortable: React.PropTypes.boolean,
-        sortAttr: React.PropTypes.string
-      })).isRequired,
-      // Host info, in a format that can be passed into a table
-      hostsInState: React.PropTypes.arrayOf(React.PropTypes.shape({
-        dataId: React.PropTypes.string.isRequired,
-        className: React.PropTypes.string,
-        data: React.PropTypes.arrayOf(React.PropTypes.shape({
-          component: React.PropTypes.func.isRequired,
-          prop: React.PropTypes.object,
-          id: React.PropTypes.string,
-          className: React.PropTypes.string
-        })).isRequired
-      })).isRequired,
-      emptyTableMessage: React.PropTypes.string.isRequired
-    })).isRequired
-  },
-
-  renderState(state, key) {
+function MachinesPage (props) {
+  function renderState(state, key) {
     return (
     <div key={key}>
       <h2> {state.stateName} </h2>
@@ -43,26 +15,46 @@ const MachinesPage = React.createClass({
       />
     </div>
     );
-  },
+  }
 
-  renderError() {
-    return (this.props.error &&
-      <p className="alert alert-danger">
-        {this.props.error}
-      </p>
-    );
-  },
-
-  render() {
-    return (
+  return (
     <div>
-      {this.renderError()}
-      <h1> {this.props.header} </h1>
-      {this.props.states.map(this.renderState)}
+      {props.error &&
+        <p className="alert alert-danger">
+          {props.error}
+        </p>
+      }
+      <h1> {props.header} </h1>
+      {props.states.map(renderState)}
     </div>
-    );
-  },
-});
+  );
+}
 
+MachinesPage.propTypes = {
+  error: React.PropTypes.string,
+  header: React.PropTypes.string.isRequired, // Eg. 'Slaves', 'Racks'
+  states: React.PropTypes.arrayOf(React.PropTypes.shape({
+    stateName: React.PropTypes.string.isRequired, // Eg. 'Active', 'Frozen', etc
+    stateTableColumnMetadata: React.PropTypes.arrayOf(React.PropTypes.shape({
+      data: React.PropTypes.string,
+      className: React.PropTypes.string,
+      doSort: React.PropTypes.func,
+      sortable: React.PropTypes.boolean,
+      sortAttr: React.PropTypes.string
+    })).isRequired,
+    // Host info, in a format that can be passed into a table
+    hostsInState: React.PropTypes.arrayOf(React.PropTypes.shape({
+      dataId: React.PropTypes.string.isRequired,
+      className: React.PropTypes.string,
+      data: React.PropTypes.arrayOf(React.PropTypes.shape({
+        component: React.PropTypes.func.isRequired,
+        prop: React.PropTypes.object,
+        id: React.PropTypes.string,
+        className: React.PropTypes.string
+      })).isRequired
+    })).isRequired,
+    emptyTableMessage: React.PropTypes.string.isRequired
+  })).isRequired
+};
 
 export default MachinesPage;
