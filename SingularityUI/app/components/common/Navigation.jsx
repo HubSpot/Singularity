c2b57e9ca0f7fd400d4068867505c85c3687061e
Kevin Moses <kmoses@hubspot.com>
2016-07-08 10:07:42 -0400

    Add id on admin dropdown

--- SingularityUI/app/components/common/Navigation.jsx
+++ SingularityUI/app/components/common/Navigation.jsx
@@ -20,7 +20,7 @@ export default class Navigation extends Component {
             <NavItem eventKey={2} href={`${config.appRoot}/status`} active={fragment === 'status'}>Status</NavItem>
             <NavItem eventKey={3} href={`${config.appRoot}/requests`} active={_.contains(['requests', 'request'], fragment)}>Requests</NavItem>
             <NavItem eventKey={4} href={`${config.appRoot}/tasks`} active={_.contains(['tasks', 'task'], fragment)}>Tasks</NavItem>
-            <NavDropdown eventKey={5} title="Admin">
+            <NavDropdown id="admin-dropdown" eventKey={5} title="Admin">
               <MenuItem eventKey={5.1} href={`${config.appRoot}/racks`} active={fragment === 'racks'}>Racks</MenuItem>
               <MenuItem eventKey={5.2} href={`${config.appRoot}/slaves`} active={fragment === 'slaves'}>Slaves</MenuItem>
               <MenuItem eventKey={5.3} href={`${config.appRoot}/webhooks`} active={fragment === 'webhooks'}>Webhooks</MenuItem>
