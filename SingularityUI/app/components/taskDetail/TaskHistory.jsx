eb29834dc194ea9a7de929c1f3b23a24e96e9ba7
Calvin Pomerantz <cpomerantz@hubspot.com>
2016-07-11 14:16:26 -0400

    Delint TaskHistory

--- SingularityUI/app/components/taskDetail/TaskHistory.jsx
+++ SingularityUI/app/components/taskDetail/TaskHistory.jsx
@@ -1,28 +1,19 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Utils from '../../utils';
-import { InfoBox, UsageInfo } from '../common/statelessComponents';
-import { Alert } from 'react-bootstrap';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
-
-import JSONButton from '../common/JSONButton';
 import Section from '../common/Section';
-import ConfirmationDialog from '../common/ConfirmationDialog';
-import CollapsableSection from '../common/CollapsableSection';
 import SimpleTable from '../common/SimpleTable';
-import Glyphicon from '../common/atomicDisplayItems/Glyphicon';
 
-export default (props) => {
-  const t = props.task;
+function TaskHistory (props) {
   return (
     <Section title="History">
       <SimpleTable
         emptyMessage="This task has no history yet"
-        entries={t.taskUpdates.concat().reverse()}
+        entries={props.task.taskUpdates.concat().reverse()}
         perPage={5}
         headers={['Status', 'Message', 'Time']}
         renderTableRow={(data, index) => {
           return (
-            <tr key={index} className={index == 0 ? 'medium-weight' : ''}>
+            <tr key={index} className={index === 0 ? 'medium-weight' : ''}>
               <td>{Utils.humanizeText(data.taskState)}</td>
               <td>{data.statusMessage ? data.statusMessage : '—'}</td>
               <td>{Utils.timeStampFromNow(data.timestamp)}</td>
@@ -33,3 +24,15 @@ export default (props) => {
     </Section>
   );
 }
+
+TaskHistory.propTypes = {
+  task: PropTypes.shape({
+    taskUpdates: PropTypes.arrayOf(PropTypes.shape({
+      taskState: PropTypes.string,
+      statusMessage: PropTypes.string,
+      timestamp: PropTypes.number
+    })).isRequired
+  }).isRequired
+};
+
+export default TaskHistory;
