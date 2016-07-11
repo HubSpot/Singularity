9947db261598e3d431be955a09b8496c749c0e5c
Calvin Pomerantz <cpomerantz@hubspot.com>
2016-07-11 14:53:53 -0400

    Delint TaskMetadataAlerts

--- SingularityUI/app/components/taskDetail/TaskMetadataAlerts.jsx
+++ SingularityUI/app/components/taskDetail/TaskMetadataAlerts.jsx
@@ -1,35 +1,22 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Utils from '../../utils';
-import { InfoBox, UsageInfo } from '../common/statelessComponents';
 import { Alert } from 'react-bootstrap';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
 
-import JSONButton from '../common/JSONButton';
-import Section from '../common/Section';
-import ConfirmationDialog from '../common/ConfirmationDialog';
-import CollapsableSection from '../common/CollapsableSection';
-import SimpleTable from '../common/SimpleTable';
-import Glyphicon from '../common/atomicDisplayItems/Glyphicon';
-
-export default (props) => {
-  const t = props.task;
-  let alerts = [];
-
-  for(let i in t.taskMetadata) {
-    let md = t.taskMetadata[i];
-    const message = md.message && (
-      <pre className='pre-scrollable'>{md.message}</pre>
+function TaskMetadataAlerts (props) {
+  const alerts = props.task.taskMetadata.map((metadataItem, key) => {
+    const message = metadataItem.message && (
+      <pre className="pre-scrollable">{metadataItem.message}</pre>
     );
-    alerts.push(
-      <Alert key={i} bsStyle={md.level == 'ERROR' ? 'danger' : 'warning'}>
-        <h4>{md.title}</h4>
+    return (
+      <Alert key={key} bsStyle={metadataItem.level === 'ERROR' ? 'danger' : 'warning'}>
+        <h4>{metadataItem.title}</h4>
         <p>
-          <strong>{Utils.timeStampFromNow(md.timestamp)}</strong> | Type: {md.type} {md.user ? `| User: ${md.user}` : null}
+          <strong>{Utils.timeStampFromNow(metadataItem.timestamp)}</strong> | Type: {metadataItem.type} {metadataItem.user && `| User: ${metadataItem.user}`}
         </p>
         {message}
       </Alert>
-    )
-  }
+    );
+  });
 
   return (
     <div>
@@ -37,3 +24,18 @@ export default (props) => {
     </div>
   );
 }
+
+TaskMetadataAlerts.propTypes = {
+  task: PropTypes.shape({
+    taskMetadata: PropTypes.arrayOf(PropTypes.shape({
+      message: PropTypes.string,
+      level: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      timestamp: PropTypes.number.isRequired,
+      type: PropTypes.string.isRequired,
+      user: PropTypes.string
+    })).isRequired
+  }).isRequired
+};
+
+export default TaskMetadataAlerts;
