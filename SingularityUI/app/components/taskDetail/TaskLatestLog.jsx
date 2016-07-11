9ce18c25f40ab45d6df2a70745ea73828f16c5df
Calvin Pomerantz <cpomerantz@hubspot.com>
2016-07-11 14:35:45 -0400

    Delint TaskLatestLog

--- SingularityUI/app/components/taskDetail/TaskLatestLog.jsx
+++ SingularityUI/app/components/taskDetail/TaskLatestLog.jsx
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Utils from '../../utils';
-import { InfoBox, UsageInfo } from '../common/statelessComponents';
-import { Alert } from 'react-bootstrap';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
-
-import JSONButton from '../common/JSONButton';
 import Section from '../common/Section';
-import ConfirmationDialog from '../common/ConfirmationDialog';
-import CollapsableSection from '../common/CollapsableSection';
-import SimpleTable from '../common/SimpleTable';
 import Glyphicon from '../common/atomicDisplayItems/Glyphicon';
 
-export default (props) => {
-  const t = props.task;
-  const link = t.isStillRunning ? (
-    <a href={`${config.appRoot}/task/${t.task.taskId.id}/tail/${Utils.substituteTaskId(config.runningTaskLogPath, t.task.taskId.id)}`} title="Log">
+function TaskLatestLog (props) {
+  const link = props.task.isStillRunning ? (
+    <a href={`${config.appRoot}/task/${props.task.task.taskId.id}/tail/${Utils.substituteTaskId(config.runningTaskLogPath, props.task.task.taskId.id)}`} title="Log">
         <span><Glyphicon iconClass="file" /> {Utils.fileName(config.runningTaskLogPath)}</span>
     </a>
   ) : (
-    <a href={`${config.appRoot}/task/${t.task.taskId.id}/tail/${Utils.substituteTaskId(config.finishedTaskLogPath, t.task.taskId.id)}`} title="Log">
+    <a href={`${config.appRoot}/task/${props.task.task.taskId.id}/tail/${Utils.substituteTaskId(config.finishedTaskLogPath, props.task.task.taskId.id)}`} title="Log">
         <span><Glyphicon iconClass="file" /> {Utils.fileName(config.finishedTaskLogPath)}</span>
     </a>
   );
@@ -32,3 +23,16 @@ export default (props) => {
     </Section>
   );
 }
+
+TaskLatestLog.propTypes = {
+  task: PropTypes.shape({
+    task: PropTypes.shape({
+      taskId: PropTypes.shape({
+        id: PropTypes.string
+      }).isRequired
+    }).isRequired,
+    isStillRunning: PropTypes.bool
+  }).isRequired
+};
+
+export default TaskLatestLog;
