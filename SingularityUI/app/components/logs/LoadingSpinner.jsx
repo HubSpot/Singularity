9c53ad83b7b4ea0aae0bd80e122cdb900a053efd
Calvin Pomerantz <cpomerantz@hubspot.com>
2016-07-15 11:42:27 -0400

    Delint LoadingSpinner

--- SingularityUI/app/components/logs/LoadingSpinner.jsx
+++ SingularityUI/app/components/logs/LoadingSpinner.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import classNames from 'classnames';
 
-class LoadingSpinner extends React.Component {
-  render() {
-    let className = classNames({
-      'page-loader': true,
-      centered: this.props.centered
-    });
+function LoadingSpinner(props) {
+  let className = classNames({
+    'page-loader': true,
+    centered: this.props.centered
+  });
 
-    if (this.props.children.length > 0) {
-      return <div className="page-loader-with-message"><div className={className} /><p>{this.props.children}</p></div>;
-    } else {
-      return <div className={className} />;
-    }
+  if (this.props.children.length > 0) {
+    return <div className="page-loader-with-message"><div className={className} /><p>{props.children}</p></div>;
   }
+  return <div className={className} />;
 }
 
 LoadingSpinner.propTypes = {
