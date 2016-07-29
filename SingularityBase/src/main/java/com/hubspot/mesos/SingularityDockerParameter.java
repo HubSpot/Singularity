df3239f020c3a0ccdbda020b850b9686ece783db
Stephen Salinas <ssalinas@hubspot.com>
2016-07-29 15:42:55 -0400

    make checkstyle happy

--- SingularityBase/src/main/java/com/hubspot/mesos/SingularityDockerParameter.java
+++ SingularityBase/src/main/java/com/hubspot/mesos/SingularityDockerParameter.java
@@ -37,10 +37,12 @@ public class SingularityDockerParameter {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
-        if (o == null || getClass() != o.getClass())
+        }
+        if (o == null || getClass() != o.getClass()) {
             return false;
+        }
         SingularityDockerParameter that = (SingularityDockerParameter) o;
         return Objects.equals(key, that.key) &&
             Objects.equals(value, that.value);
--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityMesosTaskLabel.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityMesosTaskLabel.java
@@ -39,10 +39,12 @@ public class SingularityMesosTaskLabel {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
-        if (o == null || getClass() != o.getClass())
+        }
+        if (o == null || getClass() != o.getClass()) {
             return false;
+        }
         SingularityMesosTaskLabel that = (SingularityMesosTaskLabel) o;
         return Objects.equals(key, that.key) &&
             Objects.equals(value, that.value);
