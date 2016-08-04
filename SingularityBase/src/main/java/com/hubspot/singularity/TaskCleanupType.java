8b0bad2b1354b84ba2c3ebc82dedc49ce366cdf1
Stephen Salinas <ssalinas@hubspot.com>
2016-08-04 09:45:27 -0400

    missed trailing spaces fixing merge conflict

--- SingularityBase/src/main/java/com/hubspot/singularity/TaskCleanupType.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/TaskCleanupType.java
@@ -2,7 +2,7 @@ package com.hubspot.singularity;
 
 public enum TaskCleanupType {
 
-  USER_REQUESTED(true, true), USER_REQUESTED_TASK_BOUNCE(false, false), DECOMISSIONING(false, false), SCALING_DOWN(true, false), BOUNCING(false, false), INCREMENTAL_BOUNCE(false, false),  
+  USER_REQUESTED(true, true), USER_REQUESTED_TASK_BOUNCE(false, false), DECOMISSIONING(false, false), SCALING_DOWN(true, false), BOUNCING(false, false), INCREMENTAL_BOUNCE(false, false),
   DEPLOY_FAILED(true, true), NEW_DEPLOY_SUCCEEDED(true, false), DEPLOY_STEP_FINISHED(true, false), DEPLOY_CANCELED(true, true), UNHEALTHY_NEW_TASK(true, true),
   OVERDUE_NEW_TASK(true, true), USER_REQUESTED_DESTROY(true, true), PRIORITY_KILL(true, true), INCREMENTAL_DEPLOY_FAILED(false, true), INCREMENTAL_DEPLOY_CANCELLED(false, true);
 
