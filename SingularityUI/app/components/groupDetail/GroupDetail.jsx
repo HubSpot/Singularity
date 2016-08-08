8cff85bcba69258faf6712b3af36497e74786d61
Kevin Moses <kmoses@hubspot.com>
2016-08-08 17:25:51 -0400

    Convert to functional component

--- SingularityUI/app/components/groupDetail/GroupDetail.jsx
+++ SingularityUI/app/components/groupDetail/GroupDetail.jsx
@@ -2,19 +2,12 @@ import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
 import rootComponent from '../../rootComponent';
 
-class GroupDetail extends React.Component {
-
-  static propTypes = {
-    group: PropTypes.object
-  }
-
-  render() {
-    if (!this.props.group) return <div className="loader loader-fixed"></div>;
-    return (
-      <h2>{this.props.group.id}</h2>
-    );
-  }
-}
+const GroupDetail = ({group}) => {
+  if (!group) return <div className="loader loader-fixed"></div>;
+  return (
+    <h2>{group.id}</h2>
+  );
+};
 
 function mapStateToProps(state, ownProps) {
   const group = _.find(state.api.requestGroups.data, (g) => g.id === ownProps.params.groupId);
@@ -24,5 +17,9 @@ function mapStateToProps(state, ownProps) {
     group
   });
 }
+
+GroupDetail.propTypes = {
+  group: PropTypes.object
+};
 
 export default connect(mapStateToProps)(rootComponent(GroupDetail, (props) => `Group ${props.params.groupId}`));
