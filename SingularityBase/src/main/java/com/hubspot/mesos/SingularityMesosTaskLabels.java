4f6eaa598d86aea6cec7ce66c14f1ae7265f1747
Stephen Salinas <ssalinas@hubspot.com>
2016-08-08 13:20:00 -0400

    smart serialize, use map when no dupe keys

--- SingularityBase/src/main/java/com/hubspot/mesos/SingularityMesosTaskLabels.java
+++ SingularityBase/src/main/java/com/hubspot/mesos/SingularityMesosTaskLabels.java
@@ -1,11 +1,16 @@
 package com.hubspot.mesos;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.common.base.Optional;
 
 @JsonDeserialize(using = SingularityMesosTaskLabelsDeserializer.class)
 @JsonSerialize(using = SingularityMesosTaskLabelsSerializer.class)
@@ -19,5 +24,26 @@ public class SingularityMesosTaskLabels {
 
   public List<SingularityMesosTaskLabel> getLabels() {
     return labels;
+  }
+
+  @JsonIgnore
+  public boolean hasDuplicateKey() {
+    List<String> keys = new ArrayList<>();
+    for (SingularityMesosTaskLabel label : labels) {
+      if (keys.contains(label.getKey())) {
+        return true;
+      }
+      keys.add(label.getKey());
+    }
+    return false;
+  }
+
+  @JsonIgnore
+  public Map<String, Optional<String>> toMap() {
+    Map<String, Optional<String>> map = new HashMap<>();
+    for (SingularityMesosTaskLabel label : labels) {
+      map.put(label.getKey(), label.getValue());
+    }
+    return map;
   }
 }
--- SingularityBase/src/main/java/com/hubspot/mesos/SingularityMesosTaskLabelsSerializer.java
+++ SingularityBase/src/main/java/com/hubspot/mesos/SingularityMesosTaskLabelsSerializer.java
@@ -9,10 +9,14 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 public class SingularityMesosTaskLabelsSerializer extends JsonSerializer<SingularityMesosTaskLabels> {
   @Override
   public void serialize(SingularityMesosTaskLabels labels, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-    jsonGenerator.writeStartArray();
-    for (SingularityMesosTaskLabel label : labels.getLabels()) {
-      jsonGenerator.writeObject(label);
+    if (labels.hasDuplicateKey()) {
+      jsonGenerator.writeStartArray();
+      for (SingularityMesosTaskLabel label : labels.getLabels()) {
+        jsonGenerator.writeObject(label);
+      }
+      jsonGenerator.writeEndArray();
+    } else {
+      jsonGenerator.writeObject(labels.toMap());
     }
-    jsonGenerator.writeEndArray();
   }
 }
