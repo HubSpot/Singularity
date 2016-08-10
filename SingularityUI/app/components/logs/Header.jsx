ef94dcef7fec792f346eca00bb5b112ecd4891ab
Kevin Moses <kmoses@hubspot.com>
2016-08-10 10:19:50 -0400

    Fix color mapping

--- SingularityUI/app/components/logs/Header.jsx
+++ SingularityUI/app/components/logs/Header.jsx
@@ -22,8 +22,8 @@ class Header extends React.Component {
   renderViewButtons() {
     if (this.props.multipleTasks) {
       return (<div className="btn-group" role="group" title="Select View Type">
-        <button type="button" className={classNames({btn: true, 'btn-sm': true, 'btn-default': true, 'no-margin': true, active: this.props.viewMode === 'unified'})} onClick={function () { this.props.switchViewMode('unified'); }}>Unified</button>
-        <button type="button" className={classNames({btn: true, 'btn-sm': true, 'btn-default': true, 'no-margin': true, active: this.props.viewMode === 'split'})} onClick={function () { this.props.switchViewMode('split'); }}>Split</button>
+        <button type="button" className={classNames({btn: true, 'btn-sm': true, 'btn-default': true, 'no-margin': true, active: this.props.viewMode === 'unified'})} onClick={() => { this.props.switchViewMode('unified'); }}>Unified</button>
+        <button type="button" className={classNames({btn: true, 'btn-sm': true, 'btn-default': true, 'no-margin': true, active: this.props.viewMode === 'split'})} onClick={() => { this.props.switchViewMode('split'); }}>Split</button>
       </div>);
     }
   }
--- SingularityUI/app/components/logs/LogLines.jsx
+++ SingularityUI/app/components/logs/LogLines.jsx
@@ -8,7 +8,7 @@ import { taskGroupTop, taskGroupBottom } from '../../actions/log';
 
 function sum (numbers) {
   let total = 0;
-  for (let i=0; i<numbers.length; i++) {
+  for (let i = 0; i < numbers.length; i++) {
     total += numbers[i];
   }
   return total;
@@ -58,13 +58,14 @@ class LogLines extends React.Component {
         taskId={taskId}
         timestamp={timestamp}
         isHighlighted={offset === initialOffset}
-        color={colorMap[taskId]} />;
+        color={colorMap[taskId]}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       />;
     });
   }
 
   renderLoadingMore() {
     if (this.props.terminated) {
-      return null
+      return null;
     } else if (this.props.initialDataLoaded) {
       if (this.props.reachedEndOfFile) {
         if (this.props.search) {
@@ -84,7 +85,7 @@ class LogLines extends React.Component {
 
 
   handleScroll() {
-    const {scrollTop, scrollHeight, clientHeight} = this.refs.tailContents
+    const {scrollTop, scrollHeight, clientHeight} = this.refs.tailContents;
 
     if (scrollTop < clientHeight) {
       this.props.taskGroupTop(this.props.taskGroupId, true);
@@ -100,14 +101,14 @@ class LogLines extends React.Component {
   }
 
   render() {
-    return <div className="contents-container">
+    return (<div className="contents-container">
       <div className={classNames(['tail-contents', this.props.activeColor])} ref="tailContents" onScroll={(event) => { this.handleScroll(event); }}>
         {this.renderLoadingPrevious()}
         {this.renderLogLines()}
         {this.renderLoadingMore()}
         {this.props.fileNotFound}
       </div>
-    </div>;
+    </div>);
   }
 }
 
@@ -129,15 +130,15 @@ LogLines.propTypes = {
 };
 
 function mapStateToProps(state, ownProps) {
-  const taskGroup = state.taskGroups[ownProps.taskGroupId]
+  const taskGroup = state.taskGroups[ownProps.taskGroupId];
   const tasks = taskGroup.taskIds.map(function (taskId) { return state.tasks[taskId]; });
 
-  const colorMap = {}
+  const colorMap = {};
   if (taskGroup.taskIds.length > 1) {
-    let i = 0
-    for (taskId of taskGroup.taskIds) {
+    let i = 0;
+    for (const taskId of taskGroup.taskIds) {
       colorMap[taskId] = `hsla(${(360 / taskGroup.taskIds.length) * i}, 100%, 50%, 0.1)`;
-      i++
+      i++;
     }
   }
 
@@ -152,13 +153,13 @@ function mapStateToProps(state, ownProps) {
     bottom: taskGroup.bottom,
     initialDataLoaded: _.all(_.pluck(tasks, 'initialDataLoaded')),
     terminated: _.all(_.pluck(tasks, 'terminated')),
-    reachedStartOfFile: _.all(tasks.map( function ({minOffset}) { return minOffset === 0; })),
-    reachedEndOfFile: _.all(tasks.map( function ({maxOffset, filesize}) { return maxOffset >= filesize; })),
+    reachedStartOfFile: _.all(tasks.map(function ({minOffset}) { return minOffset === 0; })),
+    reachedEndOfFile: _.all(tasks.map(function ({maxOffset, filesize}) { return maxOffset >= filesize; })),
     bytesRemainingBefore: sum(_.pluck(tasks, 'minOffset')),
-    bytesRemainingAfter: sum(tasks.map( function ({filesize, maxOffset}) { return Math.max(filesize - maxOffset, 0); })),
-    colorMap: colorMap,
+    bytesRemainingAfter: sum(tasks.map(function ({filesize, maxOffset}) { return Math.max(filesize - maxOffset, 0); })),
+    colorMap,
     search: state.search,
-  }
+  };
 }
 
 const mapDispatchToProps = { taskGroupTop, taskGroupBottom };
