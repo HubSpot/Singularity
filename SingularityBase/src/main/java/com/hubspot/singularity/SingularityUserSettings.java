3e4e96babc4fe3dc74fb7a61e07e648e929c6d11
Matthew Cotton <mcotton@hubspot.com>
2016-08-11 14:16:53 -0400

    Make SingularityUserSettings JSON-capable

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityUserSettings.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityUserSettings.java
@@ -4,10 +4,14 @@ import static com.google.common.collect.ImmutableSet.copyOf;
 
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class SingularityUserSettings {
   private final Set<String> starredRequestIds;
 
-  public SingularityUserSettings(Set<String> starredRequestIds) {
+  @JsonCreator
+  public SingularityUserSettings(@JsonProperty Set<String> starredRequestIds) {
     this.starredRequestIds = copyOf(starredRequestIds);
   }
 
