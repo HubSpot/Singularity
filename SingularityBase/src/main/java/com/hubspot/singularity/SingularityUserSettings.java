cd8ee59741203ee41e72912ba5a26f72725b1d14
Matthew Cotton <mcotton@hubspot.com>
2016-08-11 14:09:58 -0400

    Add constructor/data to SingularityUserSettings

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityUserSettings.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityUserSettings.java
@@ -1,4 +1,17 @@
 package com.hubspot.singularity;
 
+import static com.google.common.collect.ImmutableSet.copyOf;
+
+import java.util.Set;
+
 public class SingularityUserSettings {
+  private final Set<String> starredRequestIds;
+
+  public SingularityUserSettings(Set<String> starredRequestIds) {
+    this.starredRequestIds = copyOf(starredRequestIds);
+  }
+
+  public Set<String> getStarredRequestIds() {
+    return starredRequestIds;
+  }
 }
