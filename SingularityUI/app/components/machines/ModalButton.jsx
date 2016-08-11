f059c16bf5800b2079dd7c1ed93a8f0b750fa625
Calvin Pomerantz <cpomerantz@hubspot.com>
2016-08-11 17:03:08 -0400

    Make ModalButton functional

--- SingularityUI/app/components/machines/ModalButton.jsx
+++ SingularityUI/app/components/machines/ModalButton.jsx
@@ -2,37 +2,34 @@ import React, { PropTypes } from 'react';
 import FormModal from '../common/modal/FormModal';
 import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 
-class ModalButton extends React.Component {
-
-  buttonWithMaybeTooltip() {
-    if (this.props.tooltipText) {
-      return (
-        <OverlayTrigger
-          placement="top"
-          overlay={<Tooltip id="overlay">{this.props.tooltipText}</Tooltip>}>
-          <a onClick={() => this.refs.modal.show()} >{this.props.buttonChildren}</a>
-        </OverlayTrigger>
-      );
-    }
-    return <a onClick={() => this.refs.modal.show()} >{this.props.buttonChildren}</a>;
-  }
-
-  render() {
-    return (
-      <span>
-        {this.buttonWithMaybeTooltip()}
-        <FormModal
-          ref="modal"
-          action={this.props.action}
-          onConfirm={(data) => this.props.onConfirm(data)}
-          buttonStyle="danger"
-          formElements={this.props.formElements}>
-          {this.props.children}
-        </FormModal>
-      </span>
+const ModalButton = (props) => {
+  let buttonWithMaybeTooltip;
+  let modal;
+  if (props.tooltipText) {
+    buttonWithMaybeTooltip = (
+      <OverlayTrigger
+        placement="top"
+        overlay={<Tooltip id="overlay">{props.tooltipText}</Tooltip>}>
+        <a onClick={() => modal.show()} >{props.buttonChildren}</a>
+      </OverlayTrigger>
     );
+  } else {
+    buttonWithMaybeTooltip = <a onClick={() => modal.show()} >{props.buttonChildren}</a>;
   }
-}
+  return (
+    <span>
+      {buttonWithMaybeTooltip}
+      <FormModal
+        ref={modalRef => { modal = modalRef; }}
+        action={props.action}
+        onConfirm={(data) => props.onConfirm(data)}
+        buttonStyle="danger"
+        formElements={props.formElements}>
+        {props.children}
+      </FormModal>
+    </span>
+  );
+};
 
 ModalButton.propTypes = {
   buttonChildren: PropTypes.node.isRequired,
