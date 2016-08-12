70ce67cd13ca6a17ec3897ec30bc2e13f7b2c0d4
Matthew Cotton <mcotton@hubspot.com>
2016-08-12 15:13:01 -0400

    Bind the UserResource class

--- SingularityService/src/main/java/com/hubspot/singularity/resources/SingularityResourceModule.java
+++ SingularityService/src/main/java/com/hubspot/singularity/resources/SingularityResourceModule.java
@@ -38,6 +38,7 @@ public class SingularityResourceModule extends AbstractModule {
     bind(WebhookResource.class);
     bind(AuthResource.class);
     bind(MetricsResource.class);
+    bind(UserResource.class);
 
     switch (uiConfiguration.getRootUrlMode()) {
     case UI_REDIRECT: {
