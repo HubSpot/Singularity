59c4f368ffdf3f844d82a2a8f719d129f93fb3ac
Matthew Cotton <mcotton@hubspot.com>
2016-08-12 09:32:10 -0400

    Implement ZK methods for UserSettings

--- SingularityService/src/main/java/com/hubspot/singularity/data/UserManager.java
+++ SingularityService/src/main/java/com/hubspot/singularity/data/UserManager.java
@@ -1,16 +1,48 @@
 package com.hubspot.singularity.data;
 
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.utils.ZKPaths;
 
 import com.codahale.metrics.MetricRegistry;
+import com.google.common.base.Optional;
 import com.google.inject.Inject;
+import com.hubspot.singularity.SingularityUserSettings;
 import com.hubspot.singularity.config.SingularityConfiguration;
+import com.hubspot.singularity.data.transcoders.Transcoder;
 
 public class UserManager extends CuratorManager {
 
+  private final Transcoder<SingularityUserSettings> settingsTranscoder;
+
+  private static final String USER_ROOT = "/users";
+
+  private static final String SETTINGS_ROOT = USER_ROOT + "/settings";
+
   @Inject
-  public UserManager(CuratorFramework curator, SingularityConfiguration configuration, MetricRegistry metricRegistry) {
+  public UserManager(CuratorFramework curator, SingularityConfiguration configuration, MetricRegistry metricRegistry,
+                     Transcoder<SingularityUserSettings> settingsTranscoder) {
     super(curator, configuration, metricRegistry);
+    this.settingsTranscoder = settingsTranscoder;
+  }
+
+  private String getUserSettingsPath(String id) {
+    return ZKPaths.makePath(SETTINGS_ROOT, id);
+  }
+
+  public void createUserSettings(String id, SingularityUserSettings userSettings) {
+    create(getUserSettingsPath(id), userSettings, settingsTranscoder);
+  }
+
+  public void deleteUserSettings(String id) {
+    delete(getUserSettingsPath(id));
+  }
+
+  public Optional<SingularityUserSettings> getUserSettings(String id) {
+    return getData(getUserSettingsPath(id), settingsTranscoder);
+  }
+
+  public void updateUserSettings(String id, SingularityUserSettings userSettings) {
+    save(getUserSettingsPath(id), userSettings, settingsTranscoder);
   }
 
 }
