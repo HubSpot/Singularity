3f0d858d7e3d6fe54c7bbc59b1fab3648cff0fad
Kevin Moses <kmoses@hubspot.com>
2016-08-12 15:10:33 -0400

    buttons and modals'

--- SingularityUI/app/components/groupDetail/GroupDetail.jsx
+++ SingularityUI/app/components/groupDetail/GroupDetail.jsx
@@ -4,54 +4,84 @@ import rootComponent from '../../rootComponent';
 
 import { FetchGroups } from '../../actions/api/requestGroups';
 
-import { Row, Col, Tabs, Tab } from 'react-bootstrap';
+import { Row, Col, Tabs, Tab, DropdownButton, MenuItem } from 'react-bootstrap';
 import RequestDetailPage from '../requestDetail/RequestDetailPage';
 import MetadataButton from '../common/MetadataButton';
+import PauseButton from '../requests/PauseButton';
+import UnpauseButton from '../requests/UnpauseButton';
+import BounceButton from '../requests/BounceButton';
 
-const GroupDetail = ({group, location}) => {
-  const tabs = group.requestIds.map((requestId, index) => {
-    return (
-      <Tab key={index} eventKey={index} title={requestId}>
-        <div className="tab-container">
-          <RequestDetailPage index={index} params={{requestId}} location={location} showBreadcrumbs={false} />
-        </div>
-      </Tab>
+class GroupDetail extends React.Component {
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log(nextState, this.state);
+  //   return !_.isEqual(nextProps.requests, this.props.requests);
+  // }
+
+  render() {
+    const {group, requests, location} = this.props;
+
+    const tabs = group.requestIds.map((requestId, index) => {
+      return (
+        <Tab key={index} eventKey={index} title={requestId}>
+          <div className="tab-container">
+            <RequestDetailPage index={index} params={{requestId}} location={location} showBreadcrumbs={false} />
+          </div>
+        </Tab>
+      );
+    });
+    const metadata = !_.isEmpty(group.metadata) && (
+      <MetadataButton title={group.id} metadata={group.metadata}>View Metadata</MetadataButton>
     );
-  });
-  const metadata = !_.isEmpty(group.metadata) && (
-    <MetadataButton title={group.id} metadata={group.metadata}>View Metadata</MetadataButton>
-  );
-
-  return (
-    <div>
-      <Row className="detail-header">
-        <Col md={7} lg={6}>
-          <h1>{group.id}</h1>
-        </Col>
-        <Col md={5} lg={6} className="button-container">
-          {metadata}
-        </Col>
-      </Row>
-      <Tabs id="request-ids">
-        {tabs}
-      </Tabs>
-    </div>
-  );
-};
+    const dropdown = (
+      <DropdownButton bsStyle="primary" title="Apply to all" id="action-dropdown">
+        <PauseButton requestId={_.first(group.requestIds)} isScheduled={false}>
+          <MenuItem eventKey="1">Pause</MenuItem>
+        </PauseButton>
+        <UnpauseButton requestId={_.first(group.requestIds)} isScheduled={false}>
+          <MenuItem eventKey="2">Unpause</MenuItem>
+        </UnpauseButton>
+        <BounceButton requestId={_.first(group.requestIds)}>
+          <MenuItem eventKey="3">Bounce</MenuItem>
+        </BounceButton>
+      </DropdownButton>
+    );
+    console.log(requests);
+    return (
+      <div>
+        <Row className="detail-header">
+          <Col md={7} lg={6}>
+            <h1>{group.id}</h1>
+          </Col>
+          <Col md={5} lg={6} className="button-container">
+            {dropdown}
+            {metadata}
+          </Col>
+        </Row>
+        <Tabs id="request-ids">
+          {tabs}
+        </Tabs>
+      </div>
+    );
+  }
+}
 
 GroupDetail.propTypes = {
   group: PropTypes.object,
-  location: PropTypes.object
+  location: PropTypes.object,
+  requests: PropTypes.array
 };
 
-function mapStateToProps(state, ownProps) {
+const mapStateToProps = (state, ownProps) => {
   const group = _.find(state.api.requestGroups.data, (filterGroup) => filterGroup.id === ownProps.params.groupId);
+  const requests = group && _.pick({...state.api.request}, (value, key) => _.contains(group.requestIds, key));
   return ({
     notFound: !state.api.requestGroups.isFetching && !group,
     pathname: ownProps.location.pathname,
+    // requests,
     group
   });
-}
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
