59742e40430095181cbb2cf74a56b4ac7bd34641
Matthew Cotton <mcotton@hubspot.com>
2016-08-12 11:04:04 -0400

    Implement UserResource class

--- SingularityService/src/main/java/com/hubspot/singularity/resources/UserResource.java
+++ SingularityService/src/main/java/com/hubspot/singularity/resources/UserResource.java
@@ -1,4 +1,37 @@
 package com.hubspot.singularity.resources;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.common.base.Optional;
+import com.google.inject.Inject;
+import com.hubspot.singularity.SingularityService;
+import com.hubspot.singularity.SingularityUser;
+import com.hubspot.singularity.SingularityUserSettings;
+import com.hubspot.singularity.data.UserManager;
+
+@Path(UserResource.PATH)
+@Produces({ MediaType.APPLICATION_JSON })
 public class UserResource {
+  public static final String PATH = SingularityService.API_BASE_PATH + "/user";
+
+  private final Optional<SingularityUser> user;
+  private final UserManager userManager;
+
+  @Inject
+  public UserResource(Optional<SingularityUser> user, UserManager userManager) {
+    this.user = user;
+    this.userManager = userManager;
+  }
+
+  @GET
+  @Path("/settings")
+  public Optional<SingularityUserSettings> getUser() {
+    if (user.isPresent()) {
+      return userManager.getUserSettings(user.get().getId());
+    }
+    return Optional.absent();
+  }
 }
