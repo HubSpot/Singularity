f3c0aa71209eb26a0895cbac5b3f73823a1486c6
Matthew Cotton <mcotton@hubspot.com>
2016-08-15 16:21:03 -0400

    Inject SUserManager into AuthResource and use to grab Optional<SUserSettings>

--- SingularityService/src/main/java/com/hubspot/singularity/resources/AuthResource.java
+++ SingularityService/src/main/java/com/hubspot/singularity/resources/AuthResource.java
@@ -10,7 +10,9 @@ import com.google.inject.Inject;
 import com.hubspot.singularity.SingularityService;
 import com.hubspot.singularity.SingularityUser;
 import com.hubspot.singularity.SingularityUserHolder;
+import com.hubspot.singularity.SingularityUserSettings;
 import com.hubspot.singularity.config.SingularityConfiguration;
+import com.hubspot.singularity.data.UserManager;
 
 @Path(AuthResource.PATH)
 @Produces({ MediaType.APPLICATION_JSON })
@@ -18,18 +20,25 @@ public class AuthResource {
   public static final String PATH = SingularityService.API_BASE_PATH + "/auth";
 
   private final Optional<SingularityUser> user;
+  private final UserManager userManager;
   private final SingularityConfiguration configuration;
 
   @Inject
   public AuthResource(Optional<SingularityUser> user,
-      SingularityConfiguration configuration) {
+                      UserManager userManager,
+                      SingularityConfiguration configuration) {
     this.user = user;
+    this.userManager = userManager;
     this.configuration = configuration;
   }
 
   @GET
   @Path("/user")
   public SingularityUserHolder getUser() {
-    return new SingularityUserHolder(user, user.isPresent(), configuration.getAuthConfiguration().isEnabled());
+    Optional<SingularityUserSettings> userSettingsOptional = Optional.absent();
+    if (user.isPresent()) {
+      userSettingsOptional = userManager.getUserSettings(user.get().getId());
+    }
+    return new SingularityUserHolder(user, userSettingsOptional, user.isPresent(), configuration.getAuthConfiguration().isEnabled());
   }
 }
