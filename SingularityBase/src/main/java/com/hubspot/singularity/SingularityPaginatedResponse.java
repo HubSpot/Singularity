284b9063423b287d32d0be8ea78140a672fa9921
Stephen Salinas <ssalinas@hubspot.com>
2016-08-26 14:02:59 -0400

    default page to 1

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityPaginatedResponse.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityPaginatedResponse.java
@@ -9,13 +9,13 @@ public class SingularityPaginatedResponse<Q> {
 
   private final Optional<Integer> dataCount;
   private final Optional<Integer> pageCount;
-  private final Optional<Integer> page;
+  private final int page;
   private final List<Q> objects;
 
   public SingularityPaginatedResponse(final Optional<Integer> dataCount, final Optional<Integer> pageCount, final Optional<Integer> page, final List<Q> objects) {
     this.dataCount = dataCount;
     this.pageCount = pageCount;
-    this.page = page;
+    this.page = page.or(1);
     this.objects = objects;
   }
 
@@ -27,7 +27,7 @@ public class SingularityPaginatedResponse<Q> {
     return pageCount;
   }
 
-  public Optional<Integer> getPage() {
+  public int getPage() {
     return page;
   }
 
