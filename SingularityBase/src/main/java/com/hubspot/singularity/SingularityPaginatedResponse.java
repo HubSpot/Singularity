c72f6fe1b1627c2fe42db7ac33bbd1db8ee359a2
Stephen Salinas <ssalinas@hubspot.com>
2016-08-26 14:01:24 -0400

    data -> objects

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityPaginatedResponse.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityPaginatedResponse.java
@@ -1,5 +1,6 @@
 package com.hubspot.singularity;
 
+import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 
 import java.util.List;
@@ -9,13 +10,13 @@ public class SingularityPaginatedResponse<Q> {
   private final Optional<Integer> dataCount;
   private final Optional<Integer> pageCount;
   private final Optional<Integer> page;
-  private final List<Q> data;
+  private final List<Q> objects;
 
-  public SingularityPaginatedResponse(final Optional<Integer> dataCount, final Optional<Integer> pageCount, final Optional<Integer> page, final List<Q> data) {
+  public SingularityPaginatedResponse(final Optional<Integer> dataCount, final Optional<Integer> pageCount, final Optional<Integer> page, final List<Q> objects) {
     this.dataCount = dataCount;
     this.pageCount = pageCount;
     this.page = page;
-    this.data = data;
+    this.objects = objects;
   }
 
   public Optional<Integer> getDataCount() {
@@ -30,7 +31,17 @@ public class SingularityPaginatedResponse<Q> {
     return page;
   }
 
-  public List<Q> getData() {
-    return data;
+  public List<Q> getObjects() {
+    return objects;
+  }
+
+  @Override
+  public String toString() {
+    return Objects.toStringHelper(this)
+      .add("dataCount", dataCount)
+      .add("pageCount", pageCount)
+      .add("page", page)
+      .add("objects", objects)
+      .toString();
   }
 }
