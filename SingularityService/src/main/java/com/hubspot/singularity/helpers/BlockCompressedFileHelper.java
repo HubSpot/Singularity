10ed945c651ae6248c0ce5a9daa4ccdfc7ecb2f4
Stephen Salinas <ssalinas@hubspot.com>
2016-09-08 11:53:54 -0400

    working read endpoint

--- SingularityService/src/main/java/com/hubspot/singularity/helpers/BlockCompressedFileHelper.java
+++ SingularityService/src/main/java/com/hubspot/singularity/helpers/BlockCompressedFileHelper.java
@@ -5,20 +5,25 @@ import java.net.URL;
 
 import htsjdk.samtools.util.BlockCompressedInputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.base.Optional;
 import com.hubspot.mesos.json.MesosFileChunkObject;
 import com.hubspot.singularity.WebExceptions;
 
 public class BlockCompressedFileHelper {
+  private static final Logger LOG = LoggerFactory.getLogger(BlockCompressedFileHelper.class);
+
   public static MesosFileChunkObject getAndDecompressFromUrl(URL url, Optional<Long> offset, int length) {
     try {
       BlockCompressedInputStream stream = new BlockCompressedInputStream(url);
-      BlockCompressedInputStream.isValidFile(stream);
       if (offset.isPresent()) {
         stream.seek(offset.get());
       }
       byte[] bytes = new byte[length];
       int bytesRead = stream.read(bytes, 0, length);
+      LOG.trace("Read {} bytes at offset {} for log at {}", bytesRead, offset, url.getPath());
       long newOffset = stream.getPosition();
       return new MesosFileChunkObject(new String(bytes), offset.or(0L), Optional.of(newOffset));
     } catch (IOException ioe) {
