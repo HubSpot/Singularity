f0616f9424de05b4f95756ef285bc431eafb0a6f
Stephen Salinas <ssalinas@hubspot.com>
2016-09-27 12:12:40 -0400

    handle single instance case

--- SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularitySlaveAndRackManager.java
+++ SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularitySlaveAndRackManager.java
@@ -198,7 +198,7 @@ class SingularitySlaveAndRackManager {
     int racksAccountedFor = countPerRack.elementSet().size();
     double numPerRack = numDesiredInstances / (double) stateCache.getNumActiveRacks();
     if (racksAccountedFor < stateCache.getNumActiveRacks()) {
-      if (countPerRack.count(sanitizedRackId) < (int) numPerRack) {
+      if (countPerRack.count(sanitizedRackId) < Math.max(numPerRack, 1)) {
         return true;
       }
     } else {
