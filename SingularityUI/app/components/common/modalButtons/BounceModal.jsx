a6d076604318a1279efee8cccdc0c2a4c7ac2748
Stephen Salinas <ssalinas@hubspot.com>
2016-10-18 10:41:36 -0400

    don't show shell command option if none are configured

--- SingularityUI/app/components/common/modalButtons/BounceModal.jsx
+++ SingularityUI/app/components/common/modalButtons/BounceModal.jsx
@@ -27,6 +27,59 @@ class BounceModal extends Component {
   };
 
   render() {
+    let formElements = [
+      {
+        name: 'incremental',
+        type: FormModal.INPUT_TYPES.RADIO,
+        values: _.values(BounceModal.INCREMENTAL_BOUNCE_VALUE),
+        defaultValue: BounceModal.INCREMENTAL_BOUNCE_VALUE.INCREMENTAL.value
+      },
+      {
+        name: 'skipHealthchecks',
+        type: FormModal.INPUT_TYPES.BOOLEAN,
+        label: 'Skip healthchecks during bounce'
+      }
+    ]
+
+    if (config.shellCommands.length > 0) {
+      formElements.push(
+        {
+        name: 'runShellCommand',
+        type: FormModal.INPUT_TYPES.BOOLEAN,
+        label: 'Run shell command before killing tasks',
+        defaultValue: false
+        },
+        {
+          name: 'runBeforeKill',
+          type: FormModal.INPUT_TYPES.SELECT,
+          dependsOn: 'runShellCommand',
+          options: config.shellCommands.map((shellCommand) => ({
+            label: shellCommand.name,
+            value: shellCommand.name
+          }))
+        }
+      );
+    }
+
+    formElements.push(
+      {
+        name: 'durationMillis',
+        type: FormModal.INPUT_TYPES.DURATION,
+        label: 'Expiration (optional)',
+        help: (
+          <div>
+            <p>If an expiration duration is specified, this bounce will be aborted if not finished.</p>
+            <p>Default value {config.defaultBounceExpirationMinutes} minutes</p>
+          </div>
+        )
+      },
+      {
+        name: 'message',
+        type: FormModal.INPUT_TYPES.STRING,
+        label: 'Message (optional)'
+      }
+    );
+
     return (
       <FormModal
         name="Bounce Request"
@@ -39,50 +92,7 @@ class BounceModal extends Component {
           this.props.bounceRequest(data)
         }}
         buttonStyle="primary"
-        formElements={[
-          {
-            name: 'incremental',
-            type: FormModal.INPUT_TYPES.RADIO,
-            values: _.values(BounceModal.INCREMENTAL_BOUNCE_VALUE),
-            defaultValue: BounceModal.INCREMENTAL_BOUNCE_VALUE.INCREMENTAL.value
-          },
-          {
-            name: 'skipHealthchecks',
-            type: FormModal.INPUT_TYPES.BOOLEAN,
-            label: 'Skip healthchecks during bounce'
-          },
-          {
-            name: 'runShellCommand',
-            type: FormModal.INPUT_TYPES.BOOLEAN,
-            label: 'Run shell command before killing tasks',
-            defaultValue: false
-          },
-          {
-            name: 'runBeforeKill',
-            type: FormModal.INPUT_TYPES.SELECT,
-            dependsOn: 'runShellCommand',
-            options: config.shellCommands.map((shellCommand) => ({
-              label: shellCommand.name,
-              value: shellCommand.name
-            }))
-          },
-          {
-            name: 'durationMillis',
-            type: FormModal.INPUT_TYPES.DURATION,
-            label: 'Expiration (optional)',
-            help: (
-              <div>
-                <p>If an expiration duration is specified, this bounce will be aborted if not finished.</p>
-                <p>Default value {config.defaultBounceExpirationMinutes} minutes</p>
-              </div>
-            )
-          },
-          {
-            name: 'message',
-            type: FormModal.INPUT_TYPES.STRING,
-            label: 'Message (optional)'
-          }
-        ]}>
+        formElements={formElements}>
         <p>Are you sure you want to bounce this request?</p>
         <pre>{this.props.requestId}</pre>
         <p>Bouncing a request will cause replacement tasks to be scheduled and (under normal conditions) executed immediately.</p>
