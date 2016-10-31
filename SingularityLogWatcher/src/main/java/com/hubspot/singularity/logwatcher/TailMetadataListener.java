ed7aa8d386adfc395e6cc8e2a73c1e921438d09d
Stephen Salinas <ssalinas@hubspot.com>
2016-10-31 16:40:00 -0400

    Remove OOMKiller and LogWatcher Modules

--- SingularityLogWatcher/src/main/java/com/hubspot/singularity/logwatcher/TailMetadataListener.java
+++
@@ -1,9 +0,0 @@
-package com.hubspot.singularity.logwatcher;
-
-import com.hubspot.singularity.runner.base.shared.TailMetadata;
-
-public interface TailMetadataListener {
-
-  void tailChanged(TailMetadata tailMetadata);
-
-}
