bc4c2817cdb5f420dc5c0c2c56c778aebe715da7
Stephen Salinas <ssalinas@hubspot.com>
2016-12-09 13:47:57 -0500

    gitbook publish fixes

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -9,6 +9,9 @@ gitbook build
 cp SingularityUI/app/assets/static/images/favicon.ico _book/gitbook/images/favicon.ico
 cd _book
 echo 'getsingularity.com' > CNAME
+rm -rf Docs/releases/*.md
+rm -rf Docs/reference/*.md
+rm -rf README.md
 git init
 git add .
 git commit -m "update gitbook from master branch docs"
