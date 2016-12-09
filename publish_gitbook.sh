dda7200e64bed4bd2b97c83c1b9a9062d000da74
Stephen Salinas <ssalinas@hubspot.com>
2016-12-09 13:31:07 -0500

    updates to release and docs scripts

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -4,7 +4,6 @@ set -e
 
 mvn -Pbuild-swagger-documentation -DskipTests=true -B -q -fae install
 python Docs/split_api_docs.py
-sed -i '' 's/"-sharing",/"-sharing", "-livereload",/g' book.json
 gitbook install
 gitbook build
 cp SingularityUI/app/assets/static/images/favicon.ico _book/gitbook/images/favicon.ico
