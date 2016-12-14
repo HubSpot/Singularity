707a1f19f7308e903f3af7eec1ebb3cd80bb9524
Stephen Salinas <ssalinas@hubspot.com>
2016-12-14 11:01:27 -0500

    save bounce bool on expiring object

--- SingularityBase/src/main/java/com/hubspot/singularity/expiring/SingularityExpiringScale.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/expiring/SingularityExpiringScale.java
@@ -7,22 +7,28 @@ import com.hubspot.singularity.api.SingularityScaleRequest;
 public class SingularityExpiringScale extends SingularityExpiringParent<SingularityScaleRequest> {
 
   private final Optional<Integer> revertToInstances;
+  private final Optional<Boolean> bounce;
 
   public SingularityExpiringScale(@JsonProperty("requestId") String requestId, @JsonProperty("user") Optional<String> user,
       @JsonProperty("startMillis") long startMillis, @JsonProperty("expiringAPIRequestObject") SingularityScaleRequest scaleRequest, @JsonProperty("revertToInstances") Optional<Integer> revertToInstances,
-      @JsonProperty("actionId") String actionId) {
+      @JsonProperty("actionId") String actionId, @JsonProperty("bounce") Optional<Boolean> bounce) {
     super(scaleRequest, requestId, user, startMillis, actionId);
 
     this.revertToInstances = revertToInstances;
+    this.bounce = bounce;
   }
 
   public Optional<Integer> getRevertToInstances() {
     return revertToInstances;
   }
 
+  public Optional<Boolean> getBounce() {
+    return bounce;
+  }
+
   @Override
   public String toString() {
-    return "SingularityExpiringScale [revertToInstances=" + revertToInstances + "]";
+    return "SingularityExpiringScale [revertToInstances=" + revertToInstances + ", bounce=" + bounce + "]";
   }
 
 }
