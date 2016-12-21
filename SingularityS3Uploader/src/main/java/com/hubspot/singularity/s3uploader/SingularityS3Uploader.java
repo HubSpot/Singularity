6bc363c9a964b1b0e643e7574ceef974f14bac8a
Stephen Salinas <ssalinas@hubspot.com>
2016-12-21 10:59:42 -0500

    more uploader logging, fix logrotate conf

--- SingularityS3Uploader/src/main/java/com/hubspot/singularity/s3uploader/SingularityS3Uploader.java
+++ SingularityS3Uploader/src/main/java/com/hubspot/singularity/s3uploader/SingularityS3Uploader.java
@@ -243,15 +243,21 @@ public class SingularityS3Uploader implements Closeable {
         S3Object object = new S3Object(s3Bucket, file.toFile());
         object.setKey(key);
 
-        if (FileSystems.getDefault().supportedFileAttributeViews().contains("user")) {
+        Set<String> supportedViews = FileSystems.getDefault().supportedFileAttributeViews();
+        LOG.trace("Supported attribute views are {}", supportedViews);
+        if (supportedViews.contains("user")) {
           try {
-            Optional<Long> maybeStartTime = readFileAttributeAsLong(LOG_START_TIME_ATTR);
+            UserDefinedFileAttributeView view = Files.getFileAttributeView(file, UserDefinedFileAttributeView.class);
+            LOG.debug("Found file attributes {} for file {}", view.list(), file);
+            Optional<Long> maybeStartTime = readFileAttributeAsLong(LOG_START_TIME_ATTR, view);
             if (maybeStartTime.isPresent()) {
               object.getMetadataMap().put(SingularityS3Log.LOG_START_S3_ATTR, maybeStartTime.get());
+              LOG.debug("Added extra metadata for object ({}:{})", SingularityS3Log.LOG_START_S3_ATTR, maybeStartTime.get());
             }
-            Optional<Long> maybeEndTime = readFileAttributeAsLong(LOG_END_TIME_ATTR);
+            Optional<Long> maybeEndTime = readFileAttributeAsLong(LOG_END_TIME_ATTR, view);
             if (maybeEndTime.isPresent()) {
               object.getMetadataMap().put(SingularityS3Log.LOG_END_S3_ATTR, maybeEndTime.get());
+              LOG.debug("Added extra metadata for object ({}:{})", SingularityS3Log.LOG_END_S3_ATTR, maybeEndTime.get());
             }
           } catch (Exception e) {
             LOG.error("Could not get extra file metadata for {}", file, e);
@@ -286,9 +292,9 @@ public class SingularityS3Uploader implements Closeable {
       return true;
     }
 
-    private Optional<Long> readFileAttributeAsLong(String attribute) {
+    private Optional<Long> readFileAttributeAsLong(String attribute, UserDefinedFileAttributeView view) {
       try {
-        UserDefinedFileAttributeView view = Files.getFileAttributeView(file, UserDefinedFileAttributeView.class);
+        LOG.trace("Attempting to read attribute {}, from file {}", attribute, file);
         ByteBuffer buf = ByteBuffer.allocate(view.size(attribute));
         view.read(attribute, buf);
         buf.flip();
