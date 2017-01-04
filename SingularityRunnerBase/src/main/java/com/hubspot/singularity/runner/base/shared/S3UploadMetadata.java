13d2fe94ec83f4e11b43844e67aeb7674952e07e
Stephen Salinas <ssalinas@hubspot.com>
2017-01-04 14:09:56 -0500

    Move S3 file configuration to SingularityService

--- SingularityRunnerBase/src/main/java/com/hubspot/singularity/runner/base/shared/S3UploadMetadata.java
+++ SingularityRunnerBase/src/main/java/com/hubspot/singularity/runner/base/shared/S3UploadMetadata.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
+import com.hubspot.singularity.SingularityS3FormatHelper;
 
 /**
  *
@@ -23,6 +24,7 @@ import com.google.common.base.Preconditions;
  * %guid - adds a guid
  * %host - adds the hostname
  * %index - adds the index of the file uploaded at this moment (to preserve uniqueness)
+ * %group - request group name or 'default'
  *
  * For example, if the s3KeyFormat was: %filename-%Y and the file name on local disk was "file1.txt" the S3 key would be : s3Bucket/file1.txt-2015 (assuming current year is 2015)
  *
@@ -49,12 +51,13 @@ public class S3UploadMetadata {
   private final Optional<Long> finishedAfterMillisWithoutNewFile;
   private final Optional<String> s3StorageClass;
   private final Optional<Long> applyStorageClassIfOverBytes;
+  private final String groupName;
 
   @JsonCreator
   public S3UploadMetadata(@JsonProperty("directory") String directory, @JsonProperty("fileGlob") String fileGlob, @JsonProperty("s3Bucket") String s3Bucket, @JsonProperty("s3KeyFormat") String s3KeyFormat,
       @JsonProperty("finished") boolean finished, @JsonProperty("onFinishGlob") Optional<String> onFinishGlob, @JsonProperty("pid") Optional<Integer> pid, @JsonProperty("s3AccessKey") Optional<String> s3AccessKey,
       @JsonProperty("s3SecretKey") Optional<String> s3SecretKey, @JsonProperty("finishedAfterMillisWithoutNewFile") Optional<Long> finishedAfterMillisWithoutNewFile,
-      @JsonProperty("storageClass") Optional<String> s3StorageClass, @JsonProperty("applyStorageClassIfOverBytes") Optional<Long> applyStorageClassIfOverBytes) {
+      @JsonProperty("storageClass") Optional<String> s3StorageClass, @JsonProperty("applyStorageClassIfOverBytes") Optional<Long> applyStorageClassIfOverBytes, @JsonProperty("groupName") Optional<String> groupName) {
     Preconditions.checkNotNull(directory);
     Preconditions.checkNotNull(fileGlob);
     Preconditions.checkNotNull(s3Bucket);
@@ -72,44 +75,67 @@ public class S3UploadMetadata {
     this.s3StorageClass = s3StorageClass;
     this.finishedAfterMillisWithoutNewFile = finishedAfterMillisWithoutNewFile;
     this.applyStorageClassIfOverBytes = applyStorageClassIfOverBytes;
+    this.groupName = groupName.or(SingularityS3FormatHelper.DEFAULT_GROUP_NAME);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    S3UploadMetadata that = (S3UploadMetadata) o;
+
+    if (finished != that.finished) {
+      return false;
+    }
+    if (directory != null ? !directory.equals(that.directory) : that.directory != null) {
+      return false;
+    }
+    if (fileGlob != null ? !fileGlob.equals(that.fileGlob) : that.fileGlob != null) {
+      return false;
+    }
+    if (s3Bucket != null ? !s3Bucket.equals(that.s3Bucket) : that.s3Bucket != null) {
+      return false;
+    }
+    if (s3KeyFormat != null ? !s3KeyFormat.equals(that.s3KeyFormat) : that.s3KeyFormat != null) {
+      return false;
+    }
+    if (onFinishGlob != null ? !onFinishGlob.equals(that.onFinishGlob) : that.onFinishGlob != null) {
+      return false;
+    }
+    if (pid != null ? !pid.equals(that.pid) : that.pid != null) {
+      return false;
+    }
+    if (s3AccessKey != null ? !s3AccessKey.equals(that.s3AccessKey) : that.s3AccessKey != null) {
+      return false;
+    }
+    if (s3SecretKey != null ? !s3SecretKey.equals(that.s3SecretKey) : that.s3SecretKey != null) {
+      return false;
+    }
+    if (finishedAfterMillisWithoutNewFile != null ? !finishedAfterMillisWithoutNewFile.equals(that.finishedAfterMillisWithoutNewFile) : that.finishedAfterMillisWithoutNewFile != null) {
+      return false;
+    }
+    return groupName != null ? groupName.equals(that.groupName) : that.groupName == null;
   }
 
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((directory == null) ? 0 : directory.hashCode());
-    result = prime * result + ((fileGlob == null) ? 0 : fileGlob.hashCode());
+    int result = directory != null ? directory.hashCode() : 0;
+    result = 31 * result + (fileGlob != null ? fileGlob.hashCode() : 0);
+    result = 31 * result + (s3Bucket != null ? s3Bucket.hashCode() : 0);
+    result = 31 * result + (s3KeyFormat != null ? s3KeyFormat.hashCode() : 0);
+    result = 31 * result + (finished ? 1 : 0);
+    result = 31 * result + (onFinishGlob != null ? onFinishGlob.hashCode() : 0);
+    result = 31 * result + (pid != null ? pid.hashCode() : 0);
+    result = 31 * result + (s3AccessKey != null ? s3AccessKey.hashCode() : 0);
+    result = 31 * result + (s3SecretKey != null ? s3SecretKey.hashCode() : 0);
+    result = 31 * result + (finishedAfterMillisWithoutNewFile != null ? finishedAfterMillisWithoutNewFile.hashCode() : 0);
+    result = 31 * result + (groupName != null ? groupName.hashCode() : 0);
     return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    S3UploadMetadata other = (S3UploadMetadata) obj;
-    if (directory == null) {
-      if (other.directory != null) {
-        return false;
-      }
-    } else if (!directory.equals(other.directory)) {
-      return false;
-    }
-    if (fileGlob == null) {
-      if (other.fileGlob != null) {
-        return false;
-      }
-    } else if (!fileGlob.equals(other.fileGlob)) {
-      return false;
-    }
-    return true;
   }
 
   public String getDirectory() {
@@ -160,11 +186,26 @@ public class S3UploadMetadata {
     return applyStorageClassIfOverBytes;
   }
 
-  @Override
-  public String toString() {
-    return "S3UploadMetadata [directory=" + directory + ", fileGlob=" + fileGlob + ", s3Bucket=" + s3Bucket + ", s3KeyFormat=" + s3KeyFormat + ", finished=" + finished + ", onFinishGlob="
-        + onFinishGlob + ", pid=" + pid + ", s3AccessKey=" + s3AccessKey + ", s3SecretKey=" + s3SecretKey + ", finishedAfterMillisWithoutNewFile=" + finishedAfterMillisWithoutNewFile
-        + ", s3StorageClass=" + s3StorageClass + ", applyStorageClassIfOverBytes=" + applyStorageClassIfOverBytes + "]";
+  public String getGroupName() {
+    return groupName;
   }
 
+  @Override
+  public String toString() {
+    return "S3UploadMetadata{" +
+        "directory='" + directory + '\'' +
+        ", fileGlob='" + fileGlob + '\'' +
+        ", s3Bucket='" + s3Bucket + '\'' +
+        ", s3KeyFormat='" + s3KeyFormat + '\'' +
+        ", finished=" + finished +
+        ", onFinishGlob=" + onFinishGlob +
+        ", pid=" + pid +
+        ", s3AccessKey=" + s3AccessKey +
+        ", s3SecretKey=" + s3SecretKey +
+        ", finishedAfterMillisWithoutNewFile=" + finishedAfterMillisWithoutNewFile +
+        ", s3StorageClass=" + s3StorageClass +
+        ", applyStorageClassIfOverBytes=" + applyStorageClassIfOverBytes +
+        ", groupName='" + groupName + '\'' +
+        '}';
+  }
 }
