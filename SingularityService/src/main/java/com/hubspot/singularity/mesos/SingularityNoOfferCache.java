fe281a6a3959b1f6f6fde553e7762cd40d9253c2
Whitney Sorenson <wsorenson@hubspot.com>
2017-01-19 15:26:01 -0500

    introduce offer cache to allow better resource allocation

---
+++ SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityNoOfferCache.java
@@ -0,0 +1,37 @@
+package com.hubspot.singularity.mesos;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.mesos.Protos.Offer;
+import org.apache.mesos.Protos.OfferID;
+import org.apache.mesos.SchedulerDriver;
+
+import com.google.inject.Singleton;
+
+@Singleton
+public class SingularityNoOfferCache implements OfferCache {
+
+  @Override
+  public void cacheOffer(SchedulerDriver driver, long timestamp, Offer offer) {
+    driver.declineOffer(offer.getId());
+  }
+
+  @Override
+  public void rescindOffer(SchedulerDriver driver, OfferID offerId) {
+    // no-op
+  }
+
+  @Override
+  public void useOffer(OfferID offerId) {
+    // no-op
+  }
+
+  @Override
+  public List<Offer> getCachedOffers() {
+    return Collections.emptyList();
+  }
+
+
+
+}
