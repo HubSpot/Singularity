86d524cfa656907637b150a341760bb7ce518746
Peter Teixeira <pteixeira@hubspot.com>
2017-01-31 14:48:25 -0500

    Neaten up the test class w/ builders
    Use builders to create the Singularity{Request,Deploy} instances that
    were needed for testing. This lets the contructors and all of their
    `Optional.<>absent()` arguments be elided, which makes the whole think
    much, much easier to read. Thanks to Stephen for the fix.

--- SingularityService/src/test/java/com/hubspot/singularity/data/ValidatorTest.java
+++ SingularityService/src/test/java/com/hubspot/singularity/data/ValidatorTest.java
@@ -1,30 +1,16 @@
 package com.hubspot.singularity.data;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import javax.ws.rs.WebApplicationException;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.google.common.base.Optional;
 import com.google.inject.Inject;
-import com.hubspot.deploy.ExecutorData;
-import com.hubspot.deploy.HealthcheckOptions;
-import com.hubspot.mesos.Resources;
-import com.hubspot.mesos.SingularityContainerInfo;
-import com.hubspot.mesos.SingularityMesosTaskLabel;
-import com.hubspot.singularity.HealthcheckProtocol;
 import com.hubspot.singularity.RequestType;
-import com.hubspot.singularity.ScheduleType;
 import com.hubspot.singularity.SingularityDeploy;
-import com.hubspot.singularity.SingularityEmailDestination;
-import com.hubspot.singularity.SingularityEmailType;
 import com.hubspot.singularity.SingularityRequest;
+import com.hubspot.singularity.SingularityRequestBuilder;
 import com.hubspot.singularity.SingularityTestBaseNoDb;
-import com.hubspot.singularity.SlavePlacement;
 
 
 public class ValidatorTest extends SingularityTestBaseNoDb {
@@ -52,46 +38,8 @@ public class ValidatorTest extends SingularityTestBaseNoDb {
   public void itForbidsBracketCharactersInDeployIds() throws Exception {
     final String badDeployId = "deployKey[[";
 
-    SingularityDeploy singularityDeploy = new SingularityDeploy(badDeployId, badDeployId,
-        Optional.<String>absent(), Optional.<List<String>>absent(),
-        Optional.<SingularityContainerInfo>absent(), Optional.<String>absent(),
-        Optional.<String>absent(), Optional.<String>absent(),
-        Optional.<Resources>absent(), Optional.<Resources>absent(),
-        Optional.<Map<String,String>>absent(), Optional.<Map<Integer,Map<String,String>>>absent(),
-        Optional.<List<String>>absent(), Optional.<Map<String,String>>absent(),
-        Optional.<ExecutorData>absent(), Optional.<String>absent(),
-        Optional.<Long>absent(), Optional.<Map<String,String>>absent(),
-        Optional.<List<SingularityMesosTaskLabel>>absent(), Optional.<Map<Integer,Map<String,String>>>absent(),
-        Optional.<Map<Integer,List<SingularityMesosTaskLabel>>>absent(), Optional.<Long>absent(),
-        Optional.<String>absent(), Optional.<Long>absent(),
-        Optional.<Long>absent(), Optional.<Integer>absent(),
-        Optional.<Integer>absent(), Optional.<Long>absent(), Optional.<HealthcheckOptions>absent(),
-        Optional.<String>absent(), Optional.<Set<String>>absent(),
-        Optional.<Integer>absent(), Optional.<Long>absent(),
-        Optional.<Map<String,Object>>absent(), Optional.<Set<String>>absent(),
-        Optional.<List<String>>absent(), Optional.<String>absent(),
-        Optional.<String>absent(), Optional.<String>absent(),
-        Optional.<Boolean>absent(), Optional.<HealthcheckProtocol>absent(),
-        Optional.<Integer>absent(), Optional.<Integer>absent(),
-        Optional.<Boolean>absent(), Optional.<Integer>absent(),
-        Optional.<Boolean>absent(), Optional.<String>absent());
-    SingularityRequest singularityRequest = new SingularityRequest(badDeployId, RequestType.SERVICE,
-        Optional.<List<String>>absent(), Optional.<Integer>absent(),
-        Optional.<String>absent(), Optional.<Integer>absent(),
-        Optional.<Boolean>absent(), Optional.<Boolean>absent(),
-        Optional.<Long>absent(), Optional.<Long>absent(),
-        Optional.<ScheduleType>absent(), Optional.<String>absent(),
-        Optional.<String>absent(), Optional.<List<String>>absent(),
-        Optional.<SlavePlacement>absent(), Optional.<Map<String,String>>absent(),
-        Optional.<Map<String,String>>absent(), Optional.<Long>absent(),
-        Optional.<Long>absent(), Optional.<String>absent(),
-        Optional.<Set<String>>absent(), Optional.<Set<String>>absent(),
-        Optional.<Boolean>absent(), Optional.<Boolean>absent(),
-        Optional.<Map<SingularityEmailType,List<SingularityEmailDestination>>>absent(),
-        Optional.<Boolean>absent(), Optional.<Boolean>absent(),
-        Optional.<String>absent(), Optional.<Boolean>absent(),
-        Optional.<Double>absent(), Optional.<Integer>absent(),
-        Optional.<Boolean>absent(), Optional.<String>absent());
+    SingularityDeploy singularityDeploy = SingularityDeploy.newBuilder(badDeployId, badDeployId).build();
+    SingularityRequest singularityRequest = new SingularityRequestBuilder(badDeployId, RequestType.SERVICE).build();
 
     validator.checkDeploy(singularityRequest, singularityDeploy);
   }
@@ -100,46 +48,8 @@ public class ValidatorTest extends SingularityTestBaseNoDb {
   public void itForbidsQuotesInDeployIds() throws Exception {
     final String badDeployId = "deployKey'";
 
-    SingularityDeploy singularityDeploy = new SingularityDeploy(badDeployId, badDeployId,
-        Optional.<String>absent(), Optional.<List<String>>absent(),
-        Optional.<SingularityContainerInfo>absent(), Optional.<String>absent(),
-        Optional.<String>absent(), Optional.<String>absent(),
-        Optional.<Resources>absent(), Optional.<Resources>absent(),
-        Optional.<Map<String,String>>absent(), Optional.<Map<Integer,Map<String,String>>>absent(),
-        Optional.<List<String>>absent(), Optional.<Map<String,String>>absent(),
-        Optional.<ExecutorData>absent(), Optional.<String>absent(),
-        Optional.<Long>absent(), Optional.<Map<String,String>>absent(),
-        Optional.<List<SingularityMesosTaskLabel>>absent(), Optional.<Map<Integer,Map<String,String>>>absent(),
-        Optional.<Map<Integer,List<SingularityMesosTaskLabel>>>absent(), Optional.<Long>absent(),
-        Optional.<String>absent(), Optional.<Long>absent(),
-        Optional.<Long>absent(), Optional.<Integer>absent(),
-        Optional.<Integer>absent(), Optional.<Long>absent(), Optional.<HealthcheckOptions>absent(),
-        Optional.<String>absent(), Optional.<Set<String>>absent(),
-        Optional.<Integer>absent(), Optional.<Long>absent(),
-        Optional.<Map<String,Object>>absent(), Optional.<Set<String>>absent(),
-        Optional.<List<String>>absent(), Optional.<String>absent(),
-        Optional.<String>absent(), Optional.<String>absent(),
-        Optional.<Boolean>absent(), Optional.<HealthcheckProtocol>absent(),
-        Optional.<Integer>absent(), Optional.<Integer>absent(),
-        Optional.<Boolean>absent(), Optional.<Integer>absent(),
-        Optional.<Boolean>absent(), Optional.<String>absent());
-    SingularityRequest singularityRequest = new SingularityRequest(badDeployId, RequestType.SERVICE,
-        Optional.<List<String>>absent(), Optional.<Integer>absent(),
-        Optional.<String>absent(), Optional.<Integer>absent(),
-        Optional.<Boolean>absent(), Optional.<Boolean>absent(),
-        Optional.<Long>absent(), Optional.<Long>absent(),
-        Optional.<ScheduleType>absent(), Optional.<String>absent(),
-        Optional.<String>absent(), Optional.<List<String>>absent(),
-        Optional.<SlavePlacement>absent(), Optional.<Map<String,String>>absent(),
-        Optional.<Map<String,String>>absent(), Optional.<Long>absent(),
-        Optional.<Long>absent(), Optional.<String>absent(),
-        Optional.<Set<String>>absent(), Optional.<Set<String>>absent(),
-        Optional.<Boolean>absent(), Optional.<Boolean>absent(),
-        Optional.<Map<SingularityEmailType,List<SingularityEmailDestination>>>absent(),
-        Optional.<Boolean>absent(), Optional.<Boolean>absent(),
-        Optional.<String>absent(), Optional.<Boolean>absent(),
-        Optional.<Double>absent(), Optional.<Integer>absent(),
-        Optional.<Boolean>absent(), Optional.<String>absent());
+    SingularityDeploy singularityDeploy = SingularityDeploy.newBuilder(badDeployId, badDeployId).build();
+    SingularityRequest singularityRequest = new SingularityRequestBuilder(badDeployId, RequestType.SERVICE).build();
 
     validator.checkDeploy(singularityRequest, singularityDeploy);
   }
