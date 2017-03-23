bef08e05fb15b36b06ec223016a9816a49e9bdf8
Stephen Salinas <ssalinas@hubspot.com>
2017-03-23 14:59:45 -0400

    fix for metrics endpoint

--- SingularityService/src/main/java/com/hubspot/singularity/resources/MetricsResource.java
+++ SingularityService/src/main/java/com/hubspot/singularity/resources/MetricsResource.java
@@ -1,10 +1,14 @@
 package com.hubspot.singularity.resources;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
 import com.google.inject.Inject;
 import com.hubspot.singularity.SingularityService;
@@ -24,6 +28,9 @@ public class MetricsResource {
 
   @GET
   public SingularityMetricsContainer getRegistry() {
-    return new SingularityMetricsContainer(registry.getMetrics());
+    Map<String, Metric> metrics = new HashMap<>(registry.getMetrics());
+    // Not an easy way to serialize this particular one since it is a lambda, exclude it for now from the endpoint
+    metrics.entrySet().removeIf((e) -> e.getKey().contains("ManagedPooledDataSource"));
+    return new SingularityMetricsContainer(metrics);
   }
 }
