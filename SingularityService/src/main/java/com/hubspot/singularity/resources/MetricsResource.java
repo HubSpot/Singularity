8bd2fcdb10b8285afd7a456479f80c19dc51ebaf
Stephen Salinas <ssalinas@hubspot.com>
2017-03-23 13:01:11 -0400

    temporary debugging

--- SingularityService/src/main/java/com/hubspot/singularity/resources/MetricsResource.java
+++ SingularityService/src/main/java/com/hubspot/singularity/resources/MetricsResource.java
@@ -1,10 +1,16 @@
 package com.hubspot.singularity.resources;
 
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
 import com.google.inject.Inject;
 import com.hubspot.singularity.SingularityService;
@@ -13,6 +19,7 @@ import com.hubspot.singularity.metrics.SingularityMetricsContainer;
 @Path(MetricsResource.PATH)
 @Produces({ MediaType.APPLICATION_JSON })
 public class MetricsResource {
+  private static final Logger LOG = LoggerFactory.getLogger(MetricsResource.class);
   public static final String PATH = SingularityService.API_BASE_PATH + "/metrics";
 
   private final MetricRegistry registry;
@@ -24,6 +31,8 @@ public class MetricsResource {
 
   @GET
   public SingularityMetricsContainer getRegistry() {
+    Map<String, Metric> metrics = registry.getMetrics();
+    LOG.debug("Found metrics: {}", metrics);
     return new SingularityMetricsContainer(registry.getMetrics());
   }
 }
