df6f4858687347f7bccd262cb8d0e926b3a8ee9e
Stephen Salinas <ssalinas@hubspot.com>
2017-03-28 17:02:30 -0400

    More disabled actions for pollers

--- SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularityDeployPoller.java
+++ SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularityDeployPoller.java
@@ -11,7 +11,9 @@ import org.slf4j.LoggerFactory;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.hubspot.mesos.JavaUtils;
+import com.hubspot.singularity.SingularityAction;
 import com.hubspot.singularity.config.SingularityConfiguration;
+import com.hubspot.singularity.data.DisasterManager;
 import com.hubspot.singularity.mesos.SingularityMesosModule;
 
 @Singleton
@@ -20,24 +22,30 @@ public class SingularityDeployPoller extends SingularityLeaderOnlyPoller {
   private static final Logger LOG = LoggerFactory.getLogger(SingularityDeployPoller.class);
 
   private final SingularityDeployChecker deployChecker;
+  private final DisasterManager disasterManager;
 
   @Inject
-  SingularityDeployPoller(SingularityDeployChecker deployChecker, SingularityConfiguration configuration, @Named(SingularityMesosModule.SCHEDULER_LOCK_NAME) final Lock lock) {
+  SingularityDeployPoller(SingularityDeployChecker deployChecker, SingularityConfiguration configuration, @Named(SingularityMesosModule.SCHEDULER_LOCK_NAME) final Lock lock, DisasterManager disasterManager) {
     super(configuration.getCheckDeploysEverySeconds(), TimeUnit.SECONDS, lock);
 
     this.deployChecker = deployChecker;
+    this.disasterManager = disasterManager;
   }
 
   @Override
   public void runActionOnPoll() {
-    final long start = System.currentTimeMillis();
+    if (!disasterManager.isDisabled(SingularityAction.RUN_DEPLOY_POLLER)) {
+      final long start = System.currentTimeMillis();
 
-    final int numDeploys = deployChecker.checkDeploys();
+      final int numDeploys = deployChecker.checkDeploys();
 
-    if (numDeploys == 0) {
-      LOG.trace("No pending deploys");
+      if (numDeploys == 0) {
+        LOG.trace("No pending deploys");
+      } else {
+        LOG.info("Checked {} deploys in {}", numDeploys, JavaUtils.duration(start));
+      }
     } else {
-      LOG.info("Checked {} deploys in {}", numDeploys, JavaUtils.duration(start));
+      LOG.warn("Deploy poller is currently disabled");
     }
   }
 }
