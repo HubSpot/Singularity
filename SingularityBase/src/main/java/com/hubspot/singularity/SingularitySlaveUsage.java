91ef78d7f775a59b6b27d97d5237a2e7050e4255
Mathurshan Vimalesvaran <mvimalesvaran@hubspot.com>
2017-05-10 16:45:25 -0400

    consistency

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularitySlaveUsage.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularitySlaveUsage.java
@@ -87,6 +87,6 @@ public class SingularitySlaveUsage {
 
   @Override
   public String toString() {
-    return "SingularitySlaveUsage [memoryBytesUsed=" + memoryBytesUsed + ", memoryMbReserved=" + memoryMbReserved + ", memoryMbTotal=" + memoryMbTotal + ", cpusUsed=" + cpusUsed + ", cpusReserved" + cpusReserved + ", cpusTotal=" + cpusTotal + ", numTasks=" + numTasks + ", timestamp=" + timestamp + "]";
+    return "SingularitySlaveUsage [memoryBytesUsed=" + memoryBytesUsed + ", memoryMbReserved=" + memoryMbReserved + ", memoryMbTotal=" + memoryMbTotal + ", cpusUsed=" + cpusUsed + ", cpusReserved=" + cpusReserved + ", cpusTotal=" + cpusTotal + ", numTasks=" + numTasks + ", timestamp=" + timestamp + "]";
   }
 }
