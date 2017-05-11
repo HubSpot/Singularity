a4ef739d9c69ad12a302dfa511103e476e2e1c6c
Peter Teixeira <pteixeira@hubspot.com>
2017-05-11 09:36:43 -0400

    Revert breaking change in enum deserialization
    There appears to have been a change right around Dropwizard 1.0
    in how enums are deserialized; my guess is that somehow `@JsonCreator`
    cascades and prevents the `FuzzyEnumModule` from being applied properly,
    but I'm not really sure. The result was that `SingularityContainerType`
    wasn't accepting lowercase `mesos`, `docker` as valid values, which was
    a breaking change from before - and in particular, prevented creating
    deploys from the UI.

--- SingularityBase/src/main/java/com/hubspot/mesos/SingularityContainerType.java
+++ SingularityBase/src/main/java/com/hubspot/mesos/SingularityContainerType.java
@@ -1,5 +1,12 @@
 package com.hubspot.mesos;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum SingularityContainerType {
-  MESOS, DOCKER
+  MESOS, DOCKER;
+
+  @JsonCreator
+  public static SingularityContainerType fromString(String name) {
+    return valueOf(name.toUpperCase());
+  }
 }
