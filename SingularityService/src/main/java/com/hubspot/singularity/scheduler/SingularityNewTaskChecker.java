bb0445c09e3ea3c6e3b2e5d3e5a1940e0147a06c
Peter Teixeira <pteixeira@hubspot.com>
2017-05-30 14:52:28 -0400

    Check that we are not removing the request for add
    Prior to adding a load-balanced task to the load balancer, check that
    - there is no existing LB state
    - If there is an existing LB state:
      - It is still unknown to Baragon
      - We aren't already tring to remove it from Baragon

--- SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularityNewTaskChecker.java
+++ SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularityNewTaskChecker.java
@@ -326,9 +326,8 @@ public class SingularityNewTaskChecker {
     final LoadBalancerRequestId loadBalancerRequestId = new LoadBalancerRequestId(task.getTaskId().getId(), LoadBalancerRequestType.ADD, Optional.<Integer> absent());
     boolean taskCleaning = taskManager.getCleanupTaskIds().contains(task.getTaskId());
 
-    if ((!lbUpdate.isPresent()
-        || lbUpdate.get().getLoadBalancerState() == BaragonRequestState.UNKNOWN)
-        && !taskCleaning) {
+
+    if ((!lbUpdate.isPresent() || unknownNotRemoving(lbUpdate.get())) && !taskCleaning) {
       taskManager.saveLoadBalancerState(task.getTaskId(), LoadBalancerRequestType.ADD,
           new SingularityLoadBalancerUpdate(BaragonRequestState.UNKNOWN, loadBalancerRequestId, Optional.<String> absent(), System.currentTimeMillis(), LoadBalancerMethod.PRE_ENQUEUE, Optional.<String> absent()));
 
@@ -381,6 +380,11 @@ public class SingularityNewTaskChecker {
     }
 
     return isOverdue;
+  }
+
+  private boolean unknownNotRemoving(SingularityLoadBalancerUpdate update) {
+    return update.getLoadBalancerState() == BaragonRequestState.UNKNOWN
+        && update.getLoadBalancerRequestId().getRequestType() != LoadBalancerRequestType.REMOVE;
   }
 
 }
