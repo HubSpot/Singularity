2d2e5ec7061fafbdb1050d1d8e1850acf61fce0c
Gowtam Lal <glal@hubspot.com>
2017-06-16 11:51:42 -0400

    Cache new offers which are leftover after launching tasks in a SingularityOfferHolder.

--- SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityMesosScheduler.java
+++ SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityMesosScheduler.java
@@ -127,9 +127,15 @@ public class SingularityMesosScheduler implements Scheduler {
         if (!offerHolder.getAcceptedTasks().isEmpty()) {
           // Here we're launching tasks by accepting every offer the master gave us for this host (i.e., all tasks in this offer holder).
           // We want to launch these tasks using only the offers absolutely necessary, and cache the rest.
-          offerHolder.launchTasks(driver, offerCache);
+          List<Offer> leftoverOffers = offerHolder.launchTasksAndGetUnusedOffers(driver);
 
-          acceptedOffers.addAll(offerHolder.getOffers().stream().map(Offer::getId).collect(Collectors.toList()));
+          leftoverOffers.forEach((o) -> {
+            offerCache.cacheOffer(driver, start, o);
+          });
+
+          List<Offer> offersAcceptedFromSlave = offerHolder.getOffers();
+          offersAcceptedFromSlave.removeAll(leftoverOffers);
+          acceptedOffers.addAll(offersAcceptedFromSlave.stream().map(Offer::getId).collect(Collectors.toList()));
         } else {
           offerHolder.getOffers().forEach((o) -> offerCache.cacheOffer(driver, start, o));
         }
