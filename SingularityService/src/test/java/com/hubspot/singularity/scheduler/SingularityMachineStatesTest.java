9088ac746272aea1e687d3590f826c96865a71f8
Gowtam Lal <glal@hubspot.com>
2017-07-05 13:13:12 -0400

    Use GREEDY placement & specially-sized offers so we don't depend on placement strategy logic for this test.

--- SingularityService/src/test/java/com/hubspot/singularity/scheduler/SingularityMachineStatesTest.java
+++ SingularityService/src/test/java/com/hubspot/singularity/scheduler/SingularityMachineStatesTest.java
@@ -331,7 +331,7 @@ public class SingularityMachineStatesTest extends SingularitySchedulerTestBase {
     initRequest();
     initFirstDeploy();
 
-    saveAndSchedule(request.toBuilder().setSlavePlacement(Optional.of(SlavePlacement.OPTIMISTIC)).setInstances(Optional.of(2)));
+    saveAndSchedule(request.toBuilder().setSlavePlacement(Optional.of(SlavePlacement.GREEDY)).setInstances(Optional.of(2)));
 
     sms.resourceOffers(driver, Arrays.asList(createOffer(1, 128, "slave1", "host1")));
     sms.resourceOffers(driver, Arrays.asList(createOffer(1, 128, "slave2", "host2")));
