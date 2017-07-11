628c1a4bc1e1e1f3696af60db6bafccff19a8e1f
Kevin Moses <kmoses@hubspot.com>
2017-07-11 11:41:54 -0400

    Convert NewTasksDropDown to class

--- SingularityUI/app/components/logs/NewTasksDropdown.jsx
+++ SingularityUI/app/components/logs/NewTasksDropdown.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const NewTasksDropdown = ({ready, runningTasks, visibleTasks=[], onToggle}) => {
-  const renderTaskItems = () => {
-    if (!ready || !runningTasks) {
+class NewTasksDropdown extends React.Component  {
+
+  renderTaskItems() {
+    if (!this.props.ready || !this.props.runningTasks) {
       return (<li><a className="disabled">Loading...</a></li>);
     }
 
-    if (runningTasks.length === 0) {
+    if (this.props.runningTasks.length === 0) {
       return (<li><a className="disabled">No running instances</a></li>);
     }
 
-    return runningTasks.map((task, key) => {
-      const checkedClass = visibleTasks.includes(task.taskId.id)
+    return this.props.runningTasks.map((task, key) => {
+      const checkedClass = this.props.visibleTasks.includes(task.taskId.id)
         ? 'glyphicon-check'
         : 'glyphicon-unchecked';
 
       return (<li key={key}>
-          <a onClick={() => onToggle(task.taskId.id)}>
+          <a onClick={() => this.props.onToggle(task.taskId.id)}>
               <span className={classNames('glyphicon', checkedClass)} />
               <span>Instance {task.taskId.instanceNo}</span>
           </a>
@@ -25,14 +26,29 @@ const NewTasksDropdown = ({ready, runningTasks, visibleTasks=[], onToggle}) => {
     });
   };
 
-  return (<div className="btn-group" title="Select Instances">
-    <button type="button" className="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <span className="glyphicon glyphicon-tasks"></span> <span className="caret"></span>
-    </button>
-    <ul className="dropdown-menu dropdown-menu-right">
-      {renderTaskItems()}
-    </ul>
-  </div>);
+  render() {
+    return (
+      <div className="btn-group" title="Select Instances">
+        <button type="button" className="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span className="glyphicon glyphicon-tasks"></span> <span className="caret"></span>
+        </button>
+        <ul className="dropdown-menu dropdown-menu-right">
+            {this.renderTaskItems()}
+        </ul>
+      </div>
+    );
+  }
+};
+
+NewTasksDropdown.propTypes = {
+  ready: React.PropTypes.bool,
+  runningTasks: React.PropTypes.array,
+  visibleTasks: React.PropTypes.array,
+  onToggle: React.PropTypes.func
+};
+
+NewTasksDropdown.defaultProps = {
+  visibleTasks: []
 };
 
 export default NewTasksDropdown;
