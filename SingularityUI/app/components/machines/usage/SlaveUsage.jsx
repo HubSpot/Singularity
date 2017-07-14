acc7a32aa4d360d498dbd1fa8940d082b81aa4f7
Kevin Moses <kmoses@hubspot.com>
2017-07-14 16:01:27 -0400

    No colon

--- SingularityUI/app/components/machines/usage/SlaveUsage.jsx
+++ SingularityUI/app/components/machines/usage/SlaveUsage.jsx
@@ -58,7 +58,7 @@ const SlaveUsage = ({slaves, slaveUsages, activeTasks, clusterUtilization, total
       </div>
       <hr />
       <h1>Cluster Usage</h1>
-      <small>Last updated: {Utils.timestampFromNow(clusterUtilization.timestamp)}</small>
+      <small>Last updated {Utils.timestampFromNow(clusterUtilization.timestamp)}</small>
       <div>
         <ClusterAggregates utilization={clusterUtilization} totalRequests={totalRequests} />
       </div>
