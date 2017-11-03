edab7aae79c635882e9846c22e005ce722fc1c75
Gowtam Lal <glal@hubspot.com>
2017-11-03 15:41:11 -0400

    Download additional artifacts as well.

--- SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityMesosTaskBuilder.java
+++ SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityMesosTaskBuilder.java
@@ -449,7 +449,10 @@ class SingularityMesosTaskBuilder {
       commandBldr.setShell(false);
     }
 
-    for (SingularityMesosArtifact artifact : task.getDeploy().getUris().or(Collections.<SingularityMesosArtifact> emptyList())) {
+    List<SingularityMesosArtifact> combinedArtifacts = task.getDeploy().getUris().or(Collections.<SingularityMesosArtifact> emptyList());
+    combinedArtifacts.addAll(task.getPendingTask().getExtraArtifacts().or(Collections.<SingularityMesosArtifact> emptyList()));
+
+    for (SingularityMesosArtifact artifact : combinedArtifacts) {
       commandBldr.addUris(URI.newBuilder()
           .setValue(artifact.getUri())
           .setCache(artifact.isCache())
