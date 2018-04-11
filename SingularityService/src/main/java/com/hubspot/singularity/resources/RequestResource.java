3a984b70496f80a59c6687146d056b5ad0f587fd
Gowtam Lal <glal@hubspot.com>
2018-04-11 17:14:08 -0400

    Proxy run-now POSTs to the leader.

--- SingularityService/src/main/java/com/hubspot/singularity/resources/RequestResource.java
+++ SingularityService/src/main/java/com/hubspot/singularity/resources/RequestResource.java
@@ -310,8 +310,8 @@ public class RequestResource extends AbstractRequestResource {
 
   @POST
   @Path("/request/{requestId}/run")
-  public SingularityPendingRequestParent scheduleImmediately(@Auth SingularityUser user, @PathParam("requestId") String requestId) {
-    return scheduleImmediately(user, requestId, null);
+  public SingularityPendingRequestParent scheduleImmediately(@Auth SingularityUser user, @PathParam("requestId") String requestId, @Context HttpServletRequest requestContext) {
+    return scheduleImmediately(user, requestId, requestContext, null);
   }
 
   @POST
@@ -323,7 +323,12 @@ public class RequestResource extends AbstractRequestResource {
   })
   public SingularityPendingRequestParent scheduleImmediately(@Auth SingularityUser user,
                                                              @ApiParam("The request ID to run") @PathParam("requestId") String requestId,
+                                                             @Context HttpServletRequest requestContext,
                                                              SingularityRunNowRequest runNowRequest) {
+    return maybeProxyToLeader(requestContext, SingularityPendingRequestParent.class, runNowRequest, () -> scheduleImmediately(user, requestId, runNowRequest));
+  }
+
+  public SingularityPendingRequestParent scheduleImmediately(SingularityUser user, String requestId, SingularityRunNowRequest runNowRequest) {
     final Optional<SingularityRunNowRequest> maybeRunNowRequest = Optional.fromNullable(runNowRequest);
     SingularityRequestWithState requestWithState = fetchRequestWithState(requestId, user);
 
