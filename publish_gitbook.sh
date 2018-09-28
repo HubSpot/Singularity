78e519539e4954930f93b545724c2a18bc0c0705
Stephen Salinas <ssalinas@hubspot.com>
2018-09-28 09:58:31 -0400

    0.21.0 release docs

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -2,15 +2,12 @@
 
 set -e
 
-mvn -Pbuild-swagger-documentation -DskipTests=true -B -q -fae install
-python Docs/split_api_docs.py
 gitbook install
 gitbook build
 cp SingularityUI/app/assets/static/images/favicon.ico _book/gitbook/images/favicon.ico
 cd _book
 echo 'getsingularity.com' > CNAME
 rm -rf Docs/releases/*.md
-rm -rf Docs/reference/*.md
 rm -rf README.md
 git init
 git add .
