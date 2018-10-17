ab683e53ff85fbd07fef50b44f193bd2d7372f5c
Stephen Salinas <ssalinas@hubspot.com>
2018-10-17 13:25:20 -0400

    missing binding?

--- SingularityService/src/main/java/com/hubspot/singularity/data/SingularityDataModule.java
+++ SingularityService/src/main/java/com/hubspot/singularity/data/SingularityDataModule.java
@@ -29,6 +29,7 @@ public class SingularityDataModule extends AbstractModule {
     bind(SingularityValidator.class).in(Scopes.SINGLETON);
     bind(UserManager.class).in(Scopes.SINGLETON);
     bind(UsageManager.class).in(Scopes.SINGLETON);
+    bind(NotificationsManager.class).in(Scopes.SINGLETON);
 
     bind(SingularityWebCache.class).in(Scopes.SINGLETON);
 
