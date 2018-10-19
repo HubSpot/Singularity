a937df0b32d7ea0e79896dae5cd58a69a109fb5b
Paul Schoenfelder <schoenfelder.p@gmail.com>
2018-10-19 14:11:54 -0400

    PR chhange

--- SingularityService/src/main/java/com/hubspot/singularity/smtp/SmtpMailer.java
+++ SingularityService/src/main/java/com/hubspot/singularity/smtp/SmtpMailer.java
@@ -737,12 +737,8 @@ public class SmtpMailer implements SingularityMailer, Managed {
       }
     }
 
-    Set<String> emailBlacklist = Sets.newHashSet(notificationsManager.getBlacklist());
-    toList.removeAll(emailBlacklist);
-    ccList.removeAll(emailBlacklist);
-
     if (actionTaker.isPresent() && !Strings.isNullOrEmpty(actionTaker.get())) {
-      if (destination.contains(SingularityEmailDestination.ACTION_TAKER) && !emailBlacklist.contains(actionTaker.get())) {
+      if (destination.contains(SingularityEmailDestination.ACTION_TAKER)) {
         toList.add(actionTaker.get());
       } else {
         final Iterator<String> i = toList.iterator();
@@ -753,6 +749,10 @@ public class SmtpMailer implements SingularityMailer, Managed {
         }
       }
     }
+
+    Set<String> emailBlacklist = Sets.newHashSet(notificationsManager.getBlacklist());
+    toList.removeAll(emailBlacklist);
+    ccList.removeAll(emailBlacklist);
 
     smtpSender.queueMail(Lists.newArrayList(toList), Lists.newArrayList(ccList), subject, body);
   }
