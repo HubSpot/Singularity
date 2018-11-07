6644558436482d6da9ef845e0ddeff8529476f37
Paul Schoenfelder <schoenfelder.p@gmail.com>
2018-11-07 12:11:58 -0500

    parens fix

--- SingularityExecutorCleanup/src/main/java/com/hubspot/singularity/executor/cleanup/SingularityExecutorCleanup.java
+++ SingularityExecutorCleanup/src/main/java/com/hubspot/singularity/executor/cleanup/SingularityExecutorCleanup.java
@@ -217,7 +217,7 @@ public class SingularityExecutorCleanup {
             oldDefinition.getExecutorData().getApplyS3StorageClassAfterBytes(),
             oldDefinition.getExecutorData().getCpuHardLimit(),
             oldDefinition.getExecutorData().getHealthCheckResultFilePath().isPresent() ? oldDefinition.getExecutorData().getHealthCheckResultFilePath() : cleanupConfiguration.getHealthCheckResultFilePath(),
-            oldDefinition.getExecutorData().getHealthCheckOptions().isPresent() ? oldDefinition.getExecutorData().getHealthCheckOptions() : cleanupConfiguration.getHealthCheckOptions(),
+            oldDefinition.getExecutorData().getHealthCheckOptions().isPresent() ? oldDefinition.getExecutorData().getHealthCheckOptions() : cleanupConfiguration.getHealthCheckOptions()
         ),
         oldDefinition.getTaskDirectory(),
         oldDefinition.getExecutorPid(),
