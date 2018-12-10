6b4099629cf3d9eec1d7ee64605d0b42df6eaee2
Sharon Jerop Kipruto <sjeropkipruto@hubspot.com>
2018-12-10 16:51:54 -0500

    add a singularity upstream poller

---
+++ SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularityUpstreamPoller.java
@@ -0,0 +1,32 @@
+package com.hubspot.singularity.scheduler;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hubspot.singularity.config.SingularityConfiguration;
+
+@Singleton
+public class SingularityUpstreamPoller extends SingularityLeaderOnlyPoller {
+  private static final Logger LOG = LoggerFactory.getLogger(SingularityUpstreamPoller.class);
+
+  private final SingularityUpstreamChecker upstreamChecker;
+
+  @Inject
+  SingularityUpstreamPoller(SingularityConfiguration configuration, SingularityUpstreamChecker upstreamChecker) {
+    super(configuration.getCheckUpstreamsEverySeconds(), TimeUnit.SECONDS, true);
+
+    this.upstreamChecker = upstreamChecker;
+
+  }
+
+  @Override
+  public void runActionOnPoll() {
+    LOG.info("Checking upstreams");
+    upstreamChecker.checkUpstreams();
+  }
+}
