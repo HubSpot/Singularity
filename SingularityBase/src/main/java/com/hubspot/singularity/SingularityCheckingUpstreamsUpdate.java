ff20842b104f7899d4ee739833ea580fbfcc949d
Sharon Jerop Kipruto <sjeropkipruto@hubspot.com>
2019-01-16 12:18:32 -0500

    remove lbResponse from SingularityCheckingUpstreamsUpdate

--- SingularityBase/src/main/java/com/hubspot/singularity/SingularityCheckingUpstreamsUpdate.java
+++ SingularityBase/src/main/java/com/hubspot/singularity/SingularityCheckingUpstreamsUpdate.java
@@ -1,26 +1,19 @@
 package com.hubspot.singularity;
 
 import com.google.common.base.Optional;
-import com.hubspot.baragon.models.BaragonRequestState;
 import com.hubspot.baragon.models.BaragonServiceState;
 
 public class SingularityCheckingUpstreamsUpdate {
-  private final BaragonRequestState baragonRequestState;
   private final Optional<BaragonServiceState> baragonServiceState;
   private final String singularityRequestId;
 
-  public SingularityCheckingUpstreamsUpdate (BaragonRequestState baragonRequestState,Optional<BaragonServiceState> baragonServiceState, String singularityRequest) {
-    this.baragonRequestState = baragonRequestState;
+  public SingularityCheckingUpstreamsUpdate (Optional<BaragonServiceState> baragonServiceState, String singularityRequest) {
     this.baragonServiceState = baragonServiceState;
     this.singularityRequestId = singularityRequest;
   }
 
   public String getSingularityRequestId() {
     return singularityRequestId;
-  }
-
-  public BaragonRequestState getBaragonRequestState() {
-    return baragonRequestState;
   }
 
   public Optional<BaragonServiceState> getBaragonServiceState() {
@@ -30,7 +23,6 @@ public class SingularityCheckingUpstreamsUpdate {
   @Override
   public String toString() {
     return "SingularityCheckingUpstreamsUpdate{" +
-        "baragonRequestState=" + baragonRequestState +
         ", baragonServiceState=" + baragonServiceState +
         ", singularityRequestId=" + singularityRequestId +
         '}';
