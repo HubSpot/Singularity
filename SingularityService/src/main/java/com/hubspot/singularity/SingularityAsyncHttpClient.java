bccaecd1e447a0bd9e6c6ba5b7b79b7d608447d8
Stephen Salinas <ssalinas@hubspot.com>
2019-02-19 11:11:59 -0500

    need this constructor still

--- SingularityService/src/main/java/com/hubspot/singularity/SingularityAsyncHttpClient.java
+++ SingularityService/src/main/java/com/hubspot/singularity/SingularityAsyncHttpClient.java
@@ -1,11 +1,16 @@
 package com.hubspot.singularity;
 
+import javax.inject.Inject;
+
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClientConfig;
 
-// Closed in SingularityLifecycleManaged
 public class SingularityAsyncHttpClient extends AsyncHttpClient {
+
   public SingularityAsyncHttpClient(AsyncHttpClientConfig clientConfig) {
     super(clientConfig);
   }
+
+  @Inject
+  public SingularityAsyncHttpClient() {}
 }
