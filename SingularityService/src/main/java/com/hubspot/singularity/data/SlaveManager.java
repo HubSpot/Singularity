d84badd8aa12fa864dc0a54eb11b7deaabc773db
Stephen Salinas <ssalinas@hubspot.com>
2019-04-22 11:16:41 -0400

    actually leader cache slave and rack data, and actually use it

--- SingularityService/src/main/java/com/hubspot/singularity/data/SlaveManager.java
+++ SingularityService/src/main/java/com/hubspot/singularity/data/SlaveManager.java
@@ -1,12 +1,13 @@
 package com.hubspot.singularity.data;
 
+import java.util.List;
+
 import org.apache.curator.framework.CuratorFramework;
 
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.base.Optional;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.hubspot.singularity.MachineState;
 import com.hubspot.singularity.SingularityMachineStateHistoryUpdate;
 import com.hubspot.singularity.SingularitySlave;
 import com.hubspot.singularity.config.SingularityConfiguration;
@@ -38,7 +39,7 @@ public class SlaveManager extends AbstractMachineManager<SingularitySlave> {
   }
 
   public void activateLeaderCache() {
-    leaderCache.cacheSlaves(getObjects());
+    leaderCache.cacheSlaves(getObjectsNoCache(getRoot()));
   }
 
   public Optional<SingularitySlave> getSlave(String slaveId) {
@@ -50,23 +51,17 @@ public class SlaveManager extends AbstractMachineManager<SingularitySlave> {
   }
 
   @Override
-  public int getNumActive() {
-    if (leaderCache.active()) {
-      return Math.toIntExact(leaderCache.getSlaves().stream().filter(x -> x.getCurrentState().getState().equals(MachineState.ACTIVE)).count());
-    }
-
-    return super.getNumActive();
+  public List<SingularitySlave> getObjectsFromLeaderCache() {
+    return leaderCache.getSlaves();
   }
 
   @Override
-  public void saveObject(SingularitySlave slave) {
-    if (leaderCache.active()) {
-      leaderCache.putSlave(slave);
-    }
-
-    super.saveObject(slave);
+  public void saveObjectToLeaderCache(SingularitySlave singularitySlave) {
+    leaderCache.putSlave(singularitySlave);
   }
 
-
-
+  @Override
+  public void deleteFromLeaderCache(String slaveId) {
+    leaderCache.removeSlave(slaveId);
+  }
 }
