9465b085e54ee55223b72be6f7b0b07a014d22bc
Stephen Salinas <ssalinas@hubspot.com>
2019-04-22 10:57:07 -0400

    also clean existing

--- SingularityService/src/main/java/com/hubspot/singularity/data/zkmigrations/CleanOldNodesMigration.java
+++ SingularityService/src/main/java/com/hubspot/singularity/data/zkmigrations/CleanOldNodesMigration.java
@@ -1,19 +1,27 @@
 package com.hubspot.singularity.data.zkmigrations;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.curator.framework.CuratorFramework;
 
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
+import com.hubspot.singularity.SingularityRequestGroup;
+import com.hubspot.singularity.data.RequestGroupManager;
+import com.hubspot.singularity.data.RequestManager;
 
 public class CleanOldNodesMigration extends ZkDataMigration {
   private final CuratorFramework curatorFramework;
+  private final RequestGroupManager requestGroupManager;
+  private final RequestManager requestManager;
 
   @Inject
-  public CleanOldNodesMigration(CuratorFramework curatorFramework) {
+  public CleanOldNodesMigration(CuratorFramework curatorFramework, RequestGroupManager requestGroupManager, RequestManager requestManager) {
     super(12);
     this.curatorFramework = curatorFramework;
+    this.requestGroupManager = requestGroupManager;
+    this.requestManager = requestManager;
   }
 
   @Override
@@ -23,6 +31,24 @@ public class CleanOldNodesMigration extends ZkDataMigration {
       for (String node : toClean) {
         if (curatorFramework.checkExists().forPath(node) != null) {
           curatorFramework.delete().deletingChildrenIfNeeded().forPath(node);
+        }
+      }
+      List<String> allIds = requestManager.getAllRequestIds();
+      for (SingularityRequestGroup requestGroup : requestGroupManager.getRequestGroups(false)) {
+        List<String> ids = requestGroup.getRequestIds()
+            .stream()
+            .filter((id) -> allIds.contains(id))
+            .collect(Collectors.toList());
+        if (ids.isEmpty()) {
+          requestGroupManager.deleteRequestGroup(requestGroup.getId());
+        } else {
+          if (!ids.equals(requestGroup.getRequestIds())) {
+            requestGroupManager.saveRequestGroup(new SingularityRequestGroup(
+                requestGroup.getId(),
+                ids,
+                requestGroup.getMetadata()
+            ));
+          }
         }
       }
     } catch (Throwable t) {
