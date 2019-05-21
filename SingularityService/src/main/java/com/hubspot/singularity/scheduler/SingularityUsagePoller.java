de59441fa0762827202cf80d8fdb0c2c3696996b
Gowtam Lal <glal@hubspot.com>
2019-05-21 15:26:15 -0400

    Logging.

--- SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularityUsagePoller.java
+++ SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularityUsagePoller.java
@@ -205,7 +205,7 @@ public class SingularityUsagePoller extends SingularityLeaderOnlyPoller {
         if (((shufflingForCpu && currentCpuLoad <= overloadedSlave.getSystemCpusTotal()) ||
             (!shufflingForCpu && currentMemUsageBytes <= configuration.getShuffleTasksWhenSlaveMemoryUtilizationPercentageExceeds() * overloadedSlave.getSystemMemTotalBytes()))
             || shuffledTasksOnSlave > configuration.getMaxTasksToShufflePerHost() || currentShuffleCleanupsTotal >= configuration.getMaxTasksToShuffleTotal()) {
-          LOG.debug("Not shuffling any more tasks ({} overage : {}%, shuffledOnHost: {}, totalShuffleCleanups: {})", mostOverusedResource.resourceType, mostOverusedResource.overusage * 100, shuffledTasksOnSlave, currentShuffleCleanupsTotal);
+          LOG.debug("Not shuffling any more tasks on slave {} ({} overage : {}%, shuffledOnHost: {}, totalShuffleCleanups: {})", taskIdWithUsage.getTaskId().getSanitizedHost(), mostOverusedResource.resourceType, mostOverusedResource.overusage * 100, shuffledTasksOnSlave, currentShuffleCleanupsTotal);
           break;
         }
 
