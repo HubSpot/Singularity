6f0cbc62d1d895e9a7d353437d20521a42e2ddfd
Stephen Salinas <ssalinas@hubspot.com>
2019-06-18 13:12:11 -0400

    test sql updates

--- SingularityService/singularity_test.sql
+++ SingularityService/singularity_test.sql
@@ -1,51 +1,51 @@
-CREATE TABLE `requestHistory` (
-  `requestId` varchar(100) NOT NULL,
-  `createdAt` timestamp(3) NOT NULL DEFAULT '1971-01-01 00:00:01.000',
-  `requestState` varchar(25) NOT NULL,
-  `user` varchar(100) DEFAULT NULL,
-  `request` blob NOT NULL,
-  `requestJson` TEXT DEFAULT NULL,
-  `message` varchar(280) DEFAULT NULL,
-  PRIMARY KEY (`requestId`,`createdAt`)
-) DEFAULT COLLATE=ascii_bin;
+CREATE TABLE requestHistory (
+  requestId varchar(100) NOT NULL,
+  createdAt timestamp(3) NOT NULL DEFAULT '1971-01-01 00:00:01.000',
+  requestState varchar(25) NOT NULL,
+  user varchar(100) DEFAULT NULL,
+  request blob NOT NULL,
+  requestJson TEXT DEFAULT NULL,
+  message varchar(280) DEFAULT NULL,
+  PRIMARY KEY (requestId,createdAt)
+);
 
-CREATE TABLE `deployHistory` (
-  `requestId` varchar(100) NOT NULL,
-  `deployId` varchar(100) NOT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT '1971-01-01 00:00:01',
-  `user` varchar(100) DEFAULT NULL,
-  `deployStateAt` timestamp NOT NULL DEFAULT '1971-01-01 00:00:01',
-  `deployState` varchar(25) NOT NULL,
-  `bytes` mediumblob NOT NULL,
-  `deployJson` TEXT DEFAULT NULL,
-  `message` varchar(280) DEFAULT NULL,
-  PRIMARY KEY (`requestId`,`deployId`),
-  KEY `requestId` (`requestId`,`createdAt`)
-) DEFAULT COLLATE=ascii_bin;
+CREATE TABLE deployHistory (
+  requestId varchar(100) NOT NULL,
+  deployId varchar(100) NOT NULL,
+  createdAt timestamp NOT NULL DEFAULT '1971-01-01 00:00:01',
+  user varchar(100) DEFAULT NULL,
+  deployStateAt timestamp NOT NULL DEFAULT '1971-01-01 00:00:01',
+  deployState varchar(25) NOT NULL,
+  bytes mediumblob NOT NULL,
+  deployJson TEXT DEFAULT NULL,
+  message varchar(280) DEFAULT NULL,
+  PRIMARY KEY (requestId,deployId),
+  KEY requestId (requestId,createdAt)
+);
 
-CREATE TABLE `taskHistory` (
-  `taskId` varchar(200) NOT NULL,
-  `requestId` varchar(100) NOT NULL,
-  `updatedAt` timestamp NOT NULL DEFAULT '1971-01-01 00:00:01',
-  `lastTaskStatus` varchar(25) DEFAULT NULL,
-  `bytes` mediumblob NOT NULL,
-  `taskJson` TEXT DEFAULT NULL,
-  `runId` varchar(100) DEFAULT NULL,
-  `deployId` varchar(100) DEFAULT NULL,
-  `host` varchar(100) CHARACTER SET ascii DEFAULT NULL,
-  `startedAt` timestamp NULL DEFAULT NULL,
-  `purged` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`taskId`),
-  KEY `requestId` (`requestId`,`updatedAt`),
-  KEY `runId` (`runId`,`requestId`),
-  KEY `startedAt` (`requestId`,`startedAt`),
-  KEY `lastTaskStatus` (`requestId`,`lastTaskStatus`,`startedAt`),
-  KEY `deployId` (`requestId`,`deployId`,`startedAt`),
-  KEY `host` (`requestId`,`host`,`startedAt`),
-  KEY `host2` (`host`),
-  KEY `requestDeployUpdated` (`requestId`, `deployId`, `updatedAt`),
-  KEY `hostUpdated` (`host`, `updatedAt`),
-  KEY `updatedAt` (`updatedAt`,`requestId`),
-  KEY `purged` (`requestId`,`purged`,`updatedAt`),
-  KEY `startedAt3` (`startedAt`)
-) DEFAULT COLLATE=ascii_bin;
+CREATE TABLE taskHistory (
+  taskId varchar(200) NOT NULL,
+  requestId varchar(100) NOT NULL,
+  updatedAt timestamp NOT NULL DEFAULT '1971-01-01 00:00:01',
+  lastTaskStatus varchar(25) DEFAULT NULL,
+  bytes mediumblob NOT NULL,
+  taskJson TEXT DEFAULT NULL,
+  runId varchar(100) DEFAULT NULL,
+  deployId varchar(100) DEFAULT NULL,
+  host varchar(100) CHARACTER SET ascii DEFAULT NULL,
+  startedAt timestamp NULL DEFAULT NULL,
+  purged tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (taskId),
+  KEY requestId (requestId,updatedAt),
+  KEY runId (runId,requestId),
+  KEY startedAt (requestId,startedAt),
+  KEY lastTaskStatus (requestId,lastTaskStatus,startedAt),
+  KEY deployId (requestId,deployId,startedAt),
+  KEY host (requestId,host,startedAt),
+  KEY host2 (host),
+  KEY requestDeployUpdated (requestId, deployId, updatedAt),
+  KEY hostUpdated (host, updatedAt),
+  KEY updatedAt (updatedAt,requestId),
+  KEY purged (requestId,purged,updatedAt),
+  KEY startedAt3 (startedAt)
+);
