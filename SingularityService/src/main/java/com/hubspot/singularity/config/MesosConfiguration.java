29063b039f9567524cdeb8115f8663101ac259ce
Stephen Salinas <ssalinas@hubspot.com>
2019-11-05 12:50:02 -0500

    default concurrency can be higher for lock contention

--- SingularityService/src/main/java/com/hubspot/singularity/config/MesosConfiguration.java
+++ SingularityService/src/main/java/com/hubspot/singularity/config/MesosConfiguration.java
@@ -53,7 +53,7 @@ public class MesosConfiguration {
   private Optional<String> credentialPrincipal = Optional.empty();
 
   private long rxEventBufferSize = 10000;
-  private int statusUpdateConcurrencyLimit = 10;
+  private int statusUpdateConcurrencyLimit = 50;
   private int maxStatusUpdateQueueSize = 5000;
   private int offersConcurrencyLimit = 10;
   private MachineLoadMetric scoreUsingSystemLoad = MachineLoadMetric.LOAD_5;
