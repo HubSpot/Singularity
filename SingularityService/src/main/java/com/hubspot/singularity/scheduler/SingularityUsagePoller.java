37455846ca69cb4045138fa99a49dc719ef161c3
William Hou <whou@hubspot.com>
2020-01-15 10:48:28 -0500

    sanity check logging, remove after sanity regained

--- SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularityUsagePoller.java
+++ SingularityService/src/main/java/com/hubspot/singularity/scheduler/SingularityUsagePoller.java
@@ -92,7 +92,10 @@ public class SingularityUsagePoller extends SingularityLeaderOnlyPoller {
     utilizationPerRequestId.values().forEach(usageManager::saveRequestUtilization);
 
     if (configuration.isShuffleTasksForOverloadedSlaves() && !disasterManager.isDisabled(SingularityAction.TASK_SHUFFLE)) {
+      LOG.debug("Shuffling?");
       taskShuffler.shuffle(overLoadedHosts);
+    } else {
+      LOG.debug("Not shuffling?");
     }
   }
 
