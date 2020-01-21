59fd951930895885fefd6e4fc55d136b39a49b3b
William Hou <whou@hubspot.com>
2020-01-21 17:52:24 -0500

    remove stray parameter

--- SingularityService/src/main/java/com/hubspot/singularity/data/zkmigrations/ShuffleBlacklistMigration.java
+++ SingularityService/src/main/java/com/hubspot/singularity/data/zkmigrations/ShuffleBlacklistMigration.java
@@ -4,20 +4,16 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.hubspot.singularity.config.SingularityConfiguration;
 import com.hubspot.singularity.data.ShuffleConfigurationManager;
-import org.apache.curator.framework.CuratorFramework;
 
 @Singleton
 public class ShuffleBlacklistMigration extends ZkDataMigration {
-
-  private final CuratorFramework curator;
   private final SingularityConfiguration configuration;
   private final ShuffleConfigurationManager manager;
 
 
   @Inject
-  public ShuffleBlacklistMigration(CuratorFramework curator, SingularityConfiguration configuration, ShuffleConfigurationManager manager) {
+  public ShuffleBlacklistMigration(SingularityConfiguration configuration, ShuffleConfigurationManager manager) {
     super(16);
-    this.curator = curator;
     this.configuration = configuration;
     this.manager = manager;
   }
@@ -29,3 +25,4 @@ public class ShuffleBlacklistMigration extends ZkDataMigration {
     }
   }
 }
+a
