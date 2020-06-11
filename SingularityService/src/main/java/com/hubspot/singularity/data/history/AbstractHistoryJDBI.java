b3335b0f33198ff0bc7b0e9456895fbac1906fc5
Faaya Fulas <ffulas@hubspot.com>
2020-06-11 17:47:45 -0400

    Fix formatting

--- SingularityService/src/main/java/com/hubspot/singularity/data/history/AbstractHistoryJDBI.java
+++ SingularityService/src/main/java/com/hubspot/singularity/data/history/AbstractHistoryJDBI.java
@@ -41,7 +41,16 @@ public interface AbstractHistoryJDBI extends HistoryJDBI {
     Optional<Long> updatedBefore,
     Optional<Long> updatedAfter
   ) {
-    if (host.isPresent() && (updatedAfter.isPresent() || updatedBefore.isPresent()) && !(requestId.isPresent() || deployId.isPresent() || runId.isPresent() || lastTaskStatus.isPresent())) {
+    if (
+      host.isPresent() &&
+      (updatedAfter.isPresent() || updatedBefore.isPresent()) &&
+      !(
+        requestId.isPresent() ||
+        deployId.isPresent() ||
+        runId.isPresent() ||
+        lastTaskStatus.isPresent()
+      )
+    ) {
       sqlBuilder.append(" FORCE INDEX (hostUpdated) ");
     }
 
