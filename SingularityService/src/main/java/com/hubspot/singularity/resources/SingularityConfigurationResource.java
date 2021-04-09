3e24f6aec297b975ee60ef40e665753c5f0f5ad4
Rosie Ellis <rellis@hubspot.com>
2021-04-09 16:26:49 -0400

    Logging for limits endpoint

--- SingularityService/src/main/java/com/hubspot/singularity/resources/SingularityConfigurationResource.java
+++ SingularityService/src/main/java/com/hubspot/singularity/resources/SingularityConfigurationResource.java
@@ -8,10 +8,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path(ApiPaths.CONFIGURATION_RESOURCE_PATH)
 @Schema(title = "Exposes some Singularity configuration values")
 public class SingularityConfigurationResource {
+  private static final Logger LOG = LoggerFactory.getLogger(
+    SingularityConfigurationResource.class
+  );
   private final SingularityConfiguration config;
 
   @Inject
@@ -23,6 +28,12 @@ public class SingularityConfigurationResource {
   @Path("/singularity-limits")
   @Operation(summary = "Retrieve configuration data for Singularity")
   public SingularityLimits getSingularityLimits() {
+    SingularityLimits limits = new SingularityLimits(
+      config.getMaxDecommissioningAgents()
+    );
+    LOG.info(
+      "Getting singularity limits, max decom: " + limits.getMaxDecommissioningAgents()
+    );
     return new SingularityLimits(config.getMaxDecommissioningAgents());
   }
 }
