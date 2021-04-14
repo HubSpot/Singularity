1c873d2ebde6b2baac4a58e12b62458ba0972bbf
Paul Schoenfelder <schoenfelder.p@gmail.com>
2021-04-14 12:47:50 -0400

    Add media type

--- SingularityService/src/main/java/com/hubspot/singularity/resources/SingularityConfigurationResource.java
+++ SingularityService/src/main/java/com/hubspot/singularity/resources/SingularityConfigurationResource.java
@@ -6,11 +6,17 @@ import com.hubspot.singularity.config.ApiPaths;
 import com.hubspot.singularity.config.SingularityConfiguration;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path(ApiPaths.CONFIGURATION_RESOURCE_PATH)
+@Produces({ MediaType.APPLICATION_JSON })
 @Schema(title = "Exposes some Singularity configuration values")
+@Tags({ @Tag(name = "Singularity configuration") })
 public class SingularityConfigurationResource {
   private final SingularityConfiguration config;
 
