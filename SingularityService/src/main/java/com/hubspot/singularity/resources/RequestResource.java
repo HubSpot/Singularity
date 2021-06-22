f96e90f9f9598525ea9949ee0777d596bbdac965
Rosie Ellis <rellis@hubspot.com>
2021-06-22 11:59:31 -0400

    move logging for caffeine cache check

--- SingularityService/src/main/java/com/hubspot/singularity/resources/RequestResource.java
+++ SingularityService/src/main/java/com/hubspot/singularity/resources/RequestResource.java
@@ -1480,6 +1480,9 @@ public class RequestResource extends AbstractRequestResource {
       limit,
       requestTypes
     );
+    LOG.info("Key: {}", key);
+    LOG.info("Use web cache {}", useWebCache(useWebCache));
+    LOG.info("Use caffeine cache cache {}", configuration.useCaffeineCache());
     if (useCaffeineCache(useWebCache)) {
       LOG.info("Attempting to grab {} from the cache", key);
       List<SingularityRequestParent> cachedRequests = requestsCache.getIfPresent(key);
@@ -1514,8 +1517,6 @@ public class RequestResource extends AbstractRequestResource {
   }
 
   private boolean useCaffeineCache(boolean useWebCache) {
-    LOG.info("Use web cache {}", useWebCache(useWebCache));
-    LOG.info("Use caffeine cache cache {}", configuration.useCaffeineCache());
     return !useWebCache(useWebCache) && configuration.useCaffeineCache();
   }
 
