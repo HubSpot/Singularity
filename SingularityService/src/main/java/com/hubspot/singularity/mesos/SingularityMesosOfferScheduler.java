964def0e320bcab1b5e46ab657a71f3222d8a7dc
Stephen Salinas <ssalinas@hubspot.com>
2021-08-10 14:34:03 -0400

    Add timeout within offer loop for single request taking too long

--- SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityMesosOfferScheduler.java
+++ SingularityService/src/main/java/com/hubspot/singularity/mesos/SingularityMesosOfferScheduler.java
@@ -45,6 +45,7 @@ import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Function;
@@ -541,7 +542,23 @@ public class SingularityMesosOfferScheduler {
                     () -> {
                       offerCheckTempLock.lock();
                       try {
+                        long startRequest = System.currentTimeMillis();
+                        int evaluated = 0;
                         for (SingularityTaskRequestHolder taskRequestHolder : entry.getValue()) {
+                          if (
+                            System.currentTimeMillis() -
+                            startRequest >
+                            TimeUnit.SECONDS.toMillis(30) && // TODO - make configurable
+                            evaluated > 1
+                          ) {
+                            LOG.warn(
+                              "{} is holding the offer lock for too long, skipping remaining {} tasks for scheduling",
+                              taskRequestHolder.getTaskRequest().getRequest().getId(),
+                              entry.getValue().size() - evaluated
+                            );
+                            break;
+                          }
+                          evaluated++;
                           List<SingularityTaskId> activeTaskIdsForRequest = leaderCache.getActiveTaskIdsForRequest(
                             taskRequestHolder.getTaskRequest().getRequest().getId()
                           );
