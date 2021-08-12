5dd341a869313e677d5351cfc9138a79b6cd4597
ajammalamadaka <ajammalamadaka@hubspot.com>
2021-08-12 09:23:19 -0500

    Round robin load balancing for the curator frameworks

--- SingularityService/src/main/java/com/hubspot/singularity/data/curator/ZkClientsLoadDistributor.java
+++ SingularityService/src/main/java/com/hubspot/singularity/data/curator/ZkClientsLoadDistributor.java
@@ -1,16 +1,32 @@
 package com.hubspot.singularity.data.curator;
 
+import com.google.common.collect.Iterators;
+import java.util.Iterator;
 import java.util.List;
 import org.apache.curator.framework.CuratorFramework;
 
 public class ZkClientsLoadDistributor {
   private final List<CuratorFramework> curatorFrameworks;
+  private final Iterator<CuratorFramework> iterator;
 
   public ZkClientsLoadDistributor(List<CuratorFramework> curatorFrameworks) {
     this.curatorFrameworks = curatorFrameworks;
+    this.iterator = Iterators.cycle(curatorFrameworks);
   }
 
-  public void start() {}
+  public void start() {
+    for (CuratorFramework framework : curatorFrameworks) {
+      framework.start();
+    }
+  }
 
-  public void close() {}
+  public void close() {
+    for (CuratorFramework framework : curatorFrameworks) {
+      framework.close();
+    }
+  }
+
+  public CuratorFramework getCuratorFramework() {
+    return iterator.next();
+  }
 }
