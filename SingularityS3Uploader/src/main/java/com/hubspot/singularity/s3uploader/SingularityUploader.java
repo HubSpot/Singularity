fd8d6be01b2dcd6d0c09fcf0802c5a6d145c354a
Gowtam Lal <glal@hubspot.com>
2021-08-19 14:05:52 -0400

    Configurably skip the file open check in S3Uploader.

--- SingularityS3Uploader/src/main/java/com/hubspot/singularity/s3uploader/SingularityUploader.java
+++ SingularityS3Uploader/src/main/java/com/hubspot/singularity/s3uploader/SingularityUploader.java
@@ -105,6 +105,7 @@ public abstract class SingularityUploader {
       final Path file = toUpload.get(i);
       if (
         !configuration.isCheckForOpenFiles() ||
+        !uploadMetadata.isCheckIfOpen() ||
         uploadMetadata.isImmediate() ||
         !isFileOpen(file, configuration.isCheckOpenFilesViaFuser())
       ) {
