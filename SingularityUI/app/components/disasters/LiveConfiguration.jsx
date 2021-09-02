71a0b5cfb39e3c234d3915477ccf77c277915594
William Hou <whou@hubspot.com>
2021-09-02 17:09:45 -0400

    wire up config overrides from ui

--- SingularityUI/app/components/disasters/LiveConfiguration.jsx
+++ SingularityUI/app/components/disasters/LiveConfiguration.jsx
@@ -2,15 +2,24 @@ import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import Utils from '../../utils';
 import Section from '../common/Section';
+import { EnableRackSensitivity, DisableRackSensitivity, OverridePlacementStrategy } from '../../actions/api/config';
 
 class LiveConfiguration extends Component {
   static propTypes = {
     user: PropTypes.string,
+    enableRackSensitivity: PropTypes.func,
+    disableRackSensitivity: PropTypes.func,
+    overridePlacementStrategy: PropTypes.func,
   };
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      placementStrategy: '',
+    };
+    this.updatePlacementStrategy = e => {
+      this.setState({ placementStrategy: e.target.value });
+    }
   }
 
   render() {
@@ -22,23 +31,31 @@ class LiveConfiguration extends Component {
             <button
               className="btn btn-primary"
               alt="Enable Rack Sensitivity"
-              title="Enable Rack Sensitivity">
+              title="Enable Rack Sensitivity"
+              onClick={this.props.enableRackSensitivity}>
               Enable
             </button>
             <button
               className="btn btn-danger"
               alt="Disable Rack Sensitivity"
-              title="Disable Rack Sensitivity">
+              title="Disable Rack Sensitivity"
+              onClick={this.props.disableRackSensitivity}>
               Disable
             </button>
           </div>
           <div className="col-md-6">
             <h3>Placement Strategy</h3>
-            <input type="text" />
+            <input
+              type="text"
+              placeholder="GREEDY/OPTIMISTIC"
+              onChange={this.updatePlacementStrategy}
+              value={this.state.placementStrategy}
+            />
             <button
               className="btn btn-danger"
               alt="Override Placement Strategy"
-              title="Override Placement Strategy">
+              title="Override Placement Strategy"
+              onClick={() => this.props.overridePlacementStrategy(this.state.placementStrategy)}>
               Override
             </button>
           </div>
@@ -55,4 +72,12 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(LiveConfiguration);
+function mapDispatchToProps(dispatch) {
+  return {
+    enableRackSensitivity: () => dispatch(EnableRackSensitivity.trigger()),
+    disableRackSensitivity: () => dispatch(DisableRackSensitivity.trigger()),
+    overridePlacementStrategy: strategy => dispatch(OverridePlacementStrategy.trigger(strategy)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LiveConfiguration);
